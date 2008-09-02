d68ffde225cb66384232d933d8bebc76c365bc78
André van Hoorn <avh@informatik.uni-kiel.de>
2008-09-02 15:33:51 +0000

    added interceptors

---
+++ src/kieker/tpmon/interceptors/SessionRegistrationInterceptor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kieker.tpmon.interceptors;
+
+import kieker.tpmon.TpmonController;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.WebRequestInterceptor;
+
+/**
+ *
+ * @author Andre van Hoorn
+ */
+public class SessionRegistrationInterceptor implements WebRequestInterceptor {
+
+    public void preHandle(WebRequest request) throws Exception {
+        TpmonController.getInstance().registerSessionIdentifier(request.getSessionId(), Thread.currentThread().getId());
+    }
+
+    public void postHandle(WebRequest request, ModelMap map) throws Exception {
+    }
+
+    public void afterCompletion(WebRequest request, Exception map) throws Exception {
+    }
+}
+
+
