abeb269140603aadc2ddef342ac6eace2bd90688
André van Hoorn <avh@informatik.uni-kiel.de>
2008-09-14 17:20:49 +0000

    additional log plot

--- src/kieker/tests/compileTimeWeaving/bookstoreBenchmark/gen-results.r
+++ src/kieker/tests/compileTimeWeaving/bookstoreBenchmark/gen-results.r
@@ -25,17 +25,15 @@ accesslog.read = function (fn) {
 	accesslog_raw <- read.table (file=fn, header=TRUE, sep=' ', quote='"\'', dec='.',  na.strings = "NA", nrows = -1, skip =  0, row.names = NULL, fill = FALSE, strip.white = FALSE, blank.lines.skip = TRUE, comment.char="#",stringsAsFactors=FALSE)
 }
 
-data.081=accesslog.read(fn.081)
-data.090=accesslog.read(fn.090)
-data.cur=accesslog.read(fn.cur)
-
+data.081=accesslog.read(fn.081);data.081[["resp_ms"]]=data.081[["resp_ns"]]/(1000*1000)
+data.090=accesslog.read(fn.090);data.090[["resp_ms"]]=data.090[["resp_ns"]]/(1000*1000)
+data.cur=accesslog.read(fn.cur);data.cur[["resp_ms"]]=data.cur[["resp_ns"]]/(1000*1000)
 
 ## box plots
 accesslog.boxplot = function (accesslog, col=NULL, xaxt=NULL,
 add=FALSE, at=NULL){
 	attach(accesslog)
         print(unique(opname))
-        resp_ms=resp_ns/(1000*1000)
 	bp=boxplot(resp_ms~opname, add=add, col=col, xaxt=xaxt, at=at)
         #print(means)
         points(at, mean(resp_ms),pch=18,cex=2)
@@ -73,4 +71,46 @@ accesslog.boxplot(subset(data.cur,opname==curop), col=col.cur, add=TRUE, at=3.5)
 axis(side=1, at=c(1.5,2.5,3.5), labels=c("0.81", "0.90", "trunk"))
 mtext("Tpmon version", side=1, line=2)
 legend("bottomleft", legend=c("mean"), pch=c(18), pt.bg=c("black"), pt.cex=2, ncol=1, bg="white")
+
+## Non-trimmed data including min and max values (y-axis in log scale)
+
+curop="Bookstore.searchBook()"
+ylim=c(
+ min(c(subset(data.081,opname==curop)$resp_ms,subset(data.090,opname==curop)$resp_ms,subset(data.cur,opname==curop)$resp_ms)),
+ max(c(subset(data.081,opname==curop)$resp_ms,subset(data.090,opname==curop)$resp_ms,subset(data.cur,opname==curop)$resp_ms))
+)
+plot(xscale, seq(from=ylim[1],to=ylim[2],length.out=length(xscale)),type="n", xlab="", ylab="Response time (ms) [log scale]", xaxt="n", main=curop, log="y") #xaxt="n"
+accesslog.boxplot(subset(data.081,opname==curop), col=col.081, add=TRUE, at=1.5)
+accesslog.boxplot(subset(data.090,opname==curop), col=col.090, add=TRUE, at=2.5)
+accesslog.boxplot(subset(data.cur,opname==curop), col=col.cur, add=TRUE, at=3.5)
+axis(side=1, at=c(1.5,2.5,3.5), labels=c("0.81", "0.90", "trunk"))
+mtext("Tpmon version", side=1, line=2)
+legend("bottomleft", legend=c("mean"), pch=c(18), pt.bg=c("black"), pt.cex=2, ncol=1, bg="white")
+
+curop="Bookstore.searchBook()"
+ylim=c(
+ min(c(subset(data.081,opname==curop)$resp_ms,subset(data.090,opname==curop)$resp_ms,subset(data.cur,opname==curop)$resp_ms)),
+ max(c(subset(data.081,opname==curop)$resp_ms,subset(data.090,opname==curop)$resp_ms,subset(data.cur,opname==curop)$resp_ms))
+)
+plot(xscale, seq(from=ylim[1],to=ylim[2],length.out=length(xscale)),type="n", xlab="", ylab="Response time (ms) [log scale]", xaxt="n", main=curop, log="y") #xaxt="n"
+accesslog.boxplot(subset(data.081,opname==curop), col=col.081, add=TRUE, at=1.5)
+accesslog.boxplot(subset(data.090,opname==curop), col=col.090, add=TRUE, at=2.5)
+accesslog.boxplot(subset(data.cur,opname==curop), col=col.cur, add=TRUE, at=3.5)
+axis(side=1, at=c(1.5,2.5,3.5), labels=c("0.81", "0.90", "trunk"))
+mtext("Tpmon version", side=1, line=2)
+legend("bottomleft", legend=c("mean"), pch=c(18), pt.bg=c("black"), pt.cex=2, ncol=1, bg="white")
+
+curop="CRM.getOffers()"
+ylim=c(
+ min(c(subset(data.081,opname==curop)$resp_ms,subset(data.090,opname==curop)$resp_ms,subset(data.cur,opname==curop)$resp_ms)),
+ max(c(subset(data.081,opname==curop)$resp_ms,subset(data.090,opname==curop)$resp_ms,subset(data.cur,opname==curop)$resp_ms))
+)
+plot(xscale, seq(from=ylim[1],to=ylim[2],length.out=length(xscale)),type="n", xlab="", ylab="Response time (ms) [log scale]", xaxt="n", main=curop, log="y") #xaxt="n"
+accesslog.boxplot(subset(data.081,opname==curop), col=col.081, add=TRUE, at=1.5)
+accesslog.boxplot(subset(data.090,opname==curop), col=col.090, add=TRUE, at=2.5)
+accesslog.boxplot(subset(data.cur,opname==curop), col=col.cur, add=TRUE, at=3.5)
+axis(side=1, at=c(1.5,2.5,3.5), labels=c("0.81", "0.90", "trunk"))
+mtext("Tpmon version", side=1, line=2)
+legend("bottomleft", legend=c("mean"), pch=c(18), pt.bg=c("black"), pt.cex=2, ncol=1, bg="white")
+
 dev.off()
