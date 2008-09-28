5bc3d751b744ff7e608b57809b1159a8ae11262f
André van Hoorn <avh@informatik.uni-kiel.de>
2008-09-28 11:40:49 +0000

    remote servlet

--- src/kieker/tpmon/aspects/KiekerTpmonMonitoringAnnotationRemoteServlet.java
+++ src/kieker/tpmon/aspects/KiekerTpmonMonitoringAnnotationRemoteServlet.java
@@ -1,15 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *
  */
-
 package kieker.tpmon.aspects;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import kieker.tpmon.ExecutionData;
-import kieker.tpmon.annotations.*;
 import kieker.tpmon.*;
+import kieker.tpmon.annotations.*;
+import kieker.tpmon.asyncDbconnector.*;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -17,11 +17,11 @@ import org.aspectj.lang.annotation.Pointcut;
 
 /**
  *
- * @author avanhoorn
+ * @author Andre
  */
 @Aspect
-public class KiekerTpmonMonitoringAnnotationServlet extends AbstractKiekerTpmonMonitoringServlet {
-    
+public class KiekerTpmonMonitoringAnnotationRemoteServlet extends AbstractKiekerTpmonMonitoringServlet { 
+
    @Pointcut("execution(* *.do*(..)) " +
    "&& args(request,response) ")
     public void monitoredServletEntry(HttpServletRequest request, HttpServletResponse response) {
@@ -31,19 +31,28 @@ public class KiekerTpmonMonitoringAnnotationServlet extends AbstractKiekerTpmonM
     public Object doServletEntryProfiling(ProceedingJoinPoint thisJoinPoint) throws Throwable {
         return super.doServletEntryProfiling(thisJoinPoint);
     }
-   
+    
     @Pointcut("execution(@TpmonMonitoringProbe * *.*(..)) ")
     public void monitoredMethod() {
     }
-      
+   
     @Around("monitoredMethod()")
     public Object doBasicProfiling(ProceedingJoinPoint thisJoinPoint) throws Throwable {
         if (!ctrlInst.isMonitoringEnabled()) {
             return thisJoinPoint.proceed();
         }
-        
-        ExecutionData execData = this.initExecutionData(thisJoinPoint);
+
+        ExecutionData execData = this.initExecutionData(thisJoinPoint);        
         String sessionId = ctrlInst.recallThreadLocalSessionId(); // may be null
+        int eoi = 0; /* this is executionOrderIndex-th execution in this trace */
+        int ess = 0; /* this is the height in the dynamic call tree of this execution */
+        if (execData.isEntryPoint){
+            ctrlInst.storeThreadLocalEOI(0);
+            ctrlInst.storeThreadLocalESS(1);
+        } else {
+            eoi = ctrlInst.incrementAndRecallThreadLocalEOI();
+            ess = ctrlInst.recallAndIncrementThreadLocalESS();
+        }
         try{
             this.proceedAndMeasure(thisJoinPoint, execData);
         } catch (Exception e){
@@ -51,11 +60,18 @@ public class KiekerTpmonMonitoringAnnotationServlet extends AbstractKiekerTpmonM
         } finally {
             /* note that proceedAndMeasure(...) even sets the variable name
              * in case the execution of the joint point resulted in an
-             * execpetion! */
+             * exception! */
             ctrlInst.insertMonitoringDataNow(execData.componentName, 
                     execData.opname, sessionId, execData.traceId, 
-                    execData.tin, execData.tout);
-            // Since we didn't register the sessionId we won't unset it!
+                    execData.tin, execData.tout,
+                    eoi, ess);
+            if (execData.isEntryPoint){
+                // Since we didn't register the sessionId we won't unset it!
+                ctrlInst.unsetThreadLocalEOI();
+                ctrlInst.unsetThreadLocalESS();
+            } else {
+                ctrlInst.storeThreadLocalESS(ess);
+            }
         }
         return execData.retVal;
     }
