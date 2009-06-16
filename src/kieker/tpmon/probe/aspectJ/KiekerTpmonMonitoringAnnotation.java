d03050032bccf3a059e28bcf375eada1bc17e5e2
André van Hoorn <avh@informatik.uni-kiel.de>
2009-06-16 15:14:39 +0000

    restructured packages -- unfortunately lost history of probe :-(

---
+++ src/kieker/tpmon/probe/aspectJ/KiekerTpmonMonitoringAnnotation.java
@@ -0,0 +1,56 @@
+package kieker.tpmon.probes.aspectJ;
+
+import kieker.tpmon.monitoringRecords.KiekerExecutionRecord;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * kieker.tpmon.aspects.KiekerTpmonMonitoringAnnotation
+ *
+ * ==================LICENCE=========================
+ * Copyright 2006-2008 Matthias Rohr and the Kieker Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==================================================
+ *
+ * @author Andre van Hoorn
+ */
+@Aspect
+public class KiekerTpmonMonitoringAnnotation extends AbstractKiekerTpmonMonitoring { 
+
+    @Pointcut("execution(@kieker.tpmon.annotations.TpmonMonitoringProbe * *.*(..))"+
+              " && !execution(@kieker.tpmon.annotations.TpmonInternal * *.*(..))")
+    public void monitoredMethod() {
+    }
+   
+    @Around("monitoredMethod()")
+    public Object doBasicProfiling(ProceedingJoinPoint thisJoinPoint) throws Throwable {
+        if (!ctrlInst.isMonitoringEnabled()) {
+            return thisJoinPoint.proceed();
+        }
+        KiekerExecutionRecord execData = this.initExecutionData(thisJoinPoint);
+        try{
+            this.proceedAndMeasure(thisJoinPoint, execData);
+        } catch (Exception e){
+            throw e; // exceptions are forwarded          
+        } finally {
+            /* note that proceedAndMeasure(...) even sets the variable name
+             * in case the execution of the joint point resulted in an
+             * exception! */
+            ctrlInst.insertMonitoringDataNow(execData);
+        }
+        return execData.retVal;
+    }
+}
---
+++ src/kieker/tpmon/probe/spring/KiekerTpmonMethodInvocationInterceptor.java
@@ -0,0 +1,66 @@
+package kieker.tpmon.probe.spring;
+
+import kieker.tpmon.monitoringRecord.KiekerExecutionRecord;
+import kieker.tpmon.annotation.TpmonInternal;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+/**
+ * kieker.tpmon.aspects.springAspectJ.KiekerTpmonMethodInvocationInterceptor
+ *
+ * ==================LICENCE=========================
+ * Copyright 2006-2008 Matthias Rohr and the Kieker Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==================================================
+ *
+ * This annotation marks methods that are exit points for remote calls
+ * that go to an other virtual machine. The annotation tries to ensure
+ * that the trace id is propergated to an other instance of tpmon in
+ * the other virtual machine.
+ *
+ * It provides the boolean property useRuntimeClassname to select whether
+ * to use the declaring or the runtime classname of the instrumented methods.
+ * 
+ * @author Andre van Hoorn
+ */
+public class KiekerTpmonMethodInvocationInterceptor extends AbstractKiekerTpmonMethodInvocationInterceptor {
+
+    private static final Log log = LogFactory.getLog(KiekerTpmonMethodInvocationInterceptor.class);
+
+    /**
+     * @see org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation)
+     */
+    @TpmonInternal()
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        long traceId = tpmonController.recallThreadLocalTraceId();
+        // Only go on if a traceId has been registered before
+        if (traceId == -1 || !tpmonController.isMonitoringEnabled()) {
+            return invocation.proceed();
+        }
+
+        KiekerExecutionRecord execData = this.initExecutionData(invocation);
+        try {
+            this.proceedAndMeasure(invocation, execData);
+        } catch (Exception e) {
+            throw e; // exceptions are forwarded
+        } finally {
+            /* note that proceedAndMeasure(...) even sets the variable name
+             * in case the execution of the joint point resulted in an
+             * exception! */
+            tpmonController.insertMonitoringDataNow(execData);
+        }
+        return execData.retVal;
+    }
+}
