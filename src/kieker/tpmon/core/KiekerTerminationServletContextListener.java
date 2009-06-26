fac1e544c5702f8f68c91ee0555c4ddba8370090
André van Hoorn <avh@informatik.uni-kiel.de>
2009-06-26 08:13:43 +0000

    moved context listener

---
+++ src/kieker/tpmon/core/KiekerTerminationServletContextListener.java
@@ -0,0 +1,33 @@
+package kieker.tpmon.core;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Use this listener in webapps where TpmonShutdownHook is not called
+ * 
+ * @author Dennis Kieselhorst
+ *
+ */
+public class KiekerTerminationServletContextListener implements ServletContextListener {
+	private static final Log LOG = LogFactory.getLog(KiekerTerminationServletContextListener.class);
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent evt) {
+		TpmonController.getInstance().terminateMonitoring();
+		LOG.info("context destroyed");
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent evt) {
+		LOG.info("context initialized");
+	}
+
+}
