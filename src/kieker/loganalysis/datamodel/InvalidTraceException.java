d2f9cd4bb9b8eaf8e648dbbf05aa475ad784db79
André van Hoorn <avh@informatik.uni-kiel.de>
2009-07-01 14:33:24 +0000

    was missing

---
+++ src/kieker/loganalysis/datamodel/InvalidTraceException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kieker.loganalysis.datamodel;
+
+/**
+ *
+ * @author avanhoorn
+ */
+public class InvalidTraceException extends Exception {
+
+    public InvalidTraceException(String message) {
+        super(message);
+    }
+
+}
