3c1ea19ba023560e287b5117855d19d1b3d9cd3c
André van Hoorn <avh@informatik.uni-kiel.de>
2009-07-16 09:23:38 +0000

    pom files now included in release

--- src/kieker/tpmon/core/TpmonVersion.java
+++ src/kieker/tpmon/core/TpmonVersion.java
@@ -34,7 +34,7 @@ public class TpmonVersion {
 	 * The VERSION string is updated by the Ant build file, which looks for the
 	 * pattern: VERSION = <quote>.*<quote>
 	 */
-	private static final String VERSION = "0.95-20090715";
+	private static final String VERSION = "0.95-20090716-1022-20090716";
 
 	static final String COPYRIGHT = "Copyright (c) 2006-2009 Kieker Project";
 
