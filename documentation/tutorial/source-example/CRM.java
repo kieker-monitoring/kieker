6facad2a779190db6174195a2f89c3c5576a9fd3
Nils Ehmke <nie@informatik.uni-kiel.de>
2010-06-02 12:41:12 +0000

    added the directory for the sourcecode examples of the tutorial and added some more text to the tutorial as well

---
+++ documentation/tutorial/source-example/CRM.java
@@ -0,0 +1,8 @@
+package mySimpleKiekerExample.bookstoreTracing;
+
+public class CRM {
+
+    public static void getOffers(){
+	Catalog.getBook(true);
+    }
+}
---
+++ documentation/tutorial/source-example/Catalog.java
@@ -0,0 +1,11 @@
+package mySimpleKiekerExample.bookstoreTracing;
+
+public class Catalog {
+    public static void getBook(boolean complexQuery) {
+        if (complexQuery) {
+            Bookstore.waitabit(20);
+        } else {
+            Bookstore.waitabit(2);
+        }
+    }
+}
