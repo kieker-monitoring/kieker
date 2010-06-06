cdcaaba99158617ecff7a9866ac9c2e2c6e0810c
Nils Ehmke <nie@informatik.uni-kiel.de>
2010-06-06 13:49:34 +0000

    Added a sequence diagram for the tutorial; changed the examples a little bit

---
+++ documentation/tutorial/source-example/manual monitoring with own writer/src/OwnWriter.java
@@ -0,0 +1,45 @@
+package mySimpleKiekerExample.bookstoreTracing;
+
+import kieker.tpmon.writer.AbstractKiekerMonitoringLogWriter;
+import kieker.tpmon.monitoringRecord.AbstractKiekerMonitoringRecord;
+import kieker.tpmon.annotation.TpmonInternal;
+import kieker.tpmon.writer.util.async.AbstractWorkerThread;
+import java.util.Vector;
+
+public class OwnWriter extends AbstractKiekerMonitoringLogWriter {
+
+    @TpmonInternal()
+    public void registerMonitoringRecordType(int id, String className) {
+    }
+
+    @TpmonInternal()
+    public boolean writeMonitoringRecord(AbstractKiekerMonitoringRecord monitoringRecord) {
+      String arr[] = monitoringRecord.toStringArray();
+      int len = arr.length;
+      if (len > 0) {
+	for (int i = 0; i < len - 1; i++) {
+	  System.out.print(arr[i] + ", ");
+	}
+	System.out.println(arr[len-1]);
+      }
+      return true;
+    }
+
+    @TpmonInternal()
+    public boolean init(String initString) {
+      return true;
+    }
+
+    /**
+     * Returns a vector of workers, or null if none.
+     */
+    @TpmonInternal()
+    public Vector<AbstractWorkerThread> getWorkers() {
+      return null;
+    }
+
+    @TpmonInternal()
+    public String getInfoString() {
+      return null;
+    }
+} 
---
+++ documentation/tutorial/source-example/manual monitoring/src/Bookstore.java
@@ -0,0 +1,73 @@
+package mySimpleKiekerExample.bookstoreTracing;
+
+import java.util.Vector;
+import kieker.tpmon.core.TpmonController;
+import kieker.tpmon.monitoringRecord.executions.KiekerExecutionRecord;
+
+public class Bookstore extends Thread {
+
+    static int numberOfRequests = 1;
+    static int interRequestTime = 5;
+    static final Vector<Bookstore> bookstoreScenarios = new Vector<Bookstore>();
+ 
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < numberOfRequests; i++) {
+            System.out.println("Bookstore.main: Starting request " + i);
+            Bookstore newBookstore = new Bookstore();
+            bookstoreScenarios.add(newBookstore);
+            newBookstore.start();
+            Bookstore.waitabit(interRequestTime);
+        }
+        System.out.println("Bookstore.main: Finished with starting all requests.");
+        System.out.println("Bookstore.main: Waiting for threads to terminate");
+        synchronized (bookstoreScenarios) {
+            while (!bookstoreScenarios.isEmpty()) {
+                bookstoreScenarios.wait();
+            }
+        }
+	System.exit(0);
+    }
+
+    @Override
+    public void run() {
+	long tin = TpmonController.getInstance().getTime();
+        Bookstore.searchBook();
+	long tout = TpmonController.getInstance().getTime();
+
+	KiekerExecutionRecord e = KiekerExecutionRecord.getInstance("mySimpleKiekerExample.bookstoreTracing.Bookstore", "searchBook()", "sessionID", 0, tin, tout, "vnName", 0, 0);
+	TpmonController.getInstance().logMonitoringRecord(e);
+
+        synchronized (bookstoreScenarios) {
+            bookstoreScenarios.remove(this);
+            bookstoreScenarios.notify();
+        }
+    }
+
+    public static void searchBook() {
+        for (int i = 0; i < 1; i++) {
+
+	    long tin = TpmonController.getInstance().getTime();
+	    Catalog.getBook(false);
+	    long tout = TpmonController.getInstance().getTime();
+
+	    KiekerExecutionRecord e = KiekerExecutionRecord.getInstance("mySimpleKiekerExample.bookstoreTracing.Catalog", "getBook(false)", "sessionID", 0, tin, tout, "vnName", 1, 1);
+	    TpmonController.getInstance().logMonitoringRecord(e);
+
+	    tin = TpmonController.getInstance().getTime();
+            CRM.getOffers();
+	    tout = TpmonController.getInstance().getTime();
+
+	    e = KiekerExecutionRecord.getInstance("mySimpleKiekerExample.bookstoreTracing.CRM", "getOffers()", "sessionID", 0, tin, tout, "vnName", 2, 1);
+	    TpmonController.getInstance().logMonitoringRecord(e);  
+        }
+    }
+
+    public static void waitabit(long waittime) {
+        if (waittime > 0) {
+            try {
+                Thread.sleep(waittime);
+            } catch (Exception e) {
+            }
+        }
+    }
+} 
