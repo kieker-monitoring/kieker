d2f8b22b2388cab840954b403e41901923222e64
Nils Ehmke <nie@informatik.uni-kiel.de>
2010-06-16 15:03:12 +0000

    Modified the little program for the collecting of the libraries so that only JAR files will be used.

--- documentation/tutorial/source-make-library/src/main/Pair.java
+++ documentation/tutorial/source-make-library/src/main/Pair.java
@@ -1,17 +1,36 @@
 package main;
 
 /**
- * @author nils
- *
- * @version 1.0
+ * This class supplies the possibility to save two (different) objects in one
+ * object.
+ * 
+ * @author Nils Christian Ehmke
+ * 
+ * @version 1.0 The first implementation of the class.
  */
 public class Pair<FST, SND> {
 
-    public FST first;
-    public SND second;
+	/**
+	 * The field for the first object.
+	 */
+	public FST first;
+	/**
+	 * The field for the second object.
+	 */
+	public SND second;
 
-    public Pair(FST first, SND second) {
-        this.first = first;
-        this.second = second;
-    }
+	/**
+	 * Creates a new instance of the class <code>Pair</code> with the given
+	 * parameters.
+	 * 
+	 * @param first
+	 *            The first object.
+	 * @param second
+	 *            The second object.
+	 * @since 1.0
+	 */
+	public Pair(FST first, SND second) {
+		this.first = first;
+		this.second = second;
+	}
 }
