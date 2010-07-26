4f43d657f01fd00d59d7589483c3e541dfec1d1e
André van Hoorn <avh@informatik.uni-kiel.de>
2010-07-26 18:42:35 +0000

    simplified example

--- doc/tutorial/source-example/manual-monitoring/src/mySimpleKiekerExampleManual/Bookstore.java
+++ doc/tutorial/source-example/manual-monitoring/src/mySimpleKiekerExampleManual/Bookstore.java
@@ -12,8 +12,8 @@ public class Bookstore {
             MonitoringController.getInstance();
 
     public void searchBook() {
-        /* 1. Call the Catalog component's getBook() method
-         *    and log its entry and exit using Kieker. */
+        /* 1.) Call the Catalog component's getBook() method
+         *     and log its entry and exit using Kieker. */
         long tin = MONITORING_CONTROLLER.getTime();
         catalog.getBook(false);
         long tout = MONITORING_CONTROLLER.getTime();
@@ -23,8 +23,8 @@ public class Bookstore {
                 tin, tout);
         MONITORING_CONTROLLER.newMonitoringRecord(e);
 
-        /* 2. Call the CRM catalog's getOffers() method
-         *    (without monitoring). */
+        /* 2.) Call the CRM catalog's getOffers() method
+         *     (without monitoring). */
         crm.getOffers();
     }
 }
