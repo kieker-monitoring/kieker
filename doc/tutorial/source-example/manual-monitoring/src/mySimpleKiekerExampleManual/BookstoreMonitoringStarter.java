0195fd479d3bef687f4fe6683d5775e4cdaed6ca
Nils Ehmke <nie@informatik.uni-kiel.de>
2010-08-01 16:38:44 +0000

    Simplified the BookstoreMonitoringStarter.java; Added some macros; Added some TODOs

--- doc/tutorial/source-example/manual-monitoring/src/mySimpleKiekerExampleManual/BookstoreMonitoringStarter.java
+++ doc/tutorial/source-example/manual-monitoring/src/mySimpleKiekerExampleManual/BookstoreMonitoringStarter.java
@@ -1,15 +1,8 @@
 package mySimpleKiekerExampleManual;
 
-import kieker.analysis.AnalysisInstance;
-import kieker.analysis.plugin.MonitoringRecordConsumerException;
-import kieker.analysis.reader.MonitoringLogReaderException;
-import kieker.analysis.reader.filesystem.FSReader;
-import kieker.monitoring.core.MonitoringController;
-
 public class BookstoreMonitoringStarter {
 
-    public static void main(String[] args)
-            throws MonitoringLogReaderException, MonitoringRecordConsumerException {
+    public static void main(String[] args) {
         Bookstore bookstore = new Bookstore();
         for (int i = 0; i < 5; i++) {
             System.out.println("Bookstore.main: Starting request " + i);
