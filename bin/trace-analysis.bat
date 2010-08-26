24846bbeb768260549053168b2369a7fcb9229a3
Nils Ehmke <nie@informatik.uni-kiel.de>
2010-08-26 12:13:05 +0000

    Added another batch-file due to problems with cmd. Call trace-analysis.bat to start the analysis and NOT trace-analysis-para.bat.

--- bin/trace-analysis.bat
+++ bin/trace-analysis.bat
@@ -1,22 +1,8 @@
+@echo off 
 REM This wrapper script needs still improvement.
 REM
 REM @author Nils Christian Ehmke
 
-REM Don't show everything on the screen.
-@echo off 
-
-REM Get the directory of this file and change the working directory to it.
-cd %~dp0
-
-REM Set every variable we will need for the execution.
-set BINDIR=%cd%
-set CLASSPATH=%BINDIR%\..\lib\commons-logging-1.1.1.jar;%BINDIR%\..\lib\commons-cli-1.2.jar;%BINDIR%\..\dist\kieker-1.2-SNAPSHOT.jar
-set JAVAARGS="-Dlog4j.configuration=./log4j.properties -Xms56m -Xmx1024m"
-set MAINCLASSNAME=kieker.tools.traceAnalysis.TraceAnalysisTool
-
-REM Now start the tool, but don't forget to deliver the parameters.
-java %JAVAARGS% -cp "%CLASSPATH%" %MAINCLASSNAME% %*
+cmd /V:ON /C trace-analysis-para.bat
 
 @echo on
-REM Don't close the window immediately.
-PAUSE
