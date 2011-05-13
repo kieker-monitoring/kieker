7da198ab5413b749cdee84413679e7a89c1ae9cc
André van Hoorn <avh@informatik.uni-kiel.de>
2011-05-13 07:56:00 +0000

    re-added accidentially removed file

---
+++ src/monitoring/kieker/monitoring/probe/sigar/SigarSamplerFactory.java
@@ -0,0 +1,84 @@
+package kieker.monitoring.probe.sigar;
+
+import kieker.monitoring.core.controller.WriterController;
+import kieker.monitoring.probe.sigar.samplers.AbstractSigarSampler;
+import kieker.monitoring.probe.sigar.samplers.CPUsCombinedPercSampler;
+import kieker.monitoring.probe.sigar.samplers.CPUsDetailedPercSampler;
+import kieker.monitoring.probe.sigar.samplers.MemSwapUsageSampler;
+
+import org.hyperic.sigar.Humidor;
+import org.hyperic.sigar.Sigar;
+import org.hyperic.sigar.SigarProxy;
+
+/**
+ * Provides factory methods for {@link AbstractSigarSampler}s.
+ *
+ * @author Andre van Hoorn
+ */
+public final class SigarSamplerFactory implements ISigarSamplerFactory {
+        //private static final Log log = LogFactory.getLog(SigarSamplerFactory.class);
+
+        /**
+         * Returns the singleton instance of the {@link SigarSamplerFactory} which
+         * uses the singleton instance of the {@link WriterController retrieved via
+         * {@link WriterController#getInstance()}.
+         *
+         * The size of the internal thread pool used to serve the sensing and
+         * logging jobs is set to {@link SigarSamplerFactory#DEFAULT_EXECUTOR_THREAD_POOL_SIZE}.
+         */
+        public final static SigarSamplerFactory getInstance() {
+                return LazyHolder.INSTANCE;
+        }
+
+        /**
+         * {@link SigarProxy} instance used to retrieve the data to be logged.
+         */
+        private final SigarProxy sigar;
+
+        /**
+         * {@link SigarProxy} instance used by this {@link SigarSamplerFactory}.
+         *
+         * @return the sigar
+         */
+        public final SigarProxy getSigar() {
+                return this.sigar;
+        }
+
+        /**
+         * Used by {@link #getInstance()} to construct the singleton instance.
+         */
+        private SigarSamplerFactory() {
+                this(new Humidor(new Sigar()));
+        }
+
+        /**
+         * Constructs a {@link SigarSamplerFactory} with the given parameters.
+         *
+         * @param humidor
+         */
+        public SigarSamplerFactory(final Humidor humidor) {
+                this.sigar = humidor.getSigar();
+        }
+
+        @Override
+        public CPUsCombinedPercSampler createSensorCPUsCombinedPerc() {
+                return new CPUsCombinedPercSampler(this.sigar);
+        }
+
+        @Override
+        public CPUsDetailedPercSampler createSensorCPUsDetailedPerc() {
+                return new CPUsDetailedPercSampler(this.sigar);
+        }
+
+        @Override
+        public MemSwapUsageSampler createSensorMemSwapUsage() {
+                return new MemSwapUsageSampler(this.sigar);
+        }
+       
+        /**
+         * SINGLETON
+         */
+        private final static class LazyHolder {
+                private static final SigarSamplerFactory INSTANCE = new SigarSamplerFactory();
+        }
+}
