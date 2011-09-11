a3403f1d49261c4ecc516077e0ae0136eb330f3e
André van Hoorn <avh@informatik.uni-kiel.de>
2011-09-11 16:31:25 +0000

    removed TODO (#160)

--- src/tools/kieker/tools/loggingTimestampConverter/LoggingTimestampConverterTool.java
+++ src/tools/kieker/tools/loggingTimestampConverter/LoggingTimestampConverterTool.java
@@ -53,13 +53,11 @@ public class LoggingTimestampConverterTool {
 	private static final String CMD_OPT_NAME__TIMESTAMPS = "timestamps";
 
 	static {
-		initializeOptions();
+		LoggingTimestampConverterTool.initializeOptions();
 	}
 
 	@SuppressWarnings("static-access")
 	private final static void initializeOptions() {
-		// TODO: OptionGroups?
-		// See ticket http://samoa.informatik.uni-kiel.de:8000/kieker/ticket/160
 		LoggingTimestampConverterTool.options.add(OptionBuilder.withLongOpt(LoggingTimestampConverterTool.CMD_OPT_NAME__TIMESTAMPS)
 				.withArgName("timestamp1 ... timestampN").hasArgs().isRequired(true).withDescription("List of timestamps (UTC timezone) to convert")
 				.create("t"));
