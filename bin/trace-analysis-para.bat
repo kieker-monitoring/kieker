ea8cbf036309b8ccfa0f8cda525b4c33ea075dff
Jan Waller <jwa@informatik.uni-kiel.de>
2011-09-12 14:23:01 +0000

    #84

--- bin/trace-analysis-para.bat
+++ bin/trace-analysis-para.bat
@@ -3,7 +3,7 @@ REM This script should NOT be called directly. Use trace-analysis.bat instead.
 REM
 REM @author Nils Christian Ehmke
 
-SET JAVAARGS="-Dlog4j.configuration=./log4j.properties -Xms56m -Xmx1024m"
+SET JAVAARGS=-Dlog4j.configuration=./log4j.properties -Xms56m -Xmx1024m
 SET MAINCLASSNAME=kieker.tools.traceAnalysis.TraceAnalysisTool
 
 REM Get the directory of this file and change the working directory to it.
@@ -13,17 +13,17 @@ REM Set every variable we will need for the execution.
 SET BINDIR=%cd%
 
 SET CLASSPATH=%BINDIR%
-
 for /F "delims=" %%i in ('dir /B /S "%BINDIR%\..\lib\*.jar"') do (
 SET CLASSPATH=!CLASSPATH!;%%i
 )
 for /F "delims=" %%i in ('dir /B /S "%BINDIR%\..\dist\*.jar"') do (
 SET CLASSPATH=!CLASSPATH!;%%i
 )
+SET CLASSPATH=!CLASSPATH!
 
 REM Now start the tool, but don't forget to deliver the parameters.
 java %JAVAARGS% -cp "%CLASSPATH%" %MAINCLASSNAME% %*
 
 REM Don't close the window immediately.
 @echo on
-PAUSE
+@PAUSE
