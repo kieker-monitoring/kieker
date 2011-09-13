85dbc85e86cb1da8e1490d101d5981766b2301e6
André van Hoorn <avh@informatik.uni-kiel.de>
2011-09-13 07:45:10 +0000

    removed uncommented code (#237)

--- test/monitoring/kieker/test/monitoring/aspectJ/compileTimeWeaving/bookstore/synchron/Catalog.java
+++ test/monitoring/kieker/test/monitoring/aspectJ/compileTimeWeaving/bookstore/synchron/Catalog.java
@@ -23,35 +23,29 @@ package kieker.test.monitoring.aspectJ.compileTimeWeaving.bookstore.synchron;
 import kieker.monitoring.annotation.OperationExecutionMonitoringProbe;
 
 /**
- * A simple test and demonstration scenario for Kieker's 
- * monitoring component.
+ * A simple test and demonstration scenario for Kieker's monitoring component.
  * 
- * THIS VARIANT IS nearly identical TO kieker.tests.compileTimeWeaving.bookstore.Catalog.
- * Method getBook is synchronized here. This allows to test the (negative) performance 
- * influence of synchronized method invocation.
- *
- * @author Matthias Rohr
- * History:
- * 2008/20/10: Created this variant based on kieker.tests.compileTimeWeaving.bookstore.Catalog
- * 2008/01/09: Refactoring for the first release of
- *             Kieker and publication under an open source licence
- * 2007-04-18: Initial version
- *
+ * THIS VARIANT IS nearly identical TO
+ * kieker.tests.compileTimeWeaving.bookstore.Catalog. Method getBook is
+ * synchronized here. This allows to test the (negative) performance influence
+ * of synchronized method invocation.
+ * 
+ * @author Matthias Rohr History: 2008/20/10: Created this variant based on
+ *         kieker.tests.compileTimeWeaving.bookstore.Catalog 2008/01/09:
+ *         Refactoring for the first release of Kieker and publication under an
+ *         open source licence 2007-04-18: Initial version
+ * 
  */
 public class Catalog {
-    
-    @OperationExecutionMonitoringProbe()
-    public static synchronized void getBook(boolean complexQuery){
-    	if (complexQuery) {
-    	// See ticket http://samoa.informatik.uni-kiel.de/kieker/trac/ticket/237
-		//System.out.println("  complex query");
-		Bookstore.waitabit(20);
-	}
-	else 	{			
-		//System.out.println("  simple query"); 
-		Bookstore.waitabit(2);	
-	}
 
-    }
-   
+	@OperationExecutionMonitoringProbe()
+	public static synchronized void getBook(final boolean complexQuery) {
+		if (complexQuery) {
+			// complex query;
+			Bookstore.waitabit(20);
+		} else {
+			// simple query;
+			Bookstore.waitabit(2);
+		}
+	}
 }
