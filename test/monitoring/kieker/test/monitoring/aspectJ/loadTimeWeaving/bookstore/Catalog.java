2fefd57242a2da0c4b0eb8e2de7988e01b06cc15
André van Hoorn <avh@informatik.uni-kiel.de>
2011-09-13 07:48:33 +0000

    removed uncommented code (#239)

--- test/monitoring/kieker/test/monitoring/aspectJ/loadTimeWeaving/bookstore/Catalog.java
+++ test/monitoring/kieker/test/monitoring/aspectJ/loadTimeWeaving/bookstore/Catalog.java
@@ -20,33 +20,24 @@
 
 package kieker.test.monitoring.aspectJ.loadTimeWeaving.bookstore;
 
-
 /**
- * A simple test and demonstration scenario for Kieker's 
- * monitoring component.
- *
- * @author Matthias Rohr
- * History:
- * 2008/01/09: Refactoring for the first release of
- *             Kieker and publication under an open source licence
- * 2007-04-18: Initial version
- *
+ * A simple test and demonstration scenario for Kieker's monitoring component.
+ * 
+ * @author Matthias Rohr History: 2008/01/09: Refactoring for the first release
+ *         of Kieker and publication under an open source licence 2007-04-18:
+ *         Initial version
+ * 
  */
 
-
 public class Catalog {
-    
-    public static void getBook(boolean complexQuery){
-    	if (complexQuery) {
-//		System.out.println("  complex query "+Thread.currentThread().getName());
-    	//See ticket http://samoa.informatik.uni-kiel.de/kieker/trac/ticket/239
-		Bookstore.waitabit(20);
-	}
-	else 	{			
-//		System.out.println("  simple query "+Thread.currentThread().getName());
-		Bookstore.waitabit(2);	
-	}
 
-    }
-   
+	public static void getBook(final boolean complexQuery) {
+		if (complexQuery) {
+			// complex query
+			Bookstore.waitabit(20);
+		} else {
+			// simple query
+			Bookstore.waitabit(2);
+		}
+	}
 }
