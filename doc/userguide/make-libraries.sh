f8d2ebc8fad5c2b15931bb2808e41d1836b57fa2
Reiner Jung <rju@informatik.uni-kiel.de>
2011-09-28 18:57:49 +0000

    fixed listings and added new script to generate the Libraries.tex file (preparation to include license info)

--- doc/userguide/make-libraries.sh
+++ doc/userguide/make-libraries.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
 
-java -jar make-libraries.jar libraries_descriptions ../../lib/ Libraries.tex
+DESC="libraries-descriptions.txt"
+LIBRARY_DIR="../../lib" 
+TEXFILE="Libraries.tex"
+
+cat > $TEXFILE << EOF
+\begin{center}
+\begin{longtable}{|p{0.4\textwidth}|p{0.5\textwidth}|}
+\hline 
+Filename & Description\\\\
+\hline
+EOF
+
+for I in `cat libraries-descriptions.txt | sort | grep -E '^\w.*\t.*$' | sed 's/\t/%18/' | sed s/\ /%20/g` ; do
+	J=`echo $I | sed 's/%20/\ /g'`
+	LIBPATTERN=`echo $J | sed 's/^\(\w.*\)%18.*/\1/' | sed 's/\\$/\*/'`
+	LIBDESC=`echo $J | sed 's/^\w.*%18\(.*\)/\1/'`
+	LIBPATH=`find $LIBRARY_DIR -name "$LIBPATTERN" | head -1`
+	if [ "$LIBPATH" != "" ] ; then
+		LIBNAME=`basename $LIBPATH`
+		echo $LIBNAME
+		cat >> $TEXFILE << EOF
+\hline 
+$LIBNAME & $LIBDESC\\\\
+EOF
+	fi
+done
+
+cat >> $TEXFILE << EOF
+\hline 
+\end{longtable}
+\label{tabular:libraries}
+\end{center}
+EOF
+
+# end
