e6620ca896f5c423348e26e06844c59124bcfc19
André van Hoorn <avh@informatik.uni-kiel.de>
2011-10-12 13:44:45 +0000

    moved Sigar native libraries to lib/sigar-native-libs/ #306; updated writer figure in user guide

--- src/common/kieker/common/util/Version.java
+++ src/common/kieker/common/util/Version.java
@@ -46,7 +46,7 @@ public final class Version {
 	 * The VERSION string is updated by the Ant build file, which looks for the
 	 * pattern: VERSION = <quote>.*<quote>
 	 */
-	private static final String VERSION = "1.4-RC2-20111011"; // NOPMD
+	private static final String VERSION = "1.4-RC2-20111012"; // NOPMD
 	private static final String COPYRIGHT = "Copyright (c) 2006-2011 Kieker Project";
 
 	private Version() {}
