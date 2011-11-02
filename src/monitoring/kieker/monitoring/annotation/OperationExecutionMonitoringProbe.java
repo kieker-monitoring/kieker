696a7e216d140696618b6d63c003a08f62c1deae
Jan Waller <jwa@informatik.uni-kiel.de>
2011-11-02 14:30:56 +0000

    minor formatting

--- src/monitoring/kieker/monitoring/annotation/OperationExecutionMonitoringProbe.java
+++ src/monitoring/kieker/monitoring/annotation/OperationExecutionMonitoringProbe.java
@@ -29,8 +29,7 @@ import java.lang.annotation.Target;
  * @author Matthias Rohr, Thilo Focke
  * 
  *         History:
- *         2008/01/14: Refactoring for the first release of
- *         Kieker and publication under an open source licence
+ *         2008/01/14: Refactoring for the first release of Kieker and publication under an open source licence
  *         2007/03/05: New Prototype
  */
 
