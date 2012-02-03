0fdd516b4ffad79af45b2f90e962e545819237f8
Jan Waller <jwa@informatik.uni-kiel.de>
2012-02-03 15:22:25 +0100

    correction on CS check

--- src/common/kieker/common/util/Version.java
+++ src/common/kieker/common/util/Version.java
@@ -46,7 +46,7 @@ public final class Version {
 	 * The VERSION string is updated by the Ant build file, which looks for the
 	 * pattern: VERSION = <quote>.*<quote>
 	 */
-	private static final String VERSION = "1.5-SNAPSHOT-20120109"; // NOPMD
+	private static final String VERSION = "1.5-SNAPSHOT-20120203"; // NOPMD
 	private static final String COPYRIGHT = "Copyright (c) 2006-2012 Kieker Project";
 
 	private Version() {}
