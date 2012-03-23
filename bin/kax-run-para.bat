006ea0f394208d01f87d97907df4dd92d9f5e8ee
Jan Waller <jwa@informatik.uni-kiel.de>
2012-03-23 10:21:03 +0100

    Run scripts

---
+++ bin/kax-run-para.bat
@@ -0,0 +1,28 @@
+@echo off 
+REM This script should NOT be called directly. Use kax-viz.bat instead.
+REM
+REM @author Nils Christian Ehmke
+
+SET JAVAARGS=-Dlog4j.configuration=./log4j.properties -Xms56m -Xmx1024m
+SET MAINCLASSNAME=kieker.tools.KaxRun
+
+REM Get the directory of this file and change the working directory to it.
+cd %~dp0
+
+REM Set every variable we will need for the execution.
+SET BINDIR=%cd%
+
+SET CLASSPATH=%BINDIR%
+for /F "delims=" %%i in ('dir /B /S "%BINDIR%\..\lib\*.jar"') do (
+SET CLASSPATH=!CLASSPATH!;%%i
+)
+for /F "delims=" %%i in ('dir /B /S "%BINDIR%\..\dist\*.jar"') do (
+SET CLASSPATH=!CLASSPATH!;%%i
+)
+
+REM Now start the tool, but don't forget to deliver the parameters.
+java %JAVAARGS% -cp "%CLASSPATH%" %MAINCLASSNAME% %*
+
+REM Don't close the window immediately.
+@echo on
+@PAUSE
---
+++ bin/kax-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# Wrapper script needs improvement!
+#
+# @author Andre van Hoorn
+
+BINDIR=$(cd "$(dirname "$0")"; pwd)/
+
+JAVAARGS="-Dlog4j.configuration=./log4j.properties -Xms56m -Xmx1024m"
+MAINCLASSNAME=kieker.tools.KaxRun
+CLASSPATH=$(ls "${BINDIR}/../lib/"*.jar | tr "\n" ":")$(ls "${BINDIR}/../dist/"*.jar | tr "\n" ":")${BINDIR}
+
+#echo java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} "$@"
+
+time java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} "$@"
