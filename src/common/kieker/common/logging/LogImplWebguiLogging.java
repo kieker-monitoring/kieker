82a88d718be6e4c0a52f4598bdf2a441854e8bca
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2012-04-23 14:21:55 +0200

    Started with the implementation of the Webgui-Logger.

--- src/common/kieker/common/logging/LogImplWebguiLogging.java
+++ src/common/kieker/common/logging/LogImplWebguiLogging.java
@@ -20,49 +20,105 @@
 
 package kieker.common.logging;
 
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
- * @author Jan Waller, ...
+ * This is a simple logger for the Kieker WebGUI. It stores the log messages within a buffer of a static size. If the buffer is full, the oldest entry will be
+ * removed for new entries. As the entries have to be accessible from outside, the queues are stored statically.
+ * 
+ * @author Jan Waller, Nils Christian Ehmke
  */
 public final class LogImplWebguiLogging implements Log {
 
+	private final String name;
+	private static final int MAX_ENTRIES = 100;
+	private static final HashMap<String, Queue<String>> queues = new HashMap<String, Queue<String>>();
+
 	protected LogImplWebguiLogging(final String name) {
-		// ...
+		this.name = name;
 	}
 
 	public boolean isDebugEnabled() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public void debug(final String message) {
-		// TODO Auto-generated method stub
+		// Ignore
 	}
 
 	public void debug(final String message, final Throwable t) {
-		// TODO Auto-generated method stub
+		// Ignore
 	}
 
 	public void info(final String message) {
-		// TODO Auto-generated method stub
+		this.addMessage("[Info] " + message);
 	}
 
 	public void info(final String message, final Throwable t) {
-		// TODO Auto-generated method stub
+		this.addMessage("[Info] " + message);
 	}
 
 	public void warn(final String message) {
-		// TODO Auto-generated method stub
+		this.addMessage("[Warn] " + message);
 	}
 
 	public void warn(final String message, final Throwable t) {
-		// TODO Auto-generated method stub
+		this.addMessage("[Warn] " + message);
 	}
 
 	public void error(final String message) {
-		// TODO Auto-generated method stub
+		this.addMessage("[Error] " + message);
 	}
 
 	public void error(final String message, final Throwable t) {
-		// TODO Auto-generated method stub
+		this.addMessage("[Error] " + message);
+	}
+
+	private void addMessage(final String message) {
+		Queue<String> queue;
+
+		synchronized (LogImplWebguiLogging.queues) {
+			// Get the right queue and create it if necessary.
+			queue = LogImplWebguiLogging.queues.get(this.name);
+			if (queue == null) {
+				queue = new ArrayBlockingQueue<String>(MAX_ENTRIES);
+				LogImplWebguiLogging.queues.put(this.name, queue);
+			}
+		}
+
+		synchronized (queue) {
+			// Is the queue full?
+			if (queue.size() >= MAX_ENTRIES) {
+				// Yes, remove the oldest entry.
+				queue.poll();
+			}
+			final String timestamp = new GregorianCalendar().getTime().toString();
+			queue.add(timestamp + ": " + message);
+		}
+	}
+
+	public void clear() {
+		synchronized (LogImplWebguiLogging.queues) {
+			LogImplWebguiLogging.queues.clear();
+		}
+	}
+
+	public static String[] getEntries(final String name) {
+		Queue<String> queue;
+
+		synchronized (LogImplWebguiLogging.queues) {
+			// Get the right queue
+			queue = LogImplWebguiLogging.queues.get(name);
+			if (queue == null) {
+				return new String[0];
+			}
+		}
+
+		synchronized (queue) {
+			return queue.toArray(new String[queue.size()]);
+		}
 	}
 }
