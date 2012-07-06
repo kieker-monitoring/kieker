66ca891b07ef5df0837fc8c8bc6f825c149a78ff
Holger Knoche <holger.knoche@bmiag.de>
2012-07-06 16:40:34 +0200

    Added a common superclass for all graph-producing plugins

---
+++ src/tools/kieker/tools/traceAnalysis/filter/AbstractGraphProducingFilter.java
@@ -0,0 +1,70 @@
+/***************************************************************************
+ * Copyright 2012 by
+ *  + Christian-Albrechts-University of Kiel
+ *    + Department of Computer Science
+ *      + Software Engineering Group 
+ *  and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.tools.traceAnalysis.filter;
+
+import kieker.analysis.plugin.annotation.Plugin;
+import kieker.common.configuration.Configuration;
+import kieker.tools.traceAnalysis.filter.visualization.graph.AbstractGraph;
+
+/**
+ * Abstract superclass for graph-producing filters.
+ * 
+ * @author Holger Knoche
+ * 
+ * @param <G>
+ *            The graph type created by this filter
+ */
+@Plugin
+public abstract class AbstractGraphProducingFilter<G extends AbstractGraph<?, ?, ?>> extends AbstractMessageTraceProcessingFilter {
+
+	public static final String GRAPH_OUTPUT_PORT_NAME = "graphOutput";
+
+	private final G graph;
+
+	public AbstractGraphProducingFilter(final Configuration configuration, final G graph) {
+		super(configuration);
+
+		this.graph = graph;
+	}
+
+	public Configuration getCurrentConfiguration() {
+		return this.configuration;
+	}
+
+	@Override
+	protected Configuration getDefaultConfiguration() {
+		return null;
+	}
+
+	public abstract String getConfigurationName();
+
+	@Override
+	public void terminate(final boolean error) {
+		if (!error) {
+			this.deliver(GRAPH_OUTPUT_PORT_NAME, this.getGraph());
+		}
+	}
+
+	protected G getGraph() {
+		return this.graph;
+	}
+
+}
