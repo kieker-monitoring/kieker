4d56525861b44e752afcaa2c996ee0ebe3d8f2e8
Andre van Hoorn <avh@informatik.uni-kiel.de>
2012-07-26 08:37:24 +0200

    #417: created class MonitoringRecordLoggerFilter (incl. basic impl)

---
+++ src/tools/kieker/tools/logReplayer/MonitoringRecordLoggerFilter.java
@@ -0,0 +1,61 @@
+/***************************************************************************
+ * Copyright 2012 by
+ *  + Christian-Albrechts-University of Kiel
+ *    + Department of Computer Science
+ *      + Software Engineering Group 
+ *  and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.tools.logReplayer;
+
+import kieker.analysis.plugin.annotation.InputPort;
+import kieker.analysis.plugin.filter.AbstractFilterPlugin;
+import kieker.common.configuration.Configuration;
+import kieker.common.record.IMonitoringRecord;
+import kieker.monitoring.core.controller.IMonitoringController;
+import kieker.monitoring.core.controller.MonitoringController;
+
+/**
+ * 
+ * @author Andre van Hoorn
+ * 
+ */
+public class MonitoringRecordLoggerFilter extends AbstractFilterPlugin {
+	public static final String INPUT_PORT_NAME_RECORD = "monitoringRecords";
+
+	private final IMonitoringController monitoringController;
+
+	public MonitoringRecordLoggerFilter(final Configuration configuration) {
+		super(configuration);
+		// TODO: use another controller/configuration?
+		this.monitoringController = MonitoringController.createInstance(configuration);
+	}
+
+	public Configuration getCurrentConfiguration() {
+		final Configuration configuration = new Configuration();
+		return configuration;
+	}
+
+	@Override
+	protected Configuration getDefaultConfiguration() {
+		final Configuration configuration = new Configuration();
+		return configuration;
+	}
+
+	@InputPort(name = INPUT_PORT_NAME_RECORD, description = "Receives records to be passed to the controller", eventTypes = { IMonitoringRecord.class })
+	public final void inputIMonitoringRecord(final IMonitoringRecord record) {
+		this.monitoringController.newMonitoringRecord(record);
+	}
+}
