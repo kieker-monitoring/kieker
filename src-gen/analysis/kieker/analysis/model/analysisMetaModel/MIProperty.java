8d4b558c0ec7be64c60b25be2a02bf25f6121dda
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2012-09-09 11:36:30 +0200

    #564

--- src-gen/analysis/kieker/analysis/model/analysisMetaModel/MIProperty.java
+++ src-gen/analysis/kieker/analysis/model/analysisMetaModel/MIProperty.java
@@ -18,6 +18,7 @@ import org.eclipse.emf.ecore.EObject;
  * <ul>
  *   <li>{@link kieker.analysis.model.analysisMetaModel.MIProperty#getName <em>Name</em>}</li>
  *   <li>{@link kieker.analysis.model.analysisMetaModel.MIProperty#getValue <em>Value</em>}</li>
+ *   <li>{@link kieker.analysis.model.analysisMetaModel.MIProperty#getDescription <em>Description</em>}</li>
  * </ul>
  * </p>
  *
@@ -77,5 +78,31 @@ public interface MIProperty extends EObject {
 	 * @generated
 	 */
 	void setValue(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Description</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Description</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Description</em>' attribute.
+	 * @see #setDescription(String)
+	 * @see kieker.analysis.model.analysisMetaModel.MIAnalysisMetaModelPackage#getProperty_Description()
+	 * @model required="true"
+	 * @generated
+	 */
+	String getDescription();
+
+	/**
+	 * Sets the value of the '{@link kieker.analysis.model.analysisMetaModel.MIProperty#getDescription <em>Description</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Description</em>' attribute.
+	 * @see #getDescription()
+	 * @generated
+	 */
+	void setDescription(String value);
 
 } // MIProperty
