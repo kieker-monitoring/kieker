8091bda612ecec88229fa2f3260110831df3d9d3
Holger Knoche <holger.knoche@bmiag.de>
2012-09-14 15:33:45 +0200

    Added a unit test for a graph-producing plugin and infrastructure for such tests

---
+++ test/tools/kieker/test/tools/util/graph/DependencyGraphTestUtil.java
@@ -0,0 +1,147 @@
+/***************************************************************************
+ * Copyright 2012 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.test.tools.util.graph;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import kieker.analysis.AnalysisController;
+import kieker.analysis.exception.AnalysisConfigurationException;
+import kieker.common.configuration.Configuration;
+import kieker.common.record.controlflow.OperationExecutionRecord;
+import kieker.tools.traceAnalysis.filter.AbstractGraphProducingFilter;
+import kieker.tools.traceAnalysis.filter.AbstractTraceAnalysisFilter;
+import kieker.tools.traceAnalysis.filter.executionRecordTransformation.ExecutionRecordTransformationFilter;
+import kieker.tools.traceAnalysis.filter.traceReconstruction.TraceReconstructionFilter;
+import kieker.tools.traceAnalysis.filter.visualization.dependencyGraph.AbstractDependencyGraph;
+import kieker.tools.traceAnalysis.filter.visualization.dependencyGraph.DependencyGraphNode;
+import kieker.tools.traceAnalysis.filter.visualization.dependencyGraph.WeightedBidirectionalDependencyGraphEdge;
+import kieker.tools.traceAnalysis.filter.visualization.graph.AbstractGraphElement;
+import kieker.tools.traceAnalysis.systemModel.ISystemModelElement;
+import kieker.tools.traceAnalysis.systemModel.repository.SystemModelRepository;
+
+import kieker.test.analysis.util.plugin.reader.SimpleListReader;
+
+/**
+ * This class provides utility functions for dependency graph tests.
+ * 
+ * @author Holger Knoche
+ * 
+ */
+public class DependencyGraphTestUtil {
+
+	/**
+	 * Utility function to create a node lookup table for a given dependency graph.
+	 * 
+	 * @param graph
+	 *            The graph whose nodes shall be indexed
+	 * @return A map which associates the node's identifier (see {@link AbstractGraphElement#getIdentifier()}) to the actual identifier
+	 */
+	public static <T extends ISystemModelElement> Map<String, DependencyGraphNode<T>> createNodeLookupTable(final AbstractDependencyGraph<T> graph) {
+		final Map<String, DependencyGraphNode<T>> map = new HashMap<String, DependencyGraphNode<T>>();
+
+		for (final DependencyGraphNode<T> node : graph.getNodes()) {
+			map.put(node.getIdentifier(), node);
+		}
+
+		return map;
+	}
+
+	/**
+	 * Prepares a test setup (especially the filter structure) to test a given graph-producing filter. The created
+	 * structure contains all necessary plugins to process the given list of operation execution records using the given filter.
+	 * A {@link GraphReceiverPlugin} is attached to the plugin's output port to make the created graph available for
+	 * inspection.
+	 * 
+	 * @param graphProducer
+	 *            The graph-producing filter
+	 * @param inputPortName
+	 *            The input port name that accepts message traces
+	 * @param systemModelRepositoryPortName
+	 *            The repository port's name to which the system model repository must be connected.
+	 *            If the plugin does not need access to the system model repository, this parameter should be {@code null}
+	 * @param executionRecords
+	 *            The execution records that shall be processed
+	 * @return A fully-initialized {@link GraphTestSetup} instance
+	 * @throws AnalysisConfigurationException
+	 *             If the process yields an invalid analysis configuration
+	 */
+	public static GraphTestSetup prepareEnvironmentForProducerTest(final AbstractGraphProducingFilter<?> graphProducer, final String inputPortName,
+			final String systemModelRepositoryPortName, final List<OperationExecutionRecord> executionRecords) throws AnalysisConfigurationException {
+		final AnalysisController analysisController = new AnalysisController();
+
+		final SystemModelRepository systemModelRepository = new SystemModelRepository(new Configuration());
+
+		final SimpleListReader<OperationExecutionRecord> readerPlugin = new SimpleListReader<OperationExecutionRecord>(new Configuration());
+		readerPlugin.addAllObjects(executionRecords);
+
+		final ExecutionRecordTransformationFilter transformationFilter = new ExecutionRecordTransformationFilter(new Configuration());
+		final TraceReconstructionFilter traceReconstructionFilter = new TraceReconstructionFilter(new Configuration());
+		final GraphReceiverPlugin graphReceiver = new GraphReceiverPlugin(new Configuration());
+
+		// Register repositories and plugins
+		analysisController.registerRepository(systemModelRepository);
+		analysisController.registerReader(readerPlugin);
+		analysisController.registerFilter(transformationFilter);
+		analysisController.registerFilter(traceReconstructionFilter);
+		analysisController.registerFilter(graphProducer);
+		analysisController.registerFilter(graphReceiver);
+
+		// Connect repositories
+		analysisController.connect(transformationFilter, AbstractTraceAnalysisFilter.REPOSITORY_PORT_NAME_SYSTEM_MODEL, systemModelRepository);
+		analysisController.connect(traceReconstructionFilter, AbstractTraceAnalysisFilter.REPOSITORY_PORT_NAME_SYSTEM_MODEL, systemModelRepository);
+		if (systemModelRepositoryPortName != null) {
+			analysisController.connect(graphProducer, systemModelRepositoryPortName, systemModelRepository);
+		}
+
+		// Connect plugins
+		analysisController.connect(readerPlugin, SimpleListReader.OUTPUT_PORT_NAME,
+				transformationFilter, ExecutionRecordTransformationFilter.INPUT_PORT_NAME_RECORDS);
+		analysisController.connect(transformationFilter, ExecutionRecordTransformationFilter.OUTPUT_PORT_NAME_EXECUTIONS,
+				traceReconstructionFilter, TraceReconstructionFilter.INPUT_PORT_NAME_EXECUTIONS);
+		analysisController.connect(traceReconstructionFilter, TraceReconstructionFilter.OUTPUT_PORT_NAME_MESSAGE_TRACE,
+				graphProducer, inputPortName);
+		analysisController.connect(graphProducer, graphProducer.getGraphOutputPortName(),
+				graphReceiver, GraphReceiverPlugin.INPUT_PORT_NAME_GRAPHS);
+
+		return new GraphTestSetup(analysisController, graphReceiver);
+	}
+
+	/**
+	 * Utility function to test whether an edge with the given target weight exists between the given nodes.
+	 * 
+	 * @param source
+	 *            The expected source node
+	 * @param expectedWeight
+	 *            The expected target weight of the edge
+	 * @param target
+	 *            The expected target node
+	 * @return {@code True} if such an edge exists, {@code false} otherwise
+	 */
+	public static boolean dependencyEdgeExists(final DependencyGraphNode<?> source, final int expectedWeight, final DependencyGraphNode<?> target) {
+		boolean edgeFound = false;
+
+		for (final WeightedBidirectionalDependencyGraphEdge<?> edge : source.getOutgoingEdges()) {
+			if (edge.getTarget().equals(target) && (edge.getTargetWeight().get() == expectedWeight)) {
+				edgeFound = true;
+			}
+		}
+
+		return edgeFound;
+	}
+}
---
+++ test/tools/kieker/test/tools/util/graph/GraphReceiverPlugin.java
@@ -0,0 +1,96 @@
+/***************************************************************************
+ * Copyright 2012 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.test.tools.util.graph;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import kieker.analysis.plugin.annotation.InputPort;
+import kieker.analysis.plugin.annotation.Plugin;
+import kieker.analysis.plugin.filter.AbstractFilterPlugin;
+import kieker.common.configuration.Configuration;
+import kieker.tools.traceAnalysis.filter.visualization.graph.AbstractGraph;
+
+/**
+ * A plugin which receives one or more graphs and stores them for later access. This plugin is primarily
+ * intended for unit tests of graph-producing plugins.
+ * 
+ * @author Holger Knoche
+ * 
+ */
+@Plugin(description = "A plugin that receives one or more graphs and provides access them via methods.")
+public class GraphReceiverPlugin extends AbstractFilterPlugin {
+
+	/**
+	 * The name of the input port which receives graphs.
+	 */
+	public static final String INPUT_PORT_NAME_GRAPHS = "inputGraph";
+
+	private final List<AbstractGraph<?, ?, ?>> receivedGraphs = new CopyOnWriteArrayList<AbstractGraph<?, ?, ?>>();
+
+	/**
+	 * Creates a new receiver plugin with the given configuration.
+	 * 
+	 * @param configuration
+	 *            The configuration to use
+	 */
+	public GraphReceiverPlugin(final Configuration configuration) {
+		super(configuration);
+	}
+
+	public Configuration getCurrentConfiguration() {
+		return null;
+	}
+
+	@InputPort(name = INPUT_PORT_NAME_GRAPHS,
+			description = "Receives graphs to store them",
+			eventTypes = { AbstractGraph.class })
+	public void receiveGraph(final AbstractGraph<?, ?, ?> graph) {
+		this.receivedGraphs.add(graph);
+	}
+
+	/**
+	 * Returns the number of graphs received so far.
+	 * 
+	 * @return See above
+	 */
+	public int getNumberOfReceivedGraphs() {
+		return this.receivedGraphs.size();
+	}
+
+	/**
+	 * Returns the first received graph, if any.
+	 * 
+	 * @return See above
+	 */
+	public <T extends AbstractGraph<?, ?, ?>> T getFirstGraph() {
+		return this.getGraphAt(0);
+	}
+
+	/**
+	 * Returns the ({@code index} + 1)th received graph, if any.
+	 * 
+	 * @param index
+	 *            The (zero-based) index of the desired graph
+	 * @return See above
+	 */
+	@SuppressWarnings("unchecked")
+	public <T extends AbstractGraph<?, ?, ?>> T getGraphAt(final int index) {
+		return (T) this.receivedGraphs.get(index);
+	}
+
+}
