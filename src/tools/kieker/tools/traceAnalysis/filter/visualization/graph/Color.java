cd5d0bd0700359c20d00f4ef6c7c29c6c0659a10
Holger Knoche <holger.knoche@bmiag.de>
2012-09-14 19:00:10 +0200

    Small fix to avoid RGB values >= 2^24

--- src/tools/kieker/tools/traceAnalysis/filter/visualization/graph/Color.java
+++ src/tools/kieker/tools/traceAnalysis/filter/visualization/graph/Color.java
@@ -63,7 +63,7 @@ public class Color {
 	 *            The color's blue value
 	 */
 	public Color(final byte red, final byte green, final byte blue) {
-		this.rgb = (red << 16) | (green << 8) | blue;
+		this.rgb = ((red << 16) | (green << 8) | blue) & 0x00FFFFFF;
 	}
 
 	/**
