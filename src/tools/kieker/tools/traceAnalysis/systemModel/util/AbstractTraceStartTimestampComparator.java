b94d1f81539d70758b6dc7a9e17ee7fe1f74809d
Holger Knoche <holger.knoche@bmiag.de>
2012-11-01 00:04:08 +0100

    Added some useful comparators for abstract traces

---
+++ src/tools/kieker/tools/traceAnalysis/systemModel/util/AbstractTraceStartTimestampComparator.java
@@ -0,0 +1,46 @@
+/***************************************************************************
+ * Copyright 2012 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.tools.traceAnalysis.systemModel.util;
+
+import java.util.Comparator;
+
+import kieker.tools.traceAnalysis.systemModel.AbstractTrace;
+
+/**
+ * Comparator which compares abstract traces according to their start timestamps.
+ * 
+ * @author Holger Knoche
+ * 
+ */
+public class AbstractTraceStartTimestampComparator implements Comparator<AbstractTrace> {
+
+	public int compare(final AbstractTrace o1, final AbstractTrace o2) {
+		final long startTimestamp1 = o1.getStartTimestamp();
+		final long startTimestamp2 = o2.getStartTimestamp();
+
+		if (startTimestamp1 == startTimestamp2) {
+			return 0;
+		}
+		else if (startTimestamp1 < startTimestamp2) {
+			return -1;
+		}
+		else {
+			return 1;
+		}
+	}
+
+}
