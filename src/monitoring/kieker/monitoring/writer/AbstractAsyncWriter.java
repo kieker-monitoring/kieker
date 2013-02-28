78d952cad962714008c60357f9bc08f32addd01f
Jan Waller <jwa@informatik.uni-kiel.de>
2013-02-28 11:10:29 +0100

    improved QueueFull behavior for AsyncMonitoringWriter

--- src/monitoring/kieker/monitoring/writer/AbstractAsyncWriter.java
+++ src/monitoring/kieker/monitoring/writer/AbstractAsyncWriter.java
@@ -121,22 +121,32 @@ public abstract class AbstractAsyncWriter extends AbstractMonitoringWriter {
 		try {
 			switch (this.queueFullBehavior) {
 			case 1: // blocks when queue full
+				boolean interrupted = false;
 				for (int i = 0; i < 10; i++) { // drop out if more than 10 times interrupted
 					try {
 						this.blockingQueue.put(monitoringRecord);
+						if (interrupted) {
+							LOG.warn("Interupted when adding new monitoring record to queue. Tries: " + i);
+							Thread.currentThread().interrupt(); // propagate interrupt
+						}
 						return true;
 					} catch (final InterruptedException ignore) {
-						LOG.warn("Interupted when adding new monitoring record to queue. Try: " + i);
+						interrupted = true;
 					}
 				}
 				return false;
 			case 2: // does nothing if queue is full
 				if (!this.blockingQueue.offer(monitoringRecord) && ((this.missedRecords.getAndIncrement() % 1000) == 0)) { // NOCS
-					LOG.warn("Queue is full, dropping records.");
+					LOG.warn("Queue is full, dropping records. Number of already dropped records: " + this.missedRecords.get());
 				}
 				return true;
 			default: // tries to add immediately (error if full)
-				this.blockingQueue.add(monitoringRecord);
+				try {
+					this.blockingQueue.add(monitoringRecord);
+				} catch (final IllegalStateException ex) {
+					LOG.error("Failed to add new monitoring record to queue. Queue is full. Either increase 'QueueSize' or change 'QueueFullBehavior' for the configured writer.");
+					return false;
+				}
 				return true;
 			}
 		} catch (final Exception ex) { // NOPMD NOCS (IllegalCatchCheck)
