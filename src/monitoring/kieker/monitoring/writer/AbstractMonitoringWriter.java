553cd087e0d73f37481d8063470e991aa406e7f1
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2013-04-19 13:40:38 +0200

    Minor modifications for quality reasons.

--- src/monitoring/kieker/monitoring/writer/AbstractMonitoringWriter.java
+++ src/monitoring/kieker/monitoring/writer/AbstractMonitoringWriter.java
@@ -31,8 +31,9 @@ import kieker.monitoring.core.controller.IMonitoringController;
 public abstract class AbstractMonitoringWriter implements IMonitoringWriter {
 	private static final Log LOG = LogFactory.getLog(AbstractMonitoringWriter.class);
 
-	private final Configuration configuration;
+	/** The controller of this writer. */
 	protected IMonitoringController monitoringController;
+	private final Configuration configuration;
 
 	/**
 	 * 
