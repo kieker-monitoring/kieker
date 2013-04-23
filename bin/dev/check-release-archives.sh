d3cf3d5720ab6e2e8e867c91e74d66d9b2101efa
Andre van Hoorn <van.hoorn@informatik.uni-stuttgart.de>
2013-04-23 15:27:35 +0200

    #885: fixes to JavaEE example

--- bin/dev/check-release-archives.sh
+++ bin/dev/check-release-archives.sh
@@ -231,7 +231,7 @@ function assert_files_exist_bin {
 	assert_file_exists_regular "examples/userguide/appendix-Sigar/lib/sigar-"*".lib"
 	
 	assert_file_exists_regular "examples/JavaEEServletContainerExample/jetty-hightide-jpetstore/kieker.monitoring.properties"
-	assert_file_exists_regular "examples/JavaEEServletContainerExample/jetty-hightide-jpetstore/webapps/jpetstore/WEB-INF/lib/kieker-"*".jar"
+	assert_file_exists_regular "examples/JavaEEServletContainerExample/jetty-hightide-jpetstore/webapps/jpetstore/WEB-INF/lib/kieker-"*"_aspectj.jar"
 	assert_file_NOT_exists "lib/static-analysis/"
 	assert_file_NOT_exists "dist/release/"
 	assert_file_NOT_exists "bin/dev/"
