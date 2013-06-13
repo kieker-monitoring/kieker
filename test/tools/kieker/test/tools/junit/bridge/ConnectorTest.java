dc5d6c294334a650ab7eb610a46555994c5197a0
Pascale Brandt <pbr@informatik.uni-kiel.de>
2013-06-13 14:19:41 +0200

    use Rule for creation of tempFolder for records

--- test/tools/kieker/test/tools/junit/bridge/ConnectorTest.java
+++ test/tools/kieker/test/tools/junit/bridge/ConnectorTest.java
@@ -6,7 +6,9 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import kieker.common.configuration.Configuration;
 import kieker.monitoring.core.configuration.ConfigurationFactory;
@@ -25,6 +27,9 @@ public class ConnectorTest extends AbstractKiekerTest {
 
 	public ConnectorTest() {}
 
+	@Rule
+	public final TemporaryFolder tmpFolder = new TemporaryFolder();
+
 	@Test
 	public void firstTest() throws IOException {
 
@@ -35,15 +40,15 @@ public class ConnectorTest extends AbstractKiekerTest {
 		 * the second part starts a new record which is written in the TestServiceConnector class.
 		 * 
 		 */
-		final String path = "/tmp/kdb/";
-		final File dir = new File(path);
+
+		final File path = this.tmpFolder.getRoot();
 		int rows = 0;
 
-		Assert.assertTrue("Directory could not created", dir.mkdir() == true);
+		// Assert.assertTrue("Directory could not created", dir.mkdir() == false);
 
 		final Configuration configuration = ConfigurationFactory.createDefaultConfiguration();
 		configuration.setProperty("kieker.monitoring.writer.filesystem.AsyncFsWriter.storeInJavaIoTmpdir", "false");
-		configuration.setProperty("kieker.monitoring.writer.filesystem.AsyncFsWriter.customStoragePath", path);
+		configuration.setProperty("kieker.monitoring.writer.filesystem.AsyncFsWriter.customStoragePath", path.getAbsolutePath());
 
 		final ServiceContainer serviceContainer = new ServiceContainer(configuration,
 				new TestServiceConnector(this));
@@ -58,9 +63,9 @@ public class ConnectorTest extends AbstractKiekerTest {
 		try {
 			final FileReader fr = new FileReader("");
 			final BufferedReader br = new BufferedReader(fr);
-			String line;
+			final String line = br.readLine();
 
-			while ((line = br.readLine()) != null) {
+			while (line != null) {
 				rows++;
 			}
 			rows = rows - 1;
@@ -75,16 +80,18 @@ public class ConnectorTest extends AbstractKiekerTest {
 				TestServiceConnector.SEND_NUMBER_OF_RECORDS == rows);
 
 		// Delete iterativ every element from the created folder above
-		if (dir.exists())
-		{
-			for (final File f : dir.listFiles())
-			{
-				f.delete();
-			}
-			dir.delete();
-		}
+		/*
+		 * if (dir.exists())
+		 * {
+		 * for (final File f : dir.listFiles())
+		 * {
+		 * f.delete();
+		 * }
+		 * dir.delete();
+		 * }
+		 */
 
-		Assert.assertTrue("Directory is not cleaned", path.isEmpty() == true);
+		Assert.assertTrue("Directory is not cleaned", this.tmpFolder.getRoot().exists() == true);
 	}
 
 	/**
