cd9f33a993a49b0ef09f4321460bb054d79bd677
Reiner Jung <rju@informatik.uni-kiel.de>
2013-07-24 18:26:19 +0200

    fixed TCPClientForServer for TCPSingleServer test

--- test/tools/kieker/test/tools/junit/bridge/TCPClientforServer.java
+++ test/tools/kieker/test/tools/junit/bridge/TCPClientforServer.java
@@ -11,51 +11,45 @@ public class TCPClientforServer implements Runnable {
 
 	public void run() {
 
-		Socket connectionSocket;
-		try {
-			connectionSocket = new Socket(ConfigurationParameters.HOSTNAME, ConfigurationParameters.PORT);
-			while (!connectionSocket.isConnected()) {
-				try {
-					connectionSocket = new Socket(ConfigurationParameters.HOSTNAME, ConfigurationParameters.PORT);
-				} catch (final UnknownHostException e) {
-					try {
-						Thread.sleep(1000);
-					} catch (final InterruptedException e1) {
-					}
-				} catch (final IOException e) {
-					try {
-						Thread.sleep(1000);
-					} catch (final InterruptedException e1) {
-					}
-				}
-			}
+		boolean connected = false;
+
+		while (!connected) {
 			try {
-				final DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+				final Socket connectionSocket = new Socket(ConfigurationParameters.HOSTNAME, ConfigurationParameters.PORT);
+				connected = true;
+				try {
+					final DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-				for (int i = 0; i < ConfigurationParameters.SEND_NUMBER_OF_RECORDS; i++) {
-					outToClient.writeInt(1); // ID of test record type
-					outToClient.writeInt(ConfigurationParameters.TEST_OPERATION_SIGNATURE.length());
-					outToClient.writeBytes(ConfigurationParameters.TEST_OPERATION_SIGNATURE);
-					outToClient.writeInt(ConfigurationParameters.TEST_SESSION_ID.length());
-					outToClient.writeBytes(ConfigurationParameters.TEST_SESSION_ID);
-					outToClient.writeLong(ConfigurationParameters.TEST_TRACE_ID);
-					outToClient.writeLong(ConfigurationParameters.TEST_TIN);
-					outToClient.writeLong(ConfigurationParameters.TEST_TOUT);
-					outToClient.writeInt(ConfigurationParameters.TEST_HOSTNAME.length());
-					outToClient.writeBytes(ConfigurationParameters.TEST_HOSTNAME);
-					outToClient.writeInt(ConfigurationParameters.TEST_EOI);
-					outToClient.writeInt(ConfigurationParameters.TEST_ESS);
+					for (int i = 0; i < ConfigurationParameters.SEND_NUMBER_OF_RECORDS; i++) {
+						outToClient.writeInt(1); // ID of test record type
+						outToClient.writeInt(ConfigurationParameters.TEST_OPERATION_SIGNATURE.length());
+						outToClient.writeBytes(ConfigurationParameters.TEST_OPERATION_SIGNATURE);
+						outToClient.writeInt(ConfigurationParameters.TEST_SESSION_ID.length());
+						outToClient.writeBytes(ConfigurationParameters.TEST_SESSION_ID);
+						outToClient.writeLong(ConfigurationParameters.TEST_TRACE_ID);
+						outToClient.writeLong(ConfigurationParameters.TEST_TIN);
+						outToClient.writeLong(ConfigurationParameters.TEST_TOUT);
+						outToClient.writeInt(ConfigurationParameters.TEST_HOSTNAME.length());
+						outToClient.writeBytes(ConfigurationParameters.TEST_HOSTNAME);
+						outToClient.writeInt(ConfigurationParameters.TEST_EOI);
+						outToClient.writeInt(ConfigurationParameters.TEST_ESS);
+					}
+
+					connectionSocket.close();
+
+				} catch (final IOException e) {
+					Assert.assertTrue("Sending data to server failed. " + e.getMessage(), false);
 				}
-
-				connectionSocket.close();
-
+			} catch (final UnknownHostException e) {
+				Assert.assertTrue("Unknown host " + e.getMessage(), false);
 			} catch (final IOException e) {
-				Assert.assertTrue("Connection to Server failed" + e.getMessage(), false);
+				try {
+					Thread.sleep(1000);
+				} catch (final InterruptedException e1) {
+				}
 			}
-		} catch (final UnknownHostException e1) {
-		} catch (final IOException e1) {
-		}
 
+		}
 	}
 
 }
