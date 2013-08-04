c9ad83474638d29dea81feea361265f1c332d899
Reiner Jung <rju@informatik.uni-kiel.de>
2013-08-04 22:12:10 +0200

    readded abstract to AbstractCOnnectorTest

--- test/tools/kieker/test/tools/junit/bridge/AbstractConnectorTest.java
+++ test/tools/kieker/test/tools/junit/bridge/AbstractConnectorTest.java
@@ -34,7 +34,7 @@ import kieker.tools.bridge.connector.IServiceConnector;
  * 
  * @since 1.8
  */
-public class AbstractConnectorTest {
+public abstract class AbstractConnectorTest {
 
 	private IServiceConnector connector;
 	private int recordCount; // default initialization is 0
