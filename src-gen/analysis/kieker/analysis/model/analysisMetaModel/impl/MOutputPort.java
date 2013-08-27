113b17a75e27d9d04a67e000a57648e9216e7d9f
Jan Waller <jwa@informatik.uni-kiel.de>
2013-08-27 11:04:31 +0200

    Update EMF to 2.9.0 (Kepler)

--- src-gen/analysis/kieker/analysis/model/analysisMetaModel/impl/MOutputPort.java
+++ src-gen/analysis/kieker/analysis/model/analysisMetaModel/impl/MOutputPort.java
@@ -85,7 +85,7 @@ public class MOutputPort extends MPort implements MIOutputPort {
 	 */
 	public MIPlugin getParent() {
 		if (eContainerFeatureID() != MIAnalysisMetaModelPackage.OUTPUT_PORT__PARENT) return null;
-		return (MIPlugin)eContainer();
+		return (MIPlugin)eInternalContainer();
 	}
 
 	/**
