89d6d2fdcf7e2021490982cc00920edda815e471
Reiner Jung <rju@informatik.uni-kiel.de>
2013-11-22 15:05:43 +0100

    minor fix in path and script file

--- bin/kieker-data-bridge.sh
+++ bin/kieker-data-bridge.sh
@@ -5,12 +5,12 @@
 #
 # @author Andre van Hoorn
 
-BINDIR=$(cd "$(dirname "$0")"; pwd)/
+BINDIR=$(cd "$(dirname "$0")"; pwd)
 
 JAVAARGS="-Dlog4j.configuration=./log4j.properties -Xms56m -Xmx1024m"
 MAINCLASSNAME=kieker.tools.bridge.cli.CLIServerMain
-CLASSPATH=$(ls "${BINDIR}/../lib/"*.jar | tr "\n" ":")$(ls "${BINDIR}/../dist/"*.jar | tr "\n" ":")${BINDIR}
+CLASSPATH=$(ls "${BINDIR}/../lib/"*.jar | tr "\n" ":")$(ls "${BINDIR}/../dist/"*.jar | tr "\n" ":")${BINDIR}/
 
-#echo java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} "$@"
+echo java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} "$@"
 
 time java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} "$@"
