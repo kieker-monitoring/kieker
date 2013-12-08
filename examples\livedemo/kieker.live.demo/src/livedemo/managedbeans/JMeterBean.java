c244bdc178c9e5dd3087baeb22531c1d6f0c4542
Bjoern Weissenfels <bjw@informatik.uni-kiel.de>
2013-12-08 19:39:44 +0100

    added load generation with jmeter

--- examples\livedemo/kieker.live.demo/src/livedemo/managedbeans/JMeterBean.java
+++ examples\livedemo/kieker.live.demo/src/livedemo/managedbeans/JMeterBean.java
@@ -15,14 +15,33 @@ import javax.faces.bean.ManagedBean;
 @ApplicationScoped
 public class JMeterBean {
 	
-	private void runJMeter(){
+	private boolean disabled;
+	
+	public JMeterBean(){
+		this.disabled = false;
+	}
+	
+	public boolean getDisabled(){
+		return this.disabled;
+	}
+	
+	public void change(){
+		this.disabled = !this.disabled;
+	}
+	
+	public synchronized void runJMeter(){ 
+		this.disabled = true;
+		System.out.println("---disabled=true---");
 		Runtime runtime = Runtime.getRuntime();
-		File dir = new File("/jmeter/bin");
+		File dir = new File(new File("").getAbsolutePath()+"/jmeter/bin");
 		try {
-			runtime.exec("java -jar ApacheJMeter.jar -n -t Testplan.jmx",null,dir);
-		} catch (IOException e) {
+			Process p = runtime.exec("java -jar ApacheJMeter.jar -n -t Testplan.jmx",null,dir);
+			p.waitFor();
+		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
+		this.disabled = false;
+		System.out.println("---disabled=false---" + runtime.availableProcessors());
 	}
 
 }
