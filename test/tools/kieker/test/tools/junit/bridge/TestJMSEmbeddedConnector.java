6980459a3b6040e59ece73ffd39c44e3ec82cabd
Reiner Jung <rju@informatik.uni-kiel.de>
2014-03-17 14:44:21 +0100

    added test for the KDB JMSEmbeddedConnector

--- test/tools/kieker/test/tools/junit/bridge/TestJMSEmbeddedConnector.java
+++ test/tools/kieker/test/tools/junit/bridge/TestJMSEmbeddedConnector.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Copyright 2014 Kieker Project (http://kieker-monitoring.net)
+ * Copyright 2013 Kieker Project (http://kieker-monitoring.net)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,30 +21,31 @@ import kieker.common.configuration.Configuration;
 import kieker.monitoring.core.configuration.ConfigurationFactory;
 import kieker.tools.bridge.connector.ConnectorDataTransmissionException;
 import kieker.tools.bridge.connector.jms.JMSClientConnector;
+import kieker.tools.bridge.connector.jms.JMSEmbeddedConnector;
 
 /**
  * 
  * @author Reiner Jung
  * 
- * @since 1.8
+ * @since 1.9
  */
-public class TestJMSClientConnector extends AbstractConnectorTest {
+public class TestJMSEmbeddedConnector extends AbstractConnectorTest {
 
 	/**
 	 * Empty constructor to satisfy coding style.
 	 */
-	public TestJMSClientConnector() {
+	public TestJMSEmbeddedConnector() {
 		// Nothing to be done.
 	}
 
 	/**
-	 * Test the JMS client connector.
+	 * Test the JMS embedded connector.
 	 * 
 	 * @throws ConnectorDataTransmissionException
 	 *             on lookup failure for the test record
 	 */
 	@Test
-	public void testJMSClientConnector() throws ConnectorDataTransmissionException { // NOPMD
+	public void testJMSEmbeddedConnector() throws ConnectorDataTransmissionException { // NOPMD
 
 		final Thread messageGenerator = new Thread(new JMSMessageGenerator(), "Generator");
 		messageGenerator.start();
@@ -53,11 +54,12 @@ public class TestJMSClientConnector extends AbstractConnectorTest {
 		broker.start();
 
 		final Configuration configuration = ConfigurationFactory.createSingletonConfiguration();
+		configuration.setProperty(JMSEmbeddedConnector.PORT, String.valueOf(ConfigurationParameters.JMS_EMBEDDED_PORT));
 		configuration.setProperty(JMSClientConnector.USERNAME, String.valueOf(ConfigurationParameters.JMS_USERNAME));
 		configuration.setProperty(JMSClientConnector.PASSWORD, String.valueOf(ConfigurationParameters.JMS_PASSWORD));
 		configuration.setProperty(JMSClientConnector.URI, String.valueOf(ConfigurationParameters.JMS_URI));
 		// test the connector
-		this.setConnector(new JMSClientConnector(configuration, this.createLookupEntityMap()));
+		this.setConnector(new JMSEmbeddedConnector(configuration, this.createLookupEntityMap()));
 		this.initialize();
 		this.deserialize(ConfigurationParameters.SEND_NUMBER_OF_RECORDS);
 		this.close(ConfigurationParameters.SEND_NUMBER_OF_RECORDS);
