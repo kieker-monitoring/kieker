6a3c7cc34684b7a53ba0a72b6979f04a0c75dddd
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2014-03-18 17:43:10 +0100

    Added some scripts to start the GUI

---
+++ bin/trace-analysis-gui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# Wrapper script needs improvement!
+#
+# @author Andre van Hoorn
+
+BINDIR=$(cd "$(dirname "$0")"; pwd)/
+
+JAVAARGS="-Dkieker.common.logging.Log=JDK -Djava.util.logging.config.file=${BINDIR}/logging.properties -Xms56m -Xmx1024m"
+MAINCLASSNAME=kieker.tools.traceAnalysis.TraceAnalysisGUI
+CLASSPATH=$(ls "${BINDIR}/../lib/"*.jar | tr "\n" ":")$(ls "${BINDIR}/../dist/"*.jar | tr "\n" ":")${BINDIR}
+
+#echo java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} "$@"
+
+time java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} "$@"
