ea3324a747d75db926f3cad36f10f2848426431a
Christian Wulf <chw@informatik.uni-kiel.de>
2014-04-19 01:33:50 +0200

    extended test for pre-allocated exceptions

--- test/panalysis/de/chw/concurrent/alternative/UntypedExceptionalCircularWorkStealingDequeTest.java
+++ test/panalysis/de/chw/concurrent/alternative/UntypedExceptionalCircularWorkStealingDequeTest.java
@@ -20,7 +20,10 @@ public class UntypedExceptionalCircularWorkStealingDequeTest {
 	@Test
 	public void measureManyEmptyPulls() {
 		final UntypedExceptionalCircularWorkStealingDeque deque = new UntypedExceptionalCircularWorkStealingDeque();
+		System.out.println(UntypedExceptionalCircularWorkStealingDeque.DEQUE_IS_EMPTY_EXCEPTION);
+		System.out.println(UntypedExceptionalCircularWorkStealingDeque.OPERATION_ABORTED_EXCEPTION);
 
+		int counter = 0;
 		final int numIterations = UntypedCircularWorkStealingDequeTest.NUM_ITERATIONS;
 		this.stopWatch.start();
 		for (int i = 0; i < numIterations; i++) {
@@ -28,10 +31,12 @@ public class UntypedExceptionalCircularWorkStealingDequeTest {
 				deque.popBottom();
 			} catch (final DequeIsEmptyException e) {
 				// do not handle; we just want to compare the performance of throwing a preallocated exception vs. returning special values
+				counter++;
 			}
 		}
 		this.stopWatch.end();
 
 		Assert.assertThat(this.stopWatch.getDuration(), OrderingComparison.lessThan(UntypedCircularWorkStealingDequeTest.EXPECTED_DURATION));
+		Assert.assertThat(counter, OrderingComparison.comparesEqualTo(UntypedCircularWorkStealingDequeTest.NUM_ITERATIONS));
 	}
 }
