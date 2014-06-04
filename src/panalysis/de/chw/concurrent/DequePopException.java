643be8bd9d3a58c57fa8da66397cf7ddba885020
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2014-06-04 13:19:16 +0200

    Removed old files and added relocation note.

--- src/panalysis/de/chw/concurrent/DequePopException.java
+++
@@ -1,8 +0,0 @@
-package de.chw.concurrent;
-
-import de.chw.util.StacklessException;
-
-public class DequePopException extends StacklessException {
-	private static final long serialVersionUID = 496512683536868149L;
-
-}
