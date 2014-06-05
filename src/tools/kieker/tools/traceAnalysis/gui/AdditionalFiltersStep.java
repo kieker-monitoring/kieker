7035c5bd82be0bc2e25f0126f9791f8b8b643a02
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2014-06-05 12:29:15 +0200

    Feature: Timestamp filtering (#1256)

--- src/tools/kieker/tools/traceAnalysis/gui/AdditionalFiltersStep.java
+++ src/tools/kieker/tools/traceAnalysis/gui/AdditionalFiltersStep.java
@@ -48,6 +48,10 @@ public class AdditionalFiltersStep extends AbstractStep {
 	private final JTextField selectOnlyTracesInput = new JTextField("1 2 3 4 42");
 	private final JCheckBox filterTraces = new JCheckBox("Filter Traces with Following IDs Out:");
 	private final JTextField filterTracesInput = new JTextField("1 2 3 4 42");
+	private final JCheckBox ignoreBefore = new JCheckBox("Ignore Executions Before the Following Date (yyyyMMdd-HHmmss:");
+	private final JCheckBox ignoreAfter = new JCheckBox("Ignore Executions After the Following Date (yyyyMMdd-HHmmss):");
+	private final JTextField ignoreBeforeInput = new JTextField(Integer.toString(0));
+	private final JTextField ignoreAfterInput = new JTextField(Integer.toString(Integer.MAX_VALUE));
 
 	public AdditionalFiltersStep() {
 		this.addAndLayoutComponents();
@@ -95,6 +99,34 @@ public class AdditionalFiltersStep extends AbstractStep {
 		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
 		this.add(this.filterTracesInput, gridBagConstraints);
 
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		gridBagConstraints.insets.set(5, 5, 5, 5);
+		gridBagConstraints.fill = GridBagConstraints.VERTICAL;
+		gridBagConstraints.gridwidth = 1;
+		gridBagConstraints.weightx = 0.0;
+		this.add(this.ignoreBefore, gridBagConstraints);
+
+		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+		gridBagConstraints.anchor = GridBagConstraints.EAST;
+		gridBagConstraints.insets.set(5, 5, 5, 5);
+		gridBagConstraints.weightx = 0.0;
+		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+		this.add(this.ignoreBeforeInput, gridBagConstraints);
+
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		gridBagConstraints.insets.set(5, 5, 5, 5);
+		gridBagConstraints.fill = GridBagConstraints.VERTICAL;
+		gridBagConstraints.gridwidth = 1;
+		gridBagConstraints.weightx = 0.0;
+		this.add(this.ignoreAfter, gridBagConstraints);
+
+		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+		gridBagConstraints.anchor = GridBagConstraints.EAST;
+		gridBagConstraints.insets.set(5, 5, 5, 5);
+		gridBagConstraints.weightx = 0.0;
+		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+		this.add(this.ignoreAfterInput, gridBagConstraints);
+
 		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
 		gridBagConstraints.weighty = 1.0;
 		gridBagConstraints.fill = GridBagConstraints.VERTICAL;
@@ -141,6 +173,24 @@ public class AdditionalFiltersStep extends AbstractStep {
 				}
 			}
 		});
+
+		this.ignoreAfter.addItemListener(new ItemListener() {
+
+			@Override
+			@SuppressWarnings("synthetic-access")
+			public void itemStateChanged(final ItemEvent e) {
+				AdditionalFiltersStep.this.ignoreAfterInput.setEnabled(AdditionalFiltersStep.this.ignoreAfter.isSelected());
+			}
+		});
+
+		this.ignoreBefore.addItemListener(new ItemListener() {
+
+			@Override
+			@SuppressWarnings("synthetic-access")
+			public void itemStateChanged(final ItemEvent e) {
+				AdditionalFiltersStep.this.ignoreBeforeInput.setEnabled(AdditionalFiltersStep.this.ignoreBefore.isSelected());
+			}
+		});
 	}
 
 	@Override
@@ -159,6 +209,16 @@ public class AdditionalFiltersStep extends AbstractStep {
 			for (final String id : ids) {
 				parameters.add(id);
 			}
+		}
+
+		if (this.ignoreBefore.isSelected()) {
+			parameters.add("--" + Constants.CMD_OPT_NAME_IGNOREEXECUTIONSBEFOREDATE);
+			parameters.add(this.ignoreBeforeInput.getText());
+		}
+
+		if (this.ignoreAfter.isSelected()) {
+			parameters.add("--" + Constants.CMD_OPT_NAME_IGNOREEXECUTIONSAFTERDATE);
+			parameters.add(this.ignoreAfterInput.getText());
 		}
 	}
 
@@ -199,7 +259,13 @@ public class AdditionalFiltersStep extends AbstractStep {
 		this.filterTraces.setSelected(false);
 		this.filterTracesInput.setText("1 2 3 4 42");
 
+		this.ignoreAfter.setSelected(false);
+		this.ignoreBefore.setSelected(false);
+
 		this.selectOnlyTracesInput.setEnabled(false);
 		this.filterTracesInput.setEnabled(false);
+
+		this.ignoreAfterInput.setEnabled(false);
+		this.ignoreBeforeInput.setEnabled(false);
 	}
 }
