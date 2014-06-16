1b4aaeb1ec907b8c23ee0cea5f88b798dd862198
Jan Waller <jwa@informatik.uni-kiel.de>
2014-06-16 10:21:37 +0200

    more object events

---
+++ src/common/kicker/common/record/flow/trace/operation/constructor/object/BeforeConstructorObjectEvent.java
@@ -0,0 +1,150 @@
+/***************************************************************************
+ * Copyright 2014 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kicker.common.record.flow.trace.operation.constructor.object;
+
+import java.nio.BufferOverflowException;
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+
+import kicker.common.record.flow.IObjectRecord;
+import kicker.common.record.flow.trace.operation.constructor.BeforeConstructorEvent;
+import kicker.common.util.registry.IRegistry;
+
+/**
+ * @author Jan Waller
+ * 
+ * @since 1.10
+ */
+public class BeforeConstructorObjectEvent extends BeforeConstructorEvent implements IObjectRecord {
+	public static final int SIZE = (2 * TYPE_SIZE_LONG) + TYPE_SIZE_INT + (2 * TYPE_SIZE_STRING) + TYPE_SIZE_INT;
+	public static final Class<?>[] TYPES = {
+		long.class, // Event.timestamp
+		long.class, // TraceEvent.traceId
+		int.class, // TraceEvent.orderIndex
+		String.class, // OperationEvent.operationSignature
+		String.class, // OperationEvent.classSignature
+		int.class, // objectId
+	};
+
+	private static final long serialVersionUID = 8437428469894463116L;
+
+	private final int objectId;
+
+	/**
+	 * This constructor uses the given parameters to initialize the fields of this record.
+	 * 
+	 * @param timestamp
+	 *            The timestamp of this record.
+	 * @param traceId
+	 *            The trace ID.
+	 * @param orderIndex
+	 *            The order index.
+	 * @param operationSignature
+	 *            The operation signature. This parameter can be null.
+	 * @param classSignature
+	 *            The class signature. This parameter can be null.
+	 * @param objectId
+	 *            The object ID.
+	 */
+	public BeforeConstructorObjectEvent(final long timestamp, final long traceId, final int orderIndex, final String operationSignature,
+			final String classSignature,
+			final int objectId) {
+		super(timestamp, traceId, orderIndex, operationSignature, classSignature);
+		this.objectId = objectId;
+	}
+
+	/**
+	 * This constructor converts the given array into a record. It is recommended to use the array which is the result of a call to {@link #toArray()}.
+	 * 
+	 * @param values
+	 *            The values for the record.
+	 */
+	public BeforeConstructorObjectEvent(final Object[] values) { // NOPMD (values stored directly)
+		super(values, TYPES); // values[0..4]
+		this.objectId = (Integer) values[5];
+	}
+
+	/**
+	 * This constructor uses the given array to initialize the fields of this record.
+	 * 
+	 * @param values
+	 *            The values for the record.
+	 * @param types
+	 *            The types of the elements in the first array.
+	 */
+	protected BeforeConstructorObjectEvent(final Object[] values, final Class<?>[] types) { // NOPMD (values stored directly)
+		super(values, types); // values[0..4]
+		this.objectId = (Integer) values[5];
+	}
+
+	/**
+	 * This constructor converts the given array into a record.
+	 * 
+	 * @param buffer
+	 *            The bytes for the record.
+	 * 
+	 * @throws BufferUnderflowException
+	 *             if buffer not sufficient
+	 */
+	public BeforeConstructorObjectEvent(final ByteBuffer buffer, final IRegistry<String> stringRegistry) throws BufferUnderflowException {
+		super(buffer, stringRegistry);
+		this.objectId = buffer.getInt();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Object[] toArray() {
+		return new Object[] { this.getTimestamp(), this.getTraceId(), this.getOrderIndex(), this.getOperationSignature(), this.getClassSignature(),
+			this.getObjectId(), };
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void writeBytes(final ByteBuffer buffer, final IRegistry<String> stringRegistry) throws BufferOverflowException {
+		buffer.putLong(this.getTimestamp());
+		buffer.putLong(this.getTraceId());
+		buffer.putInt(this.getOrderIndex());
+		buffer.putInt(stringRegistry.get(this.getOperationSignature()));
+		buffer.putInt(stringRegistry.get(this.getClassSignature()));
+		buffer.putInt(this.getObjectId());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<?>[] getValueTypes() {
+		return TYPES; // NOPMD
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getSize() {
+		return SIZE;
+	}
+
+	@Override
+	public final int getObjectId() {
+		return this.objectId;
+	}
+}
---
+++ src/monitoring/kicker/monitoring/probe/aspectj/flow/constructorExecutionObject/AbstractAspect.java
@@ -0,0 +1,101 @@
+/***************************************************************************
+ * Copyright 2014 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kicker.monitoring.probe.aspectj.flow.constructorExecutionObject;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+import kicker.common.record.flow.trace.TraceMetadata;
+import kicker.common.record.flow.trace.operation.constructor.object.AfterConstructorFailedObjectEvent;
+import kicker.common.record.flow.trace.operation.constructor.object.AfterConstructorObjectEvent;
+import kicker.common.record.flow.trace.operation.constructor.object.BeforeConstructorObjectEvent;
+import kicker.monitoring.core.controller.IMonitoringController;
+import kicker.monitoring.core.controller.MonitoringController;
+import kicker.monitoring.core.registry.TraceRegistry;
+import kicker.monitoring.probe.aspectj.AbstractAspectJProbe;
+import kicker.monitoring.timer.ITimeSource;
+
+/**
+ * @author Jan Waller
+ * 
+ * @since 1.10
+ */
+@Aspect
+public abstract class AbstractAspect extends AbstractAspectJProbe {
+	private static final IMonitoringController CTRLINST = MonitoringController.getInstance();
+	private static final ITimeSource TIME = CTRLINST.getTimeSource();
+	private static final TraceRegistry TRACEREGISTRY = TraceRegistry.INSTANCE;
+
+	/**
+	 * The pointcut for the monitored constructors. Inheriting classes should extend the pointcut in order to find the correct constructor executions (e.g. all
+	 * constructors or only constructors with specific annotations).
+	 */
+	@Pointcut
+	public abstract void monitoredConstructor();
+
+	/**
+	 * The advice used around the constructor executions.
+	 * 
+	 * @param thisObject
+	 * @param thisJoinPoint
+	 * 
+	 * @return The result of the joint point's {@code proceed} method.
+	 * 
+	 * @throws Throwable
+	 */
+	@Around("monitoredConstructor() && this(thisObject) && notWithinKicker()")
+	public Object constructor(final Object thisObject, final ProceedingJoinPoint thisJoinPoint) throws Throwable { // NOCS (Throwable)
+		if (!CTRLINST.isMonitoringEnabled()) {
+			return thisJoinPoint.proceed();
+		}
+		final String signature = this.signatureToLongString(thisJoinPoint.getSignature());
+		if (!CTRLINST.isProbeActivated(signature)) {
+			return thisJoinPoint.proceed();
+		}
+		// common fields
+		TraceMetadata trace = TRACEREGISTRY.getTrace();
+		final boolean newTrace = trace == null;
+		if (newTrace) {
+			trace = TRACEREGISTRY.registerTrace();
+			CTRLINST.newMonitoringRecord(trace);
+		}
+		final long traceId = trace.getTraceId();
+		final String clazz = thisObject.getClass().getName();
+		final int objectId = System.identityHashCode(thisObject);
+		// measure before execution
+		CTRLINST.newMonitoringRecord(new BeforeConstructorObjectEvent(TIME.getTime(), traceId, trace.getNextOrderId(), signature, clazz, objectId));
+		// execution of the called method
+		final Object retval;
+		try {
+			retval = thisJoinPoint.proceed();
+		} catch (final Throwable th) { // NOPMD NOCS (catch throw might ok here)
+			// measure after failed execution
+			CTRLINST.newMonitoringRecord(
+					new AfterConstructorFailedObjectEvent(TIME.getTime(), traceId, trace.getNextOrderId(), signature, clazz, th.toString(), objectId));
+			throw th;
+		} finally {
+			if (newTrace) { // close the trace
+				TRACEREGISTRY.unregisterTrace();
+			}
+		}
+		// measure after successful execution
+		CTRLINST.newMonitoringRecord(new AfterConstructorObjectEvent(TIME.getTime(), traceId, trace.getNextOrderId(), signature, clazz, objectId));
+		return retval;
+	}
+}
