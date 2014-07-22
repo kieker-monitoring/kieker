9e3e395f727c39ad81bfc4ee89743a86c15bd02a
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2014-07-22 13:20:40 +0200

    Extracted MooBench.zip.

--- examples/OverheadEvaluationMicrobenchmark/executeRemoteMicroBenchmark.sh
+++ examples/OverheadEvaluationMicrobenchmark/executeRemoteMicroBenchmark.sh
@@ -2,15 +2,17 @@
 
 REMOTE_NODE=192.168.48.60
 
-# Copy Kieker and MooBench to the remote node
-scp dist/kieker-1.10-SNAPSHOT_aspectj.jar ubuntu@${REMOTE_NODE}:.
-scp examples/OverheadEvaluationMicrobenchmark/MooBench.zip ubuntu@${REMOTE_NODE}:.
+KIEKER_JAR=kieker-1.10-SNAPSHOT_aspectj.jar 
 
-# Unzip and execute MooBench
-ssh ubuntu@${REMOTE_NODE} 'unzip MooBench.zip; mv kieker*.jar lib; chmod +x benchmark.sh; ./benchmark.sh; exit'
+# Copy Kieker and MooBench to the remote node
+scp dist/${KIEKER_JAR} ubuntu@${REMOTE_NODE}:.
+scp -r examples/OverheadEvaluationMicrobenchmark/MooBench ubuntu@${REMOTE_NODE}:.
+
+# Execute MooBench
+ssh ubuntu@${REMOTE_NODE} 'cd MooBench; mv ../kieker*.jar lib; chmod +x benchmark.sh; ./benchmark.sh; exit'
 
 # Copy results to workspace
-scp ubuntu@${REMOTE_NODE}:tmp/results-kieker/results-text.csv plot.csv
+scp ubuntu@${REMOTE_NODE}:MooBench/tmp/results-kieker/results-text.csv plot.csv
 
 # Clean up
 ssh ubuntu@${REMOTE_NODE} 'rm -rf *; exit'
