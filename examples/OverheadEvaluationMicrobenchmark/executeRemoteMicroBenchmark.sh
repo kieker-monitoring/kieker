2d23c77c2fc558b31d65c5308681c0c135ac616a
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2014-07-23 14:00:05 +0200

    Save backups of results on remote node.

--- examples/OverheadEvaluationMicrobenchmark/executeRemoteMicroBenchmark.sh
+++ examples/OverheadEvaluationMicrobenchmark/executeRemoteMicroBenchmark.sh
@@ -14,5 +14,9 @@ ssh ubuntu@${REMOTE_NODE} 'cd MooBench; mv ../kieker*.jar lib; chmod +x benchmar
 # Copy results to workspace
 scp ubuntu@${REMOTE_NODE}:MooBench/tmp/results-kieker/results-text.csv plot.csv
 
-# Clean up
+# Save results on remote node and clean up
+ssh ubuntu@${REMOTE_NODE} 'cp MooBench/tmp/results-kieker/results.zip old-results --backup=t; exit'
+ssh ubuntu@${REMOTE_NODE} "cd old-results; ls -A1t | sed -e '1,100d' | xargs -d '\n' rm; exit"
+ssh ubuntu@${REMOTE_NODE} 'mv old-results /tmp/; exit'
 ssh ubuntu@${REMOTE_NODE} 'rm -rf *; exit'
+ssh ubuntu@${REMOTE_NODE} 'mv /tmp/old-results .; exit'
