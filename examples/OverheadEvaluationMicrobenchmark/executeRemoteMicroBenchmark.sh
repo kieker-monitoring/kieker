76a6487315ec8ad6e6e7b6677af6b9452f2c1cfc
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2014-07-28 10:05:22 +0200

    Extraction of constants

--- examples/OverheadEvaluationMicrobenchmark/executeRemoteMicroBenchmark.sh
+++ examples/OverheadEvaluationMicrobenchmark/executeRemoteMicroBenchmark.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
+# The following constants specify user name and address of the remote node executing the benchmark
+# Java (>= 1.6) and R (> 3.0) should be installed on the remote node
+USERNAME=ubuntu
 REMOTE_NODE=192.168.48.60
 
-KIEKER_JAR=kieker-1.10-SNAPSHOT_aspectj.jar 
+# These constants specify the location and the name of the jar file to benchmark
+BENCHMARKED_JAR_FOLDER=dist
+BENCHMARKED_JAR_NAME=kieker-1.10-SNAPSHOT_aspectj.jar 
+
+# These constants specify the location and the name of the MooBench folder
+MOOBENCH_FOLDER=examples/OverheadEvaluationMicrobenchmark
+MOOBENCH_FOLDER_NAME=MooBench
+
+# This constant specifies the name of the folder containing the results from MooBench
+RESULTS_FOLDER_NAME=results-kieker
+
+# This constant specifies the target file for the results
+RESULTS_TARGET_FILE=plot.csv
 
 # Copy Kieker and MooBench to the remote node
-scp dist/${KIEKER_JAR} ubuntu@${REMOTE_NODE}:.
-scp -r examples/OverheadEvaluationMicrobenchmark/MooBench ubuntu@${REMOTE_NODE}:.
+scp ${BENCHMARKED_JAR_FOLDER}/${BENCHMARKED_JAR_NAME} ${USERNAME}@${REMOTE_NODE}:.
+scp -r ${MOOBENCH_FOLDER}/${MOOBENCH_FOLDER_NAME} ${USERNAME}@${REMOTE_NODE}:.
 
 # Execute MooBench
-ssh ubuntu@${REMOTE_NODE} 'cd MooBench; mv ../kieker*.jar lib; chmod +x benchmark.sh; ./benchmark.sh; exit'
+ssh ${USERNAME}@${REMOTE_NODE} 'cd ${MOOBENCH_FOLDER_NAME}; mv ../${BENCHMARKED_JAR_NAME} lib; chmod +x benchmark.sh; ./benchmark.sh; exit'
 
 # Copy results to workspace
-scp ubuntu@${REMOTE_NODE}:MooBench/tmp/results-kieker/results-text.csv plot.csv
+scp ${USERNAME}@${REMOTE_NODE}:${MOOBENCH_FOLDER_NAME}/tmp/${RESULTS_FOLDER_NAME}/results-text.csv ${RESULTS_TARGET_FILE}
 
-# Save results on remote node and clean up
-ssh ubuntu@${REMOTE_NODE} 'cp MooBench/tmp/results-kieker/results.zip old-results --backup=t; exit'
-ssh ubuntu@${REMOTE_NODE} "cd old-results; ls -A1t | sed -e '1,100d' | xargs -d '\n' rm; exit"
-ssh ubuntu@${REMOTE_NODE} 'mv old-results /tmp/; exit'
-ssh ubuntu@${REMOTE_NODE} 'rm -rf *; exit'
-ssh ubuntu@${REMOTE_NODE} 'mv /tmp/old-results .; exit'
+# Save old results from previous benchmarks on the remote node and clean up
+ssh ${USERNAME}@${REMOTE_NODE} 'cp ${MOOBENCH_FOLDER_NAME}/tmp/${RESULTS_FOLDER_NAME}/results.zip old-results --backup=t; exit'
+ssh ${USERNAME}@${REMOTE_NODE} "cd old-results; ls -A1t | sed -e '1,100d' | xargs -d '\n' rm; exit"
+ssh ${USERNAME}@${REMOTE_NODE} 'mv old-results /tmp/; exit'
+ssh ${USERNAME}@${REMOTE_NODE} 'rm -rf *; exit'
+ssh ${USERNAME}@${REMOTE_NODE} 'mv /tmp/old-results .; exit'
