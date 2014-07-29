7ef3e5c717315d4bb24e27b039ed9a0c2ebcfcb2
Reiner Jung <rju@informatik.uni-kiel.de>
2014-07-29 18:40:10 +0200

    started integrating generator into Kieker.

--- bin/dev/run-irl-compiler.sh
+++ bin/dev/run-irl-compiler.sh
@@ -3,8 +3,8 @@
 BINDIR=$(cd "$(dirname "$0")"; pwd)
 
 CLASSPATH=$(ls "${BINDIR}/lib/"*.jar | tr "\n" ":")
-CLASSPATH=$CLASSPATH:"${BINDIR}/target/de.cau.cs.se.instrumentation.rl.cli-0.0.1-SNAPSHOT.jar"
+CLASSPATH="$CLASSPATH:${BINDIR}/target/de.cau.cs.se.instrumentation.rl.cli-1.0.0-SNAPSHOT.jar"
 
-java -cp ${CLASSPATH} de.cau.cs.se.instrumentation.rl.cli.CLICompilerMain "$@"
+java -cp "${CLASSPATH}" de.cau.cs.se.instrumentation.rl.cli.CLICompilerMain "$@"
 
 # end
