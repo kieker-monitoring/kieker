bd2b3c6190effa004ce66e0fd18a250dce4d6953
Reiner Jung <reiner.jung@email.uni-kiel.de>
2014-08-07 22:12:38 +0200

    added generated sources of Kieker

---
+++ src-gen/common/kieker/common/record/flow/AbstractEvent.java
@@ -0,0 +1,101 @@
+/***************************************************************************
+ * Copyright 2013 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.common.record.flow;
+
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+
+import kieker.common.record.AbstractMonitoringRecord;
+import kieker.common.record.IMonitoringRecord;
+import kieker.common.util.registry.IRegistry;
+
+import kieker.common.record.flow.IEventRecord;
+
+/**
+ * @author Generic Kieker
+ * 
+ * @since 1.10
+ */
+public abstract class AbstractEvent extends AbstractMonitoringRecord implements IMonitoringRecord.Factory, IMonitoringRecord.BinaryFactory, IEventRecord {
+		private static final long serialVersionUID = -1334777381585391660L;
+	
+	
+	
+	private final long timestamp;
+
+	/**
+	 * Creates a new instance of this class using the given parameters.
+	 * 
+	 * @param timestamp
+	 *            timestamp
+	 */
+	public AbstractEvent(final long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	
+	/**
+	 * This constructor uses the given array to initialize the fields of this record.
+	 * 
+	 * @param values
+	 *            The values for the record.
+	 * @param valueTypes
+	 *            The types of the elements in the first array.
+	 */
+	protected AbstractEvent(final Object[] values, final Class<?>[] valueTypes) { // NOPMD (values stored directly)
+		AbstractMonitoringRecord.checkArray(values, valueTypes);
+		this.timestamp = (Long) values[0];
+	}
+
+	/**
+	 * This constructor converts the given array into a record.
+	 * 
+	 * @param buffer
+	 *            The bytes for the record.
+	 * 
+	 * @throws BufferUnderflowException
+	 *             if buffer not sufficient
+	 */
+	public AbstractEvent(final ByteBuffer buffer, final IRegistry<String> stringRegistry) throws BufferUnderflowException {
+		this.timestamp = buffer.getLong();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @deprecated This record uses the {@link kieker.common.record.IMonitoringRecord.Factory} mechanism. Hence, this method is not implemented.
+	 */
+	@Deprecated
+	public void initFromArray(final Object[] values) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @deprecated This record uses the {@link kieker.common.record.IMonitoringRecord.BinaryFactory} mechanism. Hence, this method is not implemented.
+	 */
+	@Deprecated
+	public void initFromBytes(final ByteBuffer buffer, final IRegistry<String> stringRegistry) throws BufferUnderflowException {
+		throw new UnsupportedOperationException();
+	}
+
+	public final long getTimestamp() {
+		return this.timestamp;
+	}
+	
+}
---
+++ src-gen/common/kieker/common/record/flow/trace/concurrency/monitor/MonitorNotifyAllEvent.java
@@ -0,0 +1,152 @@
+/***************************************************************************
+ * Copyright 2013 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.common.record.flow.trace.concurrency.monitor;
+
+import java.nio.BufferOverflowException;
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+
+import kieker.common.record.IMonitoringRecord;
+import kieker.common.util.registry.IRegistry;
+
+import kieker.common.record.flow.trace.concurrency.monitor.AbstractMonitorEvent;
+
+/**
+ * @author Generic Kieker
+ * 
+ * @since 1.10
+ */
+public class MonitorNotifyAllEvent extends AbstractMonitorEvent implements IMonitoringRecord.Factory, IMonitoringRecord.BinaryFactory {
+	public static final int SIZE = 24; // serialization size (without variable part of strings)
+	private static final long serialVersionUID = 4887169665782029206L;
+	
+	private static final Class<?>[] TYPES = {
+		Long.class, // IEventRecord.timestamp
+		Long.class, // ITraceRecord.traceId
+		Integer.class, // ITraceRecord.orderIndex
+		Integer.class, // AbstractMonitorEvent.lockId
+	};
+	
+	
+
+	/**
+	 * Creates a new instance of this class using the given parameters.
+	 * 
+	 * @param timestamp
+	 *            timestamp
+	 * @param traceId
+	 *            traceId
+	 * @param orderIndex
+	 *            orderIndex
+	 * @param lockId
+	 *            lockId
+	 */
+	public MonitorNotifyAllEvent(final long timestamp, final long traceId, final int orderIndex, final int lockId) {
+		super(timestamp, traceId, orderIndex, lockId);
+	}
+
+	/**
+	 * This constructor converts the given array into a record. It is recommended to use the array which is the result of a call to {@link #toArray()}.
+	 * 
+	 * @param values
+	 *            The values for the record.
+	 */
+	public MonitorNotifyAllEvent(final Object[] values) { // NOPMD (direct store of values)
+		super(values, TYPES);
+	}
+	
+	/**
+	 * This constructor uses the given array to initialize the fields of this record.
+	 * 
+	 * @param values
+	 *            The values for the record.
+	 * @param valueTypes
+	 *            The types of the elements in the first array.
+	 */
+	protected MonitorNotifyAllEvent(final Object[] values, final Class<?>[] valueTypes) { // NOPMD (values stored directly)
+		super(values, valueTypes);
+	}
+
+	/**
+	 * This constructor converts the given array into a record.
+	 * 
+	 * @param buffer
+	 *            The bytes for the record.
+	 * 
+	 * @throws BufferUnderflowException
+	 *             if buffer not sufficient
+	 */
+	public MonitorNotifyAllEvent(final ByteBuffer buffer, final IRegistry<String> stringRegistry) throws BufferUnderflowException {
+		super(buffer, stringRegistry);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Object[] toArray() {
+		return new Object[] {
+			this.getTimestamp(),
+			this.getTraceId(),
+			this.getOrderIndex(),
+			this.getLockId()
+		};
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void writeBytes(final ByteBuffer buffer, final IRegistry<String> stringRegistry) throws BufferOverflowException {
+		buffer.putLong(this.getTimestamp());
+		buffer.putLong(this.getTraceId());
+		buffer.putInt(this.getOrderIndex());
+		buffer.putInt(this.getLockId());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Class<?>[] getValueTypes() {
+		return TYPES; // NOPMD
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public int getSize() {
+		return SIZE;
+	}
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @deprecated This record uses the {@link kieker.common.record.IMonitoringRecord.Factory} mechanism. Hence, this method is not implemented.
+	 */
+	@Deprecated
+	public void initFromArray(final Object[] values) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @deprecated This record uses the {@link kieker.common.record.IMonitoringRecord.BinaryFactory} mechanism. Hence, this method is not implemented.
+	 */
+	@Deprecated
+	public void initFromBytes(final ByteBuffer buffer, final IRegistry<String> stringRegistry) throws BufferUnderflowException {
+		throw new UnsupportedOperationException();
+	}
+
+}
