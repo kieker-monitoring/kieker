5f9c0ab9fd014b01dd4b1dd3c1d8ecdeda10a020
Thomas Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2014-09-02 18:24:01 +0200

    #1420 added return values to rserve-control.sh script

--- bin/dev/rserve-control.sh
+++ bin/dev/rserve-control.sh
@@ -8,18 +8,21 @@ case "$1" in
 	R CMD Rserve --vanilla &
 	#R CMD Rserve.dbg --vanilla > /tmp/rserve.dbg.log &
 	RET=$?
-	if [ $RET = 0 ]; then
+	if [ $RET = 0 ] && (ps ax | grep -i "rserve --no-save" | grep -qv grep); then
 		echo "done."
 	else
-		echo "failed (exit status: $RET)."
+		echo "failed (exit status >0 or Rserve could not be started)."
 	fi
-	exit $RET
+	exit 1
         ;;
     stop)
 	echo -n "Trying to stop Rserve..."
-	pkill Rserve
-	echo "done."
-	exit 0
+	if pkill Rserve; then
+	    echo "done."
+	else 
+	    echo "failed"
+	    exit 1
+	fi
         ;;
     *)
         ## If no parameters are given, print which are available.
