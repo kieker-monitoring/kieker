b6ad8cc082c8b172c1058450ab72978b70bc8c2f
Thomas Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2014-10-24 02:01:49 +0200

    #1541 basic version of reader

--- examples/analysis/opad-anomaly-detection/src/kieker/examples/analysis/opad/SimpleTimeSeriesFileReader.java
+++ examples/analysis/opad-anomaly-detection/src/kieker/examples/analysis/opad/SimpleTimeSeriesFileReader.java
@@ -16,13 +16,21 @@
 
 package kieker.examples.analysis.opad;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import kieker.analysis.IProjectContext;
 import kieker.analysis.plugin.annotation.OutputPort;
 import kieker.analysis.plugin.annotation.Plugin;
 import kieker.analysis.plugin.annotation.Property;
 import kieker.analysis.plugin.filter.AbstractFilterPlugin;
-import kieker.analysis.plugin.filter.forward.TeeFilter;
 import kieker.common.configuration.Configuration;
+import kieker.common.logging.Log;
+import kieker.common.logging.LogFactory;
+import kieker.tools.tslib.TimeSeriesPoint;
 
 /**
  * @author Thomas Duellmann
@@ -30,21 +38,17 @@ import kieker.common.configuration.Configuration;
  * @since 1.11
  */
 
-@Plugin(description = "A filter to print the object to a configured stream",
-outputPorts = @OutputPort(name = TeeFilter.OUTPUT_PORT_NAME_RELAYED_EVENTS, description = "Provides each incoming object", eventTypes = { Object.class }),
-configuration = {
-	@Property(name = TeeFilter.CONFIG_PROPERTY_NAME_STREAM, defaultValue = TeeFilter.CONFIG_PROPERTY_VALUE_STREAM_STDOUT,
-			description = "The name of the stream used to print the incoming data (special values are STDOUT, STDERR, STDlog, and NULL; "
-					+ "other values are interpreted as filenames)."),
-					@Property(name = TeeFilter.CONFIG_PROPERTY_NAME_ENCODING, defaultValue = TeeFilter.CONFIG_PROPERTY_VALUE_DEFAULT_ENCODING,
-					description = "The used encoding for the selected stream."),
-					@Property(name = TeeFilter.CONFIG_PROPERTY_NAME_APPEND, defaultValue = TeeFilter.CONFIG_PROPERTY_VALUE_STREAM_APPEND,
-					description = "Decides whether the filter appends to the stream in the case of a file or not.")
-})
+@Plugin(description = "A filter to read a timeseries from a file with one value per line.",
+		outputPorts = @OutputPort(name = SimpleTimeSeriesFileReader.OUTPUT_PORT_NAME_TIME_SERIES, description = "Provides the read data from file as Timeseries.", eventTypes = { TimeSeriesPoint.class }),
+		configuration = {
+			@Property(name = SimpleTimeSeriesFileReader.CONFIG_PROPERTY_NAME_INPUT_FILE, description = "", defaultValue = "inputTS.csv"),
+		})
 public class SimpleTimeSeriesFileReader extends AbstractFilterPlugin {
 
-	/** The name of the property determining whether or not the stream appends or overwrites to files. */
-	public static final String CONFIG_PROPERTY_NAME_INPUT_FILE = "input_file";
+	private static final Log LOG = LogFactory.getLog(SimpleTimeSeriesFileReader.class);
+
+	public static final String CONFIG_PROPERTY_NAME_INPUT_FILE = "inputfile";
+	public static final String OUTPUT_PORT_NAME_TIME_SERIES = "timeseries";
 
 	private final String inputFile;
 
@@ -59,6 +63,64 @@ public class SimpleTimeSeriesFileReader extends AbstractFilterPlugin {
 	public SimpleTimeSeriesFileReader(final Configuration configuration, final IProjectContext projectContext) {
 		super(configuration, projectContext);
 		this.inputFile = this.configuration.getPathProperty(CONFIG_PROPERTY_NAME_INPUT_FILE);
+		this.readInputFile(this.inputFile);
+	}
+
+	public void readInputFile(final String inputFile, final String encoding)
+	{
+		FileInputStream fis = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+
+		try {
+			fis = new FileInputStream(inputFile);
+			isr = new InputStreamReader(fis, encoding);
+			br = new BufferedReader(isr);
+
+			TimeSeriesPoint<Long> tsp;
+
+			LOG.info("Starting to read the file \"" + inputFile + "\" using encoding: " + encoding);
+
+			// As we only have the values, we use a continouous timestamp starting at 1
+			long timestamp = 1;
+			while (br.ready()) {
+				final String readLine = br.readLine();
+				Long tsValue;
+				try {
+					tsValue = Long.valueOf(readLine);
+					tsp = new TimeSeriesPoint<Long>(timestamp, tsValue);
+					super.deliver(SimpleTimeSeriesFileReader.OUTPUT_PORT_NAME_TIME_SERIES, tsp);
+					timestamp++;
+
+				} catch (final NumberFormatException nfe) {
+					// The current entry seems to be erroneous, so we skip it.
+					LOG.warn("Could not convert the line \"" + readLine + "\" to long. Skipping it.");
+					break;
+				}
+			}
+		} catch (final FileNotFoundException fnfe) {
+			LOG.error("Could not find the given file " + this.inputFile, fnfe);
+		} catch (final IOException ioe) {
+			LOG.error(ioe.getMessage(), ioe);
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
+				}
+				if (isr != null) {
+					isr.close();
+				}
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (final IOException e)
+			{
+			}
+		}
+	}
+
+	public void readInputFile(final String inputFile) {
+		this.readInputFile(inputFile, "UTF-8");
 	}
 
 	@Override
