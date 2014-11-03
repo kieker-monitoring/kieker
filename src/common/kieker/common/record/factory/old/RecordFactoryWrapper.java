5256669dbc2a04e1af2fb54b219ff46f0b362026
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2014-11-03 10:52:59 +0100

    Added missing files

---
+++ src/common/kieker/common/record/factory/old/RecordFactoryWrapper.java
@@ -0,0 +1,60 @@
+/***************************************************************************
+ * Copyright 2014 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.common.record.factory.old;
+
+import java.nio.ByteBuffer;
+
+import kieker.common.exception.MonitoringRecordException;
+import kieker.common.exception.RecordInstantiationException;
+import kieker.common.record.AbstractMonitoringRecord;
+import kieker.common.record.IMonitoringRecord;
+import kieker.common.record.factory.IRecordFactory;
+import kieker.common.util.registry.IRegistry;
+
+/**
+ * @author Christian Wulf
+ *
+ * @since 1.11
+ */
+public final class RecordFactoryWrapper implements IRecordFactory<IMonitoringRecord> {
+
+	private final String recordClassName;
+
+	public RecordFactoryWrapper(final String recordClassName) {
+		this.recordClassName = recordClassName;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public IMonitoringRecord create(final ByteBuffer buffer, final IRegistry<String> stringRegistry) {
+		try {
+			return AbstractMonitoringRecord.createFromByteBuffer(this.recordClassName, buffer, stringRegistry);
+		} catch (final MonitoringRecordException e) {
+			throw new RecordInstantiationException(e);
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public IMonitoringRecord create(final Object[] values) {
+		try {
+			return AbstractMonitoringRecord.createFromArray(this.recordClassName, values);
+		} catch (final MonitoringRecordException e) {
+			throw new RecordInstantiationException(e);
+		}
+	}
+}
