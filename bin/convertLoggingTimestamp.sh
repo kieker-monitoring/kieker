2a32d067d466b79b86c3b0a8bd0937a14cc7b5fd
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2014-11-03 12:36:01 +0100

    1560

--- bin/convertLoggingTimestamp.sh
+++ bin/convertLoggingTimestamp.sh
@@ -9,6 +9,5 @@ BINDIR=$(cd "$(dirname "$0")"; pwd)/
 
 JAVAARGS="-Dkieker.common.logging.Log=JDK -Dkieker.common.logging.Log=JDK -Djava.util.logging.config.file=${BINDIR}/logging.properties -Xms56m -Xmx1024m"
 MAINCLASSNAME=kieker.tools.loggingTimestampConverter.LoggingTimestampConverterTool
-CLASSPATH=$(ls "${BINDIR}/../lib/"*.jar | tr "\n" ":")$(ls "${BINDIR}/../dist/"*.jar | tr "\n" ":")${BINDIR}
 
-java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} $*
+java ${JAVAARGS} -cp "${BINDIR}/../lib/*":"${BINDIR}/../dist/*":"${BINDIR}" ${MAINCLASSNAME} $*
--- bin/logReplay.sh
+++ bin/logReplay.sh
@@ -9,6 +9,5 @@ BINDIR=$(cd "$(dirname "$0")"; pwd)/
 
 JAVAARGS="-Dkieker.common.logging.Log=JDK -Djava.util.logging.config.file=${BINDIR}/logging.properties"
 MAINCLASSNAME=kieker.tools.logReplayer.FilesystemLogReplayerStarter
-CLASSPATH=$(ls "${BINDIR}/../lib/"*.jar | tr "\n" ":")$(ls "${BINDIR}/../dist/"*.jar | tr "\n" ":")${BINDIR}
 
-java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} "$@"
+java ${JAVAARGS} -cp "${BINDIR}/../lib/*":"${BINDIR}/../dist/*":"${BINDIR}" ${MAINCLASSNAME} "$@"
