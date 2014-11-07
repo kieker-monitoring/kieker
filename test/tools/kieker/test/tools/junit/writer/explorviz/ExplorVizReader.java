09e72cfb0a0ef5d4d8544b2feffefada47950739
Micky Singh Multani <msm@informatik.uni-kiel.de>
2014-11-07 01:22:15 +0100

    some minor changes

--- test/tools/kieker/test/tools/junit/writer/explorviz/ExplorVizReader.java
+++ test/tools/kieker/test/tools/junit/writer/explorviz/ExplorVizReader.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  ***************************************************************************/
 
-package kieker.test.monitoring.junit.writer.explorviz;
+package kieker.test.tools.junit.writer.explorviz;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -44,11 +44,12 @@ import kieker.common.util.registry.Lookup;
  */
 @Plugin(description = "A reader which reads records from a TCP port",
 		outputPorts = {
-			@OutputPort(name = ExplorVizReader.OUTPUT_PORT_NAME_RECORDS, eventTypes = { IMonitoringRecord.class }, description = "Output Port of the ExplorVizReader")
+			@OutputPort(name = ExplorVizReader.OUTPUT_PORT_NAME_RECORDS, eventTypes = { IMonitoringRecord.class },
+					description = "Output Port of the ExplorVizReader")
 		},
 		configuration = {
 			@Property(name = ExplorVizReader.CONFIG_PROPERTY_NAME_PORT, defaultValue = "10555",
-					description = "The port of the server used for the TCP connection."),
+					description = "The port of the server used for the TCP connection.")
 		})
 public class ExplorVizReader extends AbstractReaderPlugin {
 
@@ -59,7 +60,6 @@ public class ExplorVizReader extends AbstractReaderPlugin {
 	public static final String CONFIG_PROPERTY_NAME_PORT = "port";
 
 	private static final int MESSAGE_BUFFER_SIZE = 65535;
-	public static final String ENCODING = "UTF-8";
 
 	private volatile Thread readerThread;
 	private volatile boolean terminated = false; // NOPMD
@@ -88,75 +88,24 @@ public class ExplorVizReader extends AbstractReaderPlugin {
 			}
 			final SocketChannel socketChannel = serversocket.accept();
 			final ByteBuffer buffer = ByteBuffer.allocateDirect(MESSAGE_BUFFER_SIZE);
-
-			long timestamp, traceID;
-			int orderIndex;
-			byte clazzid, notNeededClazzID;
+			byte clazzid;
 			boolean flag = true;
 
 			while ((socketChannel.read(buffer) != -1) && (!this.terminated)) {
 				buffer.flip();
-
 				try {
 					while (buffer.hasRemaining()) {
+
 						buffer.mark();
-						clazzid = buffer.get();
 
 						if (flag) {
 							this.skipBufferToFirstTestRecord(buffer);
 							flag = false;
 						}
 
-						if (clazzid == 1) { // BeforeOperationEvent
-							timestamp = buffer.getLong();
-							traceID = buffer.getLong();
-							orderIndex = buffer.getInt();
-							final int notRelevant = buffer.getInt();
-							final int opSinatureStringID = buffer.getInt();
-							final int classSignatureStringID = buffer.getInt();
-							final int emptyStringID3 = buffer.getInt();
-							notNeededClazzID = buffer.get();
-							RegistryRecord.registerRecordInRegistry(buffer, this.stringRegistry);
-							notNeededClazzID = buffer.get();
-							RegistryRecord.registerRecordInRegistry(buffer, this.stringRegistry);
-							notNeededClazzID = buffer.get();
-							RegistryRecord.registerRecordInRegistry(buffer, this.stringRegistry);
-							final String opSignature = this.stringRegistry.get(opSinatureStringID);
-							final String classSignature = this.stringRegistry.get(classSignatureStringID);
+						clazzid = buffer.get();
+						this.createReceivedRecord(clazzid, buffer);
 
-							final BeforeOperationEvent record = new BeforeOperationEvent(timestamp, traceID, orderIndex,
-									opSignature, classSignature);
-							super.deliver(OUTPUT_PORT_NAME_RECORDS, record);
-
-						} else if (clazzid == 2) { // AfterFailedOperationEvent
-							timestamp = buffer.getLong();
-							traceID = buffer.getLong();
-							orderIndex = buffer.getInt();
-							final int causeStringID = buffer.getInt();
-							notNeededClazzID = buffer.get();
-							RegistryRecord.registerRecordInRegistry(buffer, this.stringRegistry);
-							final String cause = this.stringRegistry.get(causeStringID);
-
-							final CustomAfterOperationFailedEvent record = new CustomAfterOperationFailedEvent(timestamp, traceID, orderIndex, cause);
-							super.deliver(OUTPUT_PORT_NAME_RECORDS, record);
-
-						} else if (clazzid == 3) { // AfterOperationEvent
-							final CustomAfterOperationEvent record = new CustomAfterOperationEvent(buffer.getLong(), buffer.getLong(), buffer.getInt());
-							super.deliver(OUTPUT_PORT_NAME_RECORDS, record);
-
-						} else if (clazzid == 4) { // RegistryRecord
-							final int id = buffer.getInt();
-							final byte[] strBytes = new byte[buffer.getInt()];
-							buffer.get(strBytes);
-							String string;
-							try {
-								string = new String(strBytes, ENCODING);
-							} catch (final UnsupportedEncodingException e) {
-								string = new String(strBytes); // NOPMD
-							}
-							final RegistryRecord record = new RegistryRecord(id, string);
-							super.deliver(OUTPUT_PORT_NAME_RECORDS, record);
-						}
 					}
 					buffer.clear();
 				} catch (final BufferUnderflowException ex) {
@@ -200,12 +149,80 @@ public class ExplorVizReader extends AbstractReaderPlugin {
 		return configuration;
 	}
 
+	public void createReceivedRecord(final byte clazzid, final ByteBuffer buffer) {
+		long timestamp;
+		long traceID;
+		int orderIndex;
+		byte notNeededClazzID; // NOPMD
+		final int notRelevant; // NOPMD
+		final int opSinatureStringID;
+		final int classSignatureStringID;
+		final int emptyStringID3; // NOPMD
+		final int causeStringID;
+		final String opSignature;
+		final String classSignature;
+
+		// BeforeOperationEvent
+		if (clazzid == 1) {
+			timestamp = buffer.getLong();
+			traceID = buffer.getLong();
+			orderIndex = buffer.getInt();
+			notRelevant = buffer.getInt();
+			opSinatureStringID = buffer.getInt();
+			classSignatureStringID = buffer.getInt();
+			emptyStringID3 = buffer.getInt();
+			notNeededClazzID = buffer.get();
+			RegistryRecord.registerRecordInRegistry(buffer, this.stringRegistry);
+			notNeededClazzID = buffer.get();
+			RegistryRecord.registerRecordInRegistry(buffer, this.stringRegistry);
+			notNeededClazzID = buffer.get();
+			RegistryRecord.registerRecordInRegistry(buffer, this.stringRegistry);
+			opSignature = this.stringRegistry.get(opSinatureStringID);
+			classSignature = this.stringRegistry.get(classSignatureStringID);
+
+			final BeforeOperationEvent record = new BeforeOperationEvent(timestamp, traceID, orderIndex,
+					opSignature, classSignature);
+			super.deliver(OUTPUT_PORT_NAME_RECORDS, record);
+
+			// AfterFailedOperationEvent
+		} else if (clazzid == 2) {
+			timestamp = buffer.getLong();
+			traceID = buffer.getLong();
+			orderIndex = buffer.getInt();
+			causeStringID = buffer.getInt();
+			notNeededClazzID = buffer.get();
+			RegistryRecord.registerRecordInRegistry(buffer, this.stringRegistry);
+			final String cause = this.stringRegistry.get(causeStringID);
+
+			final CustomAfterOperationFailedEvent record = new CustomAfterOperationFailedEvent(timestamp, traceID, orderIndex, cause);
+			super.deliver(OUTPUT_PORT_NAME_RECORDS, record);
+
+			// AfterOperationEvent
+		} else if (clazzid == 3) {
+			final CustomAfterOperationEvent record = new CustomAfterOperationEvent(buffer.getLong(), buffer.getLong(), buffer.getInt());
+			super.deliver(OUTPUT_PORT_NAME_RECORDS, record);
+
+			// RegistryRecord
+		} else if (clazzid == 4) {
+			final int id = buffer.getInt();
+			final byte[] strBytes = new byte[buffer.getInt()];
+			buffer.get(strBytes);
+			String string;
+			try {
+				string = new String(strBytes, RegistryRecord.ENCODING);
+			} catch (final UnsupportedEncodingException e) {
+				string = new String(strBytes); // NOPMD
+			}
+			final RegistryRecord record = new RegistryRecord(id, string);
+			super.deliver(OUTPUT_PORT_NAME_RECORDS, record);
+		}
+	}
+
 	public void skipBufferToFirstTestRecord(final ByteBuffer buffer) {
-		byte testByte;
-		int testInt;
+		byte testByte; // NOPMD
+		int testInt; // NOPMD
 
 		testByte = buffer.get();
-
 		for (int i = 0; i < 4; i++) {
 			testInt = buffer.getInt();
 		}
