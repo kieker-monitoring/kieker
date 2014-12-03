ed2d7700253a726dd193325eb667354a59223182
Christian Wulf <chw@informatik.uni-kiel.de>
2014-12-03 19:09:03 +0100

    added new proposal for IRegistry and ILookup

---
+++ src/common/kieker/common/util/registry/newversion/ILookup.java
@@ -0,0 +1,31 @@
+/***************************************************************************
+ * Copyright 2014 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.common.util.registry.newversion;
+
+/**
+ * @author Christian Wulf
+ *
+ * @since 1.11
+ */
+public interface ILookup<T> {
+
+	void add(int uniqueId, T element);
+
+	T get(int uniqueId);
+
+	// int getSize();
+}
