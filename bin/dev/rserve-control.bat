5f1e07d2cc3d2f6ed22d303d6f2de9276c5a6018
Thomas Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2015-03-24 16:41:10 +0100

    #1629 added fix to batch file

--- bin/dev/rserve-control.bat
+++ bin/dev/rserve-control.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
 
+cd %~dp0
+
+SET BINDIR=%cd%
+
 IF "%1"=="start" (
   ECHO "Trying to start Rserve..."
-  start R CMD BATCH RserveStart.R RserveStart.log --vanilla
+  start R CMD BATCH %BINDIR%\RserveStart.R RserveStart.log --vanilla
 )
 
 IF "%1"=="stop" (
@@ -14,3 +18,5 @@ IF "%1"=="" (
   :: Print usage
   ECHO "Usage: %0 (start|stop)"
 )
+
+@ECHO ON
