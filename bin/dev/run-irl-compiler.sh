72cf92c9c5e553bd25c1431a57b1e1cfc7d857fa
Reiner Jung <rju@informatik.uni-kiel.de>
2015-09-04 13:01:46 +0200

    updated compiler

--- bin/dev/run-irl-compiler.sh
+++ bin/dev/run-irl-compiler.sh
@@ -2,7 +2,7 @@
 
 BINDIR=$(cd "$(dirname "$0")"; pwd)
 
-CLASSPATH=$CLASSPATH:"${BINDIR}/target/de.cau.cs.se.instrumentation.rl.cli-1.1.0-SNAPSHOT.jar"
+CLASSPATH=$CLASSPATH:"${BINDIR}/target/de.cau.cs.se.instrumentation.rl.cli-1.1.0.jar"
 
 java -cp ${CLASSPATH} de.cau.cs.se.instrumentation.rl.cli.CLICompilerMain "$@"
 
