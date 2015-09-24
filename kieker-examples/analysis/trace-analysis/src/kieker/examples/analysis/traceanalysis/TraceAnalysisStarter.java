8a8a897f57d992362543fe4ec3d113f0a0994248
Thomas Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2015-09-24 16:33:59 +0200

    #1675 added TraceEventRecords2ExecutionAndMessageTraceFilter

--- kieker-examples/analysis/trace-analysis/src/kieker/examples/analysis/traceanalysis/TraceAnalysisStarter.java
+++ kieker-examples/analysis/trace-analysis/src/kieker/examples/analysis/traceanalysis/TraceAnalysisStarter.java
@@ -18,34 +18,54 @@ package kieker.examples.analysis.traceanalysis;
 
 import kieker.analysis.AnalysisController;
 import kieker.analysis.exception.AnalysisConfigurationException;
-import kieker.analysis.plugin.filter.forward.TeeFilter;
+import kieker.analysis.plugin.filter.flow.EventRecordTraceReconstructionFilter;
+import kieker.analysis.plugin.filter.forward.ListCollectionFilter;
 import kieker.analysis.plugin.reader.filesystem.FSReader;
 import kieker.common.configuration.Configuration;
 import kieker.tools.traceAnalysis.filter.AbstractTraceAnalysisFilter;
 import kieker.tools.traceAnalysis.filter.executionRecordTransformation.ExecutionRecordTransformationFilter;
-import kieker.tools.traceAnalysis.filter.sessionReconstruction.SessionReconstructionFilter;
+import kieker.tools.traceAnalysis.filter.flow.TraceEventRecords2ExecutionAndMessageTraceFilter;
 import kieker.tools.traceAnalysis.filter.traceReconstruction.TraceReconstructionFilter;
+import kieker.tools.traceAnalysis.systemModel.MessageTrace;
 import kieker.tools.traceAnalysis.systemModel.repository.SystemModelRepository;
 
 /**
+ * This class shows an example on how to do a trace analysis with different inputs (OperationExecutionRecords, TraceEvents).
+ * Depending on the input the data is processed differently.
+ *
  * @author Thomas Duellmann
  *
  * @since 1.12
  */
 public class TraceAnalysisStarter {
 
-	private static final String INPUT_MONITORING_LOG_FN = "./input-data/";
+	// Depending on the input, you have to change the input path and the reader configuration (commented with ALTERNATIVE)
+	private static final String INPUT_MONITORING_LOG_OER = "./input-data/operation-execution-log/";
+	// private static final String INPUT_MONITORING_LOG_TE = "./input-data/trace-event-log/";
 
-	public static void main(final String[] args) throws IllegalStateException, AnalysisConfigurationException {
+	public static void main(final String[] args) throws IllegalStateException, AnalysisConfigurationException, InterruptedException {
 		final AnalysisController analysisController = new AnalysisController();
+
+		// OPTIONAL: Instead of using the ListCollectionFilter, you can define your own filter that reads the traces.
+		final ListCollectionFilter<MessageTrace> list = new ListCollectionFilter<MessageTrace>(new Configuration(), analysisController);
 
 		// Initialize and register the list reader
 		final Configuration fsReaderConfig = new Configuration();
-		fsReaderConfig.setProperty(FSReader.CONFIG_PROPERTY_NAME_INPUTDIRS, INPUT_MONITORING_LOG_FN);
+
+		// When using the OperationExecution log as input:
+		fsReaderConfig.setProperty(FSReader.CONFIG_PROPERTY_NAME_INPUTDIRS, INPUT_MONITORING_LOG_OER);
+
+		// ALTERNATIVE when using the TraceEvent log as input:
+		// fsReaderConfig.setProperty(FSReader.CONFIG_PROPERTY_NAME_INPUTDIRS, INPUT_MONITORING_LOG_TE);
+
 		final FSReader reader = new FSReader(fsReaderConfig, analysisController);
 
 		// Initialize and register the system model repository
 		final SystemModelRepository systemModelRepository = new SystemModelRepository(new Configuration(), analysisController);
+
+		//
+		// BEGIN OperationExecutionRecord path
+		//
 
 		// Initialize, register and connect the execution record transformation filter
 		final ExecutionRecordTransformationFilter executionRecordTransformationFilter = new ExecutionRecordTransformationFilter(new Configuration(),
@@ -62,18 +82,42 @@ public class TraceAnalysisStarter {
 		analysisController.connect(executionRecordTransformationFilter, ExecutionRecordTransformationFilter.OUTPUT_PORT_NAME_EXECUTIONS,
 				traceReconstructionFilter, TraceReconstructionFilter.INPUT_PORT_NAME_EXECUTIONS);
 
-		// Initialize, register and connect the session reconstruction filter
-		final Configuration bareSessionReconstructionFilterConfiguration = new Configuration();
-		bareSessionReconstructionFilterConfiguration.setProperty(SessionReconstructionFilter.CONFIG_PROPERTY_NAME_MAX_THINK_TIME,
-				SessionReconstructionFilter.CONFIG_PROPERTY_VALUE_MAX_THINK_TIME);
+		analysisController.connect(traceReconstructionFilter, TraceReconstructionFilter.OUTPUT_PORT_NAME_MESSAGE_TRACE, list, ListCollectionFilter.INPUT_PORT_NAME);
+		//
+		// END OperationExecutionRecord path
+		//
 
-		// Trace objects now provided by the traceReconstructionFilter's output ports (TraceReconstructionFilter.OUTPUT_PORT_NAME_{MESSAGE|EXECUTION}_TRACE).
+		//
+		// BEGIN TraceEventRecord path
+		//
+		final EventRecordTraceReconstructionFilter eventRecordTraceReconstructionFilter = new EventRecordTraceReconstructionFilter(new Configuration(),
+				analysisController);
+		// analysisController.connect(eventRecordTraceReconstructionFilter, AbstractTraceAnalysisFilter.REPOSITORY_PORT_NAME_SYSTEM_MODEL, systemModelRepository);
+		analysisController.connect(reader, FSReader.OUTPUT_PORT_NAME_RECORDS, eventRecordTraceReconstructionFilter,
+				EventRecordTraceReconstructionFilter.INPUT_PORT_NAME_TRACE_RECORDS);
 
-		// TODO: Instead of using the TeeFilter, define your own filter that reads the traces.
-		final TeeFilter teeFilter = new TeeFilter(new Configuration(), analysisController);
-		analysisController.connect(traceReconstructionFilter, TraceReconstructionFilter.OUTPUT_PORT_NAME_MESSAGE_TRACE,
-				teeFilter, TeeFilter.INPUT_PORT_NAME_EVENTS);
+		// Read the TraceEventRecords and write the Execution- and MessageTraces to the output
+		final TraceEventRecords2ExecutionAndMessageTraceFilter ter2eamt = new TraceEventRecords2ExecutionAndMessageTraceFilter(new Configuration(),
+				analysisController);
+		analysisController.connect(ter2eamt, AbstractTraceAnalysisFilter.REPOSITORY_PORT_NAME_SYSTEM_MODEL, systemModelRepository);
+		analysisController.connect(eventRecordTraceReconstructionFilter, EventRecordTraceReconstructionFilter.OUTPUT_PORT_NAME_TRACE_VALID, ter2eamt,
+				TraceEventRecords2ExecutionAndMessageTraceFilter.INPUT_PORT_NAME_EVENT_TRACE);
+
+		// This can be added to also to also output the ExectionTraces:
+		// analysisController.connect(ter2eamt, TraceEventRecords2ExecutionAndMessageTraceFilter.OUTPUT_PORT_NAME_EXECUTION_TRACE, list,
+		// ListCollectionFilter.INPUT_PORT_NAME);
+
+		analysisController.connect(ter2eamt, TraceEventRecords2ExecutionAndMessageTraceFilter.OUTPUT_PORT_NAME_MESSAGE_TRACE, list,
+				ListCollectionFilter.INPUT_PORT_NAME);
+		//
+		// END TraceEventRecord path
+		//
 
 		analysisController.run();
+
+		for (final MessageTrace mt : list.getList()) {
+			System.out.println(mt);
+		}
+
 	}
 }
