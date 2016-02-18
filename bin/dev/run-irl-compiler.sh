217a82ca6f19fbe8ebc1e1ab368edf7dae1da73d
Reiner Jung <reiner.jung@email.uni-kiel.de>
2016-02-18 19:48:04 +0100

    fixed bug #1762 caused by using JarModelGlobalScopeProvider which is necessary to be able to read jar files containing Java record classes. Therefore, this is only a fix for Kieker build scripts.

--- bin/dev/run-irl-compiler.sh
+++ bin/dev/run-irl-compiler.sh
@@ -2,7 +2,7 @@
 
 BINDIR=$(cd "$(dirname "$0")"; pwd)
 
-CLASSPATH=$CLASSPATH:"${BINDIR}/target/de.cau.cs.se.instrumentation.rl.cli-1.1.0.jar"
+CLASSPATH="${BINDIR}/target/de.cau.cs.se.instrumentation.rl.cli-1.2.0-SNAPSHOT.jar"
 
 java -cp ${CLASSPATH} de.cau.cs.se.instrumentation.rl.cli.CLICompilerMain "$@"
 
