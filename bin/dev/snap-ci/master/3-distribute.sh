77f019694eed24c121365cddc14f79feab82766d
Thomas F. Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2016-03-31 12:53:43 +0200

    Created scripts to be executed by snap-ci (to keep the information about the stages version-controlled).

---
+++ bin/dev/snap-ci/master/3-distribute.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+WORK_DIR=/tmp/kieker
+
+sudo docker run -v ${WORK_DIR}:/opt/kieker kieker/kieker-build:openjdk6 /bin/bash -c "cd /opt/kieker; ./gradlew -S distribute -x test -x check"
+
+cd /
---
+++ bin/dev/snap-ci/master/4-checkscript.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+WORK_DIR=/tmp/kieker
+
+sudo docker run -v ${WORK_DIR}:/opt/kieker kieker/kieker-build:openjdk6 /bin/bash -c "cd /opt/kieker; ./bin/dev/check-release-archives.sh"
+
+cd /
