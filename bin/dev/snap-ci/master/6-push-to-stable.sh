084c7f490f47ba076243cf0291770aa90a652982
Thomas F. Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2016-07-05 17:49:44 +0200

    KIEKER-1432 Created script for the push-to-stable stage.

---
+++ bin/dev/snap-ci/master/6-push-to-stable.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+WORK_DIR=/tmp/kieker
+
+if [ "${SNAP_BRANCH}" == "master" ]; then
+  echo "We are in master - pushing to stable branch."
+  cd ${WORK_DIR}
+  git remote add github https://${KIEKER_USER}:${KIEKER_PASS}@github.com/kieker-monitoring/kieker.git
+#  git checkout stable
+#  git merge master
+  git push github stable
+
+  STAGE_RESULT=$?
+else
+  echo "We are not in master - skipping."
+  STAGE_RESULT=0
+fi
+
+cd /
+
+exit $STAGE_RESULT
