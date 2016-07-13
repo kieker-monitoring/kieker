64a5dfbcc536e43db0641d69578586918883c41a
Thomas F. Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2016-07-13 10:36:41 +0200

    KIEKER-1432 Added comments to push to stable snap-ci script.

--- bin/dev/snap-ci/master/6-push-to-stable.sh
+++ bin/dev/snap-ci/master/6-push-to-stable.sh
@@ -5,7 +5,11 @@ WORK_DIR=/tmp/kieker
 if [ "${SNAP_BRANCH}" == "master" ]; then
   echo "We are in master - pushing to stable branch."
   cd ${WORK_DIR}
+
+  # Add remote 'github'
   git remote add github git@github.com:kieker-monitoring/kieker.git
+ 
+  # Push content of master branch to stable branch
   git push github master:stable
 
   STAGE_RESULT=$?
