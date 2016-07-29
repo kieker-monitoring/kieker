26d42c09459727f2ca6d37582de67f725b376f52
Thomas F. Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2016-07-29 14:53:28 +0200

    Fixed typo in compile script for snap-ci.

--- bin/dev/snap-ci/master/1-compile.sh
+++ bin/dev/snap-ci/master/1-compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo docker run -v ${SNAP_WORKIN_DIR}:/opt/kieker kieker/kieker-build:openjdk6 /bin/bash -c "cd /opt/kieker; ./gradlew -S compileJava compileTestJava"
+sudo docker run -v ${SNAP_WORKING_DIR}:/opt/kieker kieker/kieker-build:openjdk6 /bin/bash -c "cd /opt/kieker; ./gradlew -S compileJava compileTestJava"
 
 exit $?
