a963e461a1ff8b9ce8c77de28bda0f12f987c4cb
Thomas F. Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2016-07-29 14:48:11 +0200

    KIEKER-1432 Changed the scripts to represent the changes in snap-ci. Now the scripts use the actual SNAP_WORKING_DIR as working directory.

--- bin/dev/snap-ci/master/0-init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ARTIFACT_DIR="${SNAP_WORKING_DIR}/snap-artifacts"
-KIEKER_DIR="${ARTIFACT_DIR}/kieker"
-TMP_DIR=/tmp/kieker
-
-cp -r ${SNAP_WORKING_DIR}/. ${TMP_DIR}
-mkdir -p ${ARTIFACT_DIR}/
-mv ${TMP_DIR}/ ${ARTIFACT_DIR}
