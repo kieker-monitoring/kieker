83ccb41d44272b8a84af54f59b31a9f9e87d2fc8
Thomas F. Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2016-08-02 22:58:50 +0200

    KIEKER-1432 Fixed superfluous line breaks and added a condition for the extended release archive check.

--- bin/dev/snap-ci/master/5-checkrelease.sh
+++ bin/dev/snap-ci/master/5-checkrelease.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-sudo docker run -v ${SNAP_WORKING_DIR}:/opt/kieker kieker/kieker-build:openjdk6 /bin/bash -c "cd 
-/opt/kieker; ./gradlew checkReleaseArchives"
+if [ "${SNAP_BRANCH}" == "master" ]; then
+  echo "We are in master - executing the extended release archive check."
 
-exit $?
+  sudo docker run -v ${SNAP_WORKING_DIR}:/opt/kieker kieker/kieker-build:openjdk6 /bin/bash -c "cd /opt/kieker; ./gradlew checkReleaseArchives"
+
+  STAGE_RESULT=$?
+else
+  echo "We are not in master - skipping the extended release archive check."
+  STAGE_RESULT=0
+fi
+
+exit $STAGE_RESULT
