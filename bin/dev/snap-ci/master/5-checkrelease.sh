72ee8f1eb7f2cec1ea4fafc280311fb98a2cb8b3
Thomas F. Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2016-08-02 22:39:49 +0200

    KIEKER-1432 Applied split of the check release script in the stages for snap-ci.

--- bin/dev/snap-ci/master/5-checkrelease.sh
+++ bin/dev/snap-ci/master/5-checkrelease.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo docker run -v ${SNAP_WORKING_DIR}:/opt/kieker kieker/kieker-build:openjdk6 /bin/bash -c "cd 
-/opt/kieker; ./gradlew checkReleaseArchivesShort"
+/opt/kieker; ./gradlew checkReleaseArchives"
 
 exit $?
