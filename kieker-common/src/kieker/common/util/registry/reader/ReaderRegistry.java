def2f6c8359d3fac3706e7d7a9950aaf4c8cbbb8
Christian Wulf <chw@informatik.uni-kiel.de>
2016-09-30 09:41:16 +0200

    fixed PMD and checkstyle issues

--- kieker-common/src/kieker/common/util/registry/reader/ReaderRegistry.java
+++ kieker-common/src/kieker/common/util/registry/reader/ReaderRegistry.java
@@ -1,9 +1,26 @@
+/***************************************************************************
+ * Copyright 2016 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
 package kieker.common.util.registry.reader;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Represents an unsynchronized registry for read-only purposes. It is used by the readers within the analysis component.
+ *
  * @author Christian Wulf
  *
  * @since 1.13
@@ -12,7 +29,14 @@ import java.util.Map;
 public class ReaderRegistry<E> {
 
 	// TODO replace by a high performance map with primitive key type
-	private final Map<Long, E> registryEntries = new HashMap<Long, E>();
+	private final Map<Long, E> registryEntries = new HashMap<Long, E>(); // NOPMD (should be unsynchronized)
+
+	/**
+	 * Constructs an unsynchronized reader registry.
+	 */
+	public ReaderRegistry() {
+		super();
+	}
 
 	public E get(final long key) {
 		return this.registryEntries.get(key);
--- kieker-monitoring/src/kieker/monitoring/writer/tcp/SingleSocketTcpWriterThread.java
+++ kieker-monitoring/src/kieker/monitoring/writer/tcp/SingleSocketTcpWriterThread.java
@@ -1,3 +1,18 @@
+/***************************************************************************
+ * Copyright 2016 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
 package kieker.monitoring.writer.tcp;
 
 import java.io.IOException;
@@ -25,7 +40,6 @@ class SingleSocketTcpWriterThread extends AbstractAsyncThread {
 
 	private final SocketChannel socketChannel;
 	private final ByteBuffer byteBuffer;
-	private final IRegistry<String> stringRegistry;
 	private final boolean flush;
 	private final RecordSerializer recordSerializer;
 
@@ -34,9 +48,9 @@ class SingleSocketTcpWriterThread extends AbstractAsyncThread {
 		super(monitoringController, writeQueue);
 		this.byteBuffer = ByteBuffer.allocateDirect(bufferSize);
 		this.socketChannel = SocketChannel.open(new InetSocketAddress(hostname, port));
-		this.stringRegistry = this.monitoringController.getStringRegistry();
 		this.flush = flush;
-		this.recordSerializer = new RecordSerializer(this.stringRegistry);
+		final IRegistry<String> stringRegistry = this.monitoringController.getStringRegistry();
+		this.recordSerializer = new RecordSerializer(stringRegistry);
 	}
 
 	@Override
