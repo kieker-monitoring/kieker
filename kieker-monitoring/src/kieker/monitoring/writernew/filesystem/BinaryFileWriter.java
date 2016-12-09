0e53f5e2a575ba3728b7c177a1a94278d532ec55
Christian Wulf <chw@informatik.uni-kiel.de>
2016-12-09 21:30:58 +0100

    worked on binary file writer

--- kieker-monitoring/src/kieker/monitoring/writernew/filesystem/BinaryFileWriter.java
+++ kieker-monitoring/src/kieker/monitoring/writernew/filesystem/BinaryFileWriter.java
@@ -16,7 +16,6 @@
 
 package kieker.monitoring.writernew.filesystem;
 
-import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DateFormat;
@@ -30,22 +29,19 @@ import kieker.common.util.filesystem.FSUtil;
 import kieker.monitoring.core.controller.IMonitoringController;
 import kieker.monitoring.core.controller.MonitoringController;
 import kieker.monitoring.registry.IRegistryListener;
-import kieker.monitoring.registry.IWriterRegistry;
 import kieker.monitoring.registry.WriterRegistry;
 import kieker.monitoring.writernew.AbstractMonitoringWriter;
 
 /**
- * @author Matthias Rohr, Robert von Massow, Andre van Hoorn, Jan Waller, Christian Wulf
+ * @author Jan Waller, Christian Wulf
  *
- * @since < 0.9
+ * @since 1.9
  */
-public class AsciiFileWriter extends AbstractMonitoringWriter implements IRegistryListener<String> {
+public class BinaryFileWriter extends AbstractMonitoringWriter implements IRegistryListener<String> {
 
-	private static final String PREFIX = AsciiFileWriter.class.getName() + ".";
+	private static final String PREFIX = BinaryFileWriter.class.getName() + ".";
 	/** The name of the configuration for the custom storage path if the writer is advised not to store in the temporary directory. */
 	private static final String CONFIG_PATH = PREFIX + "customStoragePath";
-	/** The name of the configuration for the charset name (e.g. "UTF-8") */
-	private static final String CONFIG_CHARSETNAME = PREFIX + "charsetName";
 	/** The name of the configuration determining the maximal number of entries in a file. */
 	private static final String CONFIG_MAXENTRIESINFILE = PREFIX + "maxEntriesInFile";
 	/** The name of the configuration determining the maximal size of the files in MiB. */
@@ -53,19 +49,17 @@ public class AsciiFileWriter extends AbstractMonitoringWriter implements IRegist
 	/** The name of the configuration determining the maximal number of log files. */
 	private static final String CONFIG_MAXLOGFILES = PREFIX + "maxLogFiles";
 
-	private final FileWriterPool fileWriterPool;
+	private final BinaryFileWriterPool fileWriterPool;
 	private final MappingFileWriter mappingFileWriter;
+	private final WriterRegistry writerRegistry;
 
-	private final IWriterRegistry<String> writerRegistry;
-
-	public AsciiFileWriter(final Configuration configuration) {
+	public BinaryFileWriter(final Configuration configuration) {
 		super(configuration);
 		final Path folder = this.buildKiekerLogFolder(configuration.getStringProperty(CONFIG_PATH));
-		final String charsetName = configuration.getStringProperty(CONFIG_CHARSETNAME);
 		final int maxEntriesInFile = configuration.getIntProperty(CONFIG_MAXENTRIESINFILE);
 		// this.configMaxlogSize = configuration.getIntProperty(CONFIG_MAXLOGSIZE);
 		// this.configMaxLogFiles = configuration.getIntProperty(CONFIG_MAXLOGFILES);
-		this.fileWriterPool = new FileWriterPool(folder, charsetName, maxEntriesInFile);
+		this.fileWriterPool = new BinaryFileWriterPool(folder, maxEntriesInFile);
 		this.mappingFileWriter = new MappingFileWriter(folder, charsetName);
 
 		this.writerRegistry = new WriterRegistry(this);
@@ -91,37 +85,17 @@ public class AsciiFileWriter extends AbstractMonitoringWriter implements IRegist
 
 	@Override
 	public void writeMonitoringRecord(final IMonitoringRecord record) {
-		final String recordClassName = record.getClass().getName();
-		this.writerRegistry.register(recordClassName);
 
-		final PrintWriter fileWriter = this.fileWriterPool.getFileWriter();
-
-		fileWriter.print('$');
-		fileWriter.print(this.writerRegistry.getId(recordClassName));
-		fileWriter.print(';');
-		fileWriter.print(record.getLoggingTimestamp());
-		for (final Object recordField : record.toArray()) {
-			fileWriter.print(';');
-			fileWriter.print(String.valueOf(recordField));
-		}
-		fileWriter.println();
 	}
 
 	@Override
-	public void onNewRegistryEntry(final String recordClassName, final int id) {
-		final PrintWriter mappingFileWriter = this.mappingFileWriter.getFileWriter();
+	public void onNewRegistryEntry(final String value, final int id) {
 
-		mappingFileWriter.print('$');
-		mappingFileWriter.print(id);
-		mappingFileWriter.print('=');
-		mappingFileWriter.print(recordClassName);
-		mappingFileWriter.println();
 	}
 
 	@Override
 	public void onTerminating() {
-		this.fileWriterPool.close();
-		this.mappingFileWriter.close();
+
 	}
 
 }
