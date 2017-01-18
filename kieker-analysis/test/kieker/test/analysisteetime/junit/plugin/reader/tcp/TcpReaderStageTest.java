061cf13050c72fa35492171fe335bdc071a30cb7
Sören Henning <stu114708@informatik.uni-kiel.de>
2017-01-18 18:29:35 +0100

    migrated tcp reader tests

---
+++ kieker-analysis/test/kieker/test/analysisteetime/junit/plugin/reader/tcp/TcpReaderStageTest.java
@@ -0,0 +1,34 @@
+package kieker.test.analysisteetime.junit.plugin.reader.tcp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import kieker.analysisteetime.plugin.reader.tcp.TcpReaderStage;
+import kieker.common.record.IMonitoringRecord;
+
+import teetime.framework.test.StageTester;
+
+public class TcpReaderStageTest {
+
+	@Test
+	@Ignore
+	public void testNonStringRecordTransmission() throws Exception {
+		final List<IMonitoringRecord> receivedRecords = new ArrayList<IMonitoringRecord>();
+
+		final TcpReaderStage stage = new TcpReaderStage();
+		StageTester.test(stage).and()
+				.receive(receivedRecords).from(stage.getOutputPort())
+				.start();
+
+		final List<IMonitoringRecord> sentRecords = new ArrayList<IMonitoringRecord>();
+		// TODO
+
+		MatcherAssert.assertThat(receivedRecords, Is.is(IsEqual.equalTo(sentRecords)));
+	}
+}
