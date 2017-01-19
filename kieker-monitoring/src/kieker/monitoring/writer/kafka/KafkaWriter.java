c6f6d5c4134d123fa42c301bb8008caec5546e60
Holger Knoche <hkn@informatik.uni-kiel.de>
2017-01-19 10:48:16 +0100

    Added Kafka reader and writer

---
+++ kieker-monitoring/src/kieker/monitoring/writer/kafka/KafkaWriter.java
@@ -0,0 +1,110 @@
+/***************************************************************************
+ * Copyright 2017 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+package kieker.monitoring.writer.kafka;
+
+import java.nio.ByteBuffer;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import kieker.common.configuration.Configuration;
+import kieker.monitoring.writer.raw.AbstractRawDataWriter;
+
+/**
+ * Raw data writer which sends monitoring records to a Kafka topic.
+ *
+ * @author Holger Knoche
+ *
+ * @since 1.13
+ */
+public class KafkaWriter extends AbstractRawDataWriter {
+	
+	private static final String PREFIX = KafkaWriter.class.getName() + ".";
+	
+	/** The name of the configuration property for the topic name. */
+	public static final String CONFIG_PROPERTY_TOPIC_NAME = PREFIX + "topicName";
+	/** The name of the configuration property for the bootstrap servers. */
+	public static final String CONFIG_PROPERTY_BOOTSTRAP_SERVERS = PREFIX + "bootstrapServers";
+	/** The name of the configuration property for the acks parameter. */
+	public static final String CONFIG_PROPERTY_ACKS = PREFIX + "acks";
+	/** The name of the configuration property for the batch size. */
+	public static final String CONFIG_PROPERTY_BATCH_SIZE = PREFIX + "batchSize";
+	/** The name of the configuration property for the linger interval. */
+	public static final String CONFIG_PROPERTY_LINGER_MS = PREFIX + "lingerMs";
+	/** The name of the configuration property for the buffer memory size. */
+	public static final String CONFIG_PROPERTY_BUFFER_MEMORY = PREFIX + "bufferMemory";
+	
+	private static final String DEFAULT_ACKS = "all";
+	private static final int DEFAULT_BATCH_SIZE = 16384;
+	private static final int DEFAULT_LINGER_MS = 1;
+	private static final int DEFAULT_BUFFER_MEMORY = 32 << 20;	
+
+	private final String bootstrapServers;
+	private final String topicName;
+	private final String acknowledges;
+	private final int lingerMs;
+	private final int batchSize;
+	private final int bufferMemory;
+	
+	private Producer<String, byte[]> producer;
+	
+	public KafkaWriter(final Configuration configuration) {
+		super(configuration);
+		
+		this.bootstrapServers = configuration.getStringProperty(CONFIG_PROPERTY_BOOTSTRAP_SERVERS);
+		this.topicName = configuration.getStringProperty(CONFIG_PROPERTY_TOPIC_NAME);
+		this.acknowledges = configuration.getStringProperty(CONFIG_PROPERTY_ACKS, DEFAULT_ACKS);
+		this.lingerMs = configuration.getIntProperty(CONFIG_PROPERTY_LINGER_MS, DEFAULT_LINGER_MS);
+		this.batchSize = configuration.getIntProperty(CONFIG_PROPERTY_BATCH_SIZE, DEFAULT_BATCH_SIZE);
+		this.bufferMemory = configuration.getIntProperty(CONFIG_PROPERTY_BUFFER_MEMORY, DEFAULT_BUFFER_MEMORY);
+	}
+
+	@Override
+	public void init() throws Exception {
+		Properties properties = new Properties();
+		
+		properties.put("bootstrap.servers", this.bootstrapServers);
+		properties.put("acks", this.acknowledges);
+		properties.put("batch.size", this.batchSize);
+		properties.put("linger.ms", this.lingerMs);
+		properties.put("buffer.memory", this.bufferMemory);
+		
+		properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		properties.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+		
+		this.producer = new KafkaProducer<String, byte[]>(properties);
+	}
+
+	@Override
+	public void terminate() {
+		if (this.producer != null) {
+			this.producer.close();
+		}
+	}
+
+	@Override
+	public void writeData(final ByteBuffer buffer, final int offset, final int length) {
+		buffer.position(offset);
+		byte[] rawData = new byte[length];
+		buffer.get(rawData);
+		
+		ProducerRecord<String, byte[]> record = new ProducerRecord<String, byte[]>(this.topicName, rawData);
+		this.producer.send(record);
+	}
+
+}
