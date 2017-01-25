1e154544c18d5b50cda0e0ab2dd8f6461de62937
Lars Blümke <lbl@informatik.uni-kiel.de>
2017-01-25 11:19:04 +0100

    KIEKER-1500 Moved tests for imported readers to right package

--- kieker-analysis/test/kieker/test/analysisteetime/junit/plugin/reader/tcp/TcpReaderConfig.java
+++ kieker-analysis/test/kieker/test/analysisteetime/junit/plugin/reader/tcp/TcpReaderConfig.java
@@ -1,4 +1,4 @@
-package kieker.analysisteetime.plugin.reader.tcp;
+package kieker.test.analysisteetime.junit.plugin.reader.tcp;
 
 import kieker.analysisteetime.plugin.reader.tcp.TcpReaderStage;
 import kieker.common.record.IMonitoringRecord;
