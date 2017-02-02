7deead9637be8270d5cfd046326c5ca15f4e2519
Sören Henning <stu114708@informatik.uni-kiel.de>
2017-02-02 15:24:26 +0100

    added Javadoc

--- kieker-analysis/src/kieker/analysisteetime/util/graph/export/dot/SimpleDotExportConfiguration.java
+++ kieker-analysis/src/kieker/analysisteetime/util/graph/export/dot/SimpleDotExportConfiguration.java
@@ -6,6 +6,15 @@ import kieker.analysisteetime.util.graph.mapping.DirectPropertyMapper;
 import kieker.analysisteetime.util.graph.util.dot.attributes.DotEdgeAttribute;
 import kieker.analysisteetime.util.graph.util.dot.attributes.DotNodeAttribute;
 
+/**
+ * A {@link DotExportConfiguration} that maps the property <em>label</em> of edges
+ * and vertices to the <em>label</em> attribute of the corresponding nodes and
+ * edges of the dot graph.
+ *
+ * @author Sören Henning
+ *
+ * @since 1.13
+ */
 public class SimpleDotExportConfiguration extends DotExportConfiguration {
 
 	public SimpleDotExportConfiguration() {
