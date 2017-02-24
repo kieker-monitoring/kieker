b93c2e6afaf4ac43dc633566b3eec01de08a0888
Christian Wulf <chw@informatik.uni-kiel.de>
2017-02-24 14:36:52 +0100

    fixed findbugs issues

--- kieker-monitoring/test/kieker/test/monitoring/junit/writer/collector/TestRawDataStorage.java
+++ kieker-monitoring/test/kieker/test/monitoring/junit/writer/collector/TestRawDataStorage.java
@@ -33,6 +33,7 @@ import java.util.Map;
 public final class TestRawDataStorage {
 
 	private static final TestRawDataStorage INSTANCE = new TestRawDataStorage();
+	private static final ByteArrayOutputStream EMPTY_STREAM = new ByteArrayOutputStream();
 
 	private final Map<String, ByteArrayOutputStream> rawData;
 
@@ -76,17 +77,17 @@ public final class TestRawDataStorage {
 	 *
 	 * @param id
 	 *            The ID to retrieve the data for
-	 * @return The stored data, or {@code null} if no data exists
+	 * @return The stored data (empty if no data exists)
 	 */
 	public byte[] getData(final String id) {
-        synchronized(this) {
-            final ByteArrayOutputStream stream = this.rawData.get(id);
+		synchronized (this) {
+			ByteArrayOutputStream stream = this.rawData.get(id);
 
-            if (stream == null) {
-                return null;
-            }
+			if (stream == null) {
+				stream = EMPTY_STREAM;
+			}
 
-            return stream.toByteArray();
-        }
+			return stream.toByteArray();
+		}
 	}
 }
