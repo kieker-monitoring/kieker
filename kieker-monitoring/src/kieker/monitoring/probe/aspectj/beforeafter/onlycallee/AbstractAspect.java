0e4288c576bb7fa3b67bc8f3b71e3fe8e8e938eb
Christian Wulf <chw@informatik.uni-kiel.de>
2017-07-12 21:31:15 +0200

    fixed syntax errors

--- kieker-monitoring/src/kieker/monitoring/probe/aspectj/beforeafter/onlycallee/AbstractAspect.java
+++ kieker-monitoring/src/kieker/monitoring/probe/aspectj/beforeafter/onlycallee/AbstractAspect.java
@@ -21,13 +21,27 @@ import kieker.monitoring.core.registry.TraceRegistry;
 import kieker.monitoring.probe.aspectj.AbstractAspectJProbe;
 import kieker.monitoring.timer.ITimeSource;
 
+/**
+ * This aspect spawns before and after events by foregoing an around advice.
+ * Instead, it uses before and after advices only so that "cflow" can be used when specifying its pointcut.
+ * 
+ * This implementation uses <code>JoinPoint.StaticPart</code> instead of <code>JoinPoint</code> in the advices for performance reasons:
+ * <blockquote>If you only need the static information about the join point, you may access the static part of the join point directly with the special variable
+ * thisJoinPointStaticPart.
+ * Using thisJoinPointStaticPart will avoid the run-time creation of the join point object that may be necessary when using thisJoinPoint directly.
+ * </blockquote>
+ * 
+ * @author Christian Wulf (chw)
+ *
+ */
 @Aspect
 public abstract class AbstractAspect extends AbstractAspectJProbe {
+
 	private static final IMonitoringController CTRLINST = MonitoringController.getInstance();
 	private static final ITimeSource TIME = CTRLINST.getTimeSource();
 	private static final TraceRegistry TRACEREGISTRY = TraceRegistry.INSTANCE;
 
-	private final Map<JoinPoint, TraceMetadata> entryJoinPoints = new ConcurrentHashMap<>();
+	private final Map<JoinPoint.StaticPart, TraceMetadata> entryJoinPoints = new ConcurrentHashMap<>();
 
 	/**
 	 * The pointcut for the monitored operations. Inheriting classes should
@@ -37,12 +51,12 @@ public abstract class AbstractAspect extends AbstractAspectJProbe {
 	@Pointcut
 	public abstract void monitoredOperation();
 
-	@Before("monitoredOperation() && target(calleeObject) && notWithinKieker()")
-	public void beforeOperation(final JoinPoint thisJoinPoint, final Object calleeObject) throws Throwable { // NOCS (Throwable)
+	@Before("monitoredOperation() && notWithinKieker()")
+	public void beforeOperation(final JoinPoint.StaticPart jpStaticPart) throws Throwable { // NOCS (Throwable)
 		if (!CTRLINST.isMonitoringEnabled()) {
 			return;
 		}
-		final String operationSignature = this.signatureToLongString(thisJoinPoint.getSignature());
+		final String operationSignature = this.signatureToLongString(jpStaticPart.getSignature());
 		if (!CTRLINST.isProbeActivated(operationSignature)) {
 			return;
 		}
@@ -52,14 +66,14 @@ public abstract class AbstractAspect extends AbstractAspectJProbe {
 		if (newTrace) {
 			trace = TRACEREGISTRY.registerTrace(); // TODO parent trace is never used, so reduce impl. (chw)
 			CTRLINST.newMonitoringRecord(trace);
-			this.entryJoinPoints.put(thisJoinPoint, trace);
+			this.entryJoinPoints.put(jpStaticPart, trace);
 		}
 
 		final long traceId = trace.getTraceId();
-		final String clazz = calleeObject.getClass().getName();
+		final String typeName = jpStaticPart.getSignature().getDeclaringTypeName();
 		// measure before execution
 		CTRLINST.newMonitoringRecord(
-				new BeforeOperationEvent(TIME.getTime(), traceId, trace.getNextOrderId(), operationSignature, clazz));
+				new BeforeOperationEvent(TIME.getTime(), traceId, trace.getNextOrderId(), operationSignature, typeName));
 	}
 
 	// @Around("monitoredOperation() && notWithinKieker()")
@@ -82,25 +96,41 @@ public abstract class AbstractAspect extends AbstractAspectJProbe {
 	// }
 	// }
 
-	@AfterReturning("monitoredOperation() && target(calleeObject) && notWithinKieker()")
-	public void afterOperation(final JoinPoint thisJoinPoint, Object calleeObject) {
-		final String operationSignature = this.signatureToLongString(thisJoinPoint.getSignature());
-		final String clazz = calleeObject.getClass().getName();
+	@AfterReturning("monitoredOperation() && notWithinKieker()")
+	public void afterReturningOperation(final JoinPoint.StaticPart jpStaticPart) {
+		System.out.println("AbstractAspect.afterReturningOperation()");
+		final TraceMetadata trace = TRACEREGISTRY.getTrace();
+		// this check indirectly includes the checks for isMonitoringEnabled and isProbeActivated
+		if (trace == null) {
+			return;
+		}
 
-		CTRLINST.newMonitoringRecord(new AfterOperationEvent(TIME.getTime(), traceId, trace.getNextOrderId(), operationSignature, clazz));
+		final String operationSignature = this.signatureToLongString(jpStaticPart.getSignature());
+		final String typeName = jpStaticPart.getSignature().getDeclaringTypeName();
+
+		CTRLINST.newMonitoringRecord(new AfterOperationEvent(TIME.getTime(), trace.getTraceId(), trace.getNextOrderId(), operationSignature, typeName));
 	}
 
-	@AfterThrowing(pointcut = "monitoredOperation() && target(calleeObject) && notWithinKieker()", throwing = "th")
-	public void afterThrowing(final JoinPoint thisJoinPoint, Object calleeObject, Throwable th) {
-		final String operationSignature = this.signatureToLongString(thisJoinPoint.getSignature());
-		final String clazz = calleeObject.getClass().getName();
+	@AfterThrowing(pointcut = "monitoredOperation() && notWithinKieker()", throwing = "th")
+	public void afterThrowing(final JoinPoint.StaticPart jpStaticPart, Throwable th) {
+		System.out.println("AbstractAspect.afterThrowing()");
+		final TraceMetadata trace = TRACEREGISTRY.getTrace();
+		// this check indirectly includes the checks for isMonitoringEnabled and isProbeActivated
+		if (trace == null) {
+			return;
+		}
 
-		CTRLINST.newMonitoringRecord(new AfterOperationFailedEvent(TIME.getTime(), traceId, trace.getNextOrderId(), operationSignature, clazz, th.toString()));
+		final String operationSignature = this.signatureToLongString(jpStaticPart.getSignature());
+		final String typeName = jpStaticPart.getSignature().getDeclaringTypeName();
+
+		CTRLINST.newMonitoringRecord(
+				new AfterOperationFailedEvent(TIME.getTime(), trace.getTraceId(), trace.getNextOrderId(), operationSignature, typeName, th.toString()));
 	}
 
 	@After("monitoredOperation() && notWithinKieker()")
-	public void afterOperation(final JoinPoint thisJoinPoint) {
-		if (this.entryJoinPoints.remove(thisJoinPoint) != null) {
+	public void afterOperation(final JoinPoint.StaticPart jpStaticPart) {
+		System.out.println("AbstractAspect.afterOperation()");
+		if (this.entryJoinPoints.remove(jpStaticPart) != null) {
 			TRACEREGISTRY.unregisterTrace();
 		}
 	}
