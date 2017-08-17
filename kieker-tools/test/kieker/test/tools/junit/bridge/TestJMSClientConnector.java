224b1a97dfa56b5c06f44d3672f8386c2688f93f
Thomas F. Düllmann <duellmtk@informatik.uni-stuttgart.de>
2017-08-17 15:19:03 +0200

    KIEKER-1328: re-enabled the previously ignored tests.

--- kieker-tools/test/kieker/test/tools/junit/bridge/TestJMSClientConnector.java
+++ kieker-tools/test/kieker/test/tools/junit/bridge/TestJMSClientConnector.java
@@ -44,8 +44,7 @@ public class TestJMSClientConnector extends AbstractConnectorTest {
 	 * @throws ConnectorDataTransmissionException
 	 *             on lookup failure for the test record
 	 */
-	@Test // NOCS (ignore test until it was fixed)
-	@Ignore
+	@Test
 	public void testJMSClientConnector() throws ConnectorDataTransmissionException { // NOPMD
 		final Thread messageGenerator = new Thread(new JMSMessageGenerator(ConfigurationParameters.JMS_URI, ConfigurationParameters.JMS_CLIENT_FACTORY_LOOKUP_NAME),
 				"Generator");
