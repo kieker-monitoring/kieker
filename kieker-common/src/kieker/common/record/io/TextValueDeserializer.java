166cba1596f4dfc181e7d3ee8ef9f8da27b08763
Reiner Jung <reiner.jung@email.uni-kiel.de>
2017-09-06 15:30:54 +0200

    added TextValue(De)serializer and adapted the AsciiFileWriter to them.

---
+++ kieker-common/src/kieker/common/record/io/TextValueDeserializer.java
@@ -0,0 +1,112 @@
+/***************************************************************************
+ * Copyright 2017 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+package kieker.common.record.io;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+
+/**
+ * Text value deserializer implementation.
+ *
+ * @author Reiner Jung
+ * @since 1.13
+ *
+ */
+public class TextValueDeserializer implements IValueDeserializer {
+
+	private final CharBuffer buffer;
+
+	protected TextValueDeserializer(final CharBuffer buffer) {
+		this.buffer = buffer;
+	}
+
+	public static TextValueDeserializer create(final CharBuffer buffer) {
+		return new TextValueDeserializer(buffer);
+	}
+
+	private String readValue() {
+		String result = "";
+		char ch;
+		boolean escape = false;
+		do {
+			escape = false;
+			ch = this.buffer.get();
+			if ((ch == '\\') && !escape) {
+				escape = true;
+			}
+			if (escape || (ch != ';')) {
+				result += ch;
+			}
+		} while (escape || (ch != ';'));
+		return result;
+	}
+
+	@Override
+	public boolean getBoolean() {
+		return Boolean.parseBoolean(this.readValue());
+	}
+
+	@Override
+	public byte getByte() {
+		return Byte.parseByte(this.readValue());
+	}
+
+	@Override
+	public char getChar() {
+		final char ch = this.buffer.get();
+		this.buffer.get(); /** reading semicolon. */
+		return ch;
+	}
+
+	@Override
+	public short getShort() {
+		return Short.parseShort(this.readValue());
+	}
+
+	@Override
+	public int getInt() {
+		return Integer.parseInt(this.readValue());
+	}
+
+	@Override
+	public long getLong() {
+		return Long.parseLong(this.readValue());
+	}
+
+	@Override
+	public float getFloat() {
+		return Float.parseFloat(this.readValue());
+	}
+
+	@Override
+	public double getDouble() {
+		return Double.parseDouble(this.readValue());
+	}
+
+	@Override
+	public String getString() {
+		return this.readValue();
+	}
+
+	@Override
+	public byte[] getBytes(final byte[] target) {
+		final char[] charTarget = new char[target.length];
+		this.buffer.get(charTarget);
+		ByteBuffer.wrap(target).asCharBuffer().put(charTarget);
+		return target;
+	}
+
+}
