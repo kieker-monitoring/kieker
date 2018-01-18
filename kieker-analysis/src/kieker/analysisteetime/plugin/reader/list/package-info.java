401b42ac6756bd1d3e837562dfb2bdaf1b84a3a0
Christian Wulf <chw@informatik.uni-kiel.de>
2018-01-18 11:14:52 +0100

    removed jmx reader and associated integration test; added package-info files for legacy packages; fixed raw type warnings in PlotStep

---
+++ kieker-analysis/src/kieker/analysisteetime/plugin/reader/list/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The original legacy reader implementation <code>ListReader</code> has been removed as of v1.14. Use
+ * {@link teetime.stage.InitialElementProducer} instead.
+ */
+package kieker.analysisteetime.plugin.reader.list;
---
+++ kieker-analysis/src/kieker/analysisteetime/plugin/reader/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Contains reader implementations using several different technologies.
+ * <p>
+ * There is no general reader implementation for
+ * <ul>
+ * <li>databases</li>
+ * <li>JMX objects</li>
+ * </ul>
+ */
+package kieker.analysisteetime.plugin.reader;
