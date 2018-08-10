2a3c5298dc302c5e0215de4f3f365fb32b288ae1
Holger Knoche <hkn@informatik.uni-kiel.de>
2018-08-10 17:25:53 +0200

    Binary wrapper needs to clear instead of rewind

--- kieker-monitoring/src/kieker/monitoring/writer/serializer/BinaryChunkLengthWrapper.java
+++ kieker-monitoring/src/kieker/monitoring/writer/serializer/BinaryChunkLengthWrapper.java
@@ -24,26 +24,26 @@ import java.nio.ByteBuffer;
  * @author Holger Knoche
  * @since 2.0
  */
-public class BinaryChunkLengthWrapper extends AbstractBinaryDataWrapper {	
+public class BinaryChunkLengthWrapper extends AbstractBinaryDataWrapper {
 
 	private final ByteBuffer outputBuffer;
-	
+
 	public BinaryChunkLengthWrapper(final int sourceBufferSize) {
 		// The wrapper adds an int, i.e. 4 bytes
-		final int requiredOutputBufferSize = sourceBufferSize + 4;		
+		final int requiredOutputBufferSize = sourceBufferSize + 4;
 		this.outputBuffer = ByteBuffer.allocate(requiredOutputBufferSize);
 	}
-	
+
 	@Override
 	public ByteBuffer wrap(final ByteBuffer data) {
 		final ByteBuffer targetBuffer = this.outputBuffer;
 		final int dataSize = data.limit();
-		
+
 		// Rewind the target buffer and prepend the data length
-		targetBuffer.rewind();
+		targetBuffer.clear();
 		targetBuffer.putInt(dataSize);
 		targetBuffer.put(data);
-		
+
 		return targetBuffer;
 	}
 
