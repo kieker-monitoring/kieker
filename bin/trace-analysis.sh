29e660cd3a22e5ff799b3a4e6b89298329313e67
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-10-29 09:16:23 +0100

    switched back to old analysis main

--- bin/trace-analysis.sh
+++ bin/trace-analysis.sh
@@ -8,7 +8,7 @@
 BINDIR=$(cd "$(dirname "$0")"; pwd)/
 
 JAVAARGS="-Dkieker.common.logging.Log=JDK -Djava.util.logging.config.file=${BINDIR}/logging.properties -Xms56m -Xmx1024m"
-MAINCLASSNAME=kieker.tools.trace.analysis.TraceAnalysisToolMain
+MAINCLASSNAME=kieker.tools.trace.analysis.TraceAnalysisTool
 
 #echo java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} "$@"
 
