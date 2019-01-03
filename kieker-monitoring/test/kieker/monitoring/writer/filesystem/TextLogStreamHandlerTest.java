c4099331e42dbb6164f98fa4201e4fc46d1f3849
Danish Manzoor <stu213178@mail.uni-kiel.de>
2019-01-03 20:15:07 +0500

    new test files created

---
+++ kieker-monitoring/test/kieker/monitoring/writer/filesystem/TextLogStreamHandlerTest.java
@@ -0,0 +1,45 @@
+/***************************************************************************
+ * Copyright 2018 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+package kieker.monitoring.writer.filesystem;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Danish Manzoor
+ *
+ */
+public class TextLogStreamHandlerTest {
+
+	/**
+	 * Test method for
+	 * {@link kieker.monitoring.writer.filesystem.TextLogStreamHandler#serialize(kieker.common.record.IMonitoringRecord, int)}.
+	 */
+	@Test
+	public void testSerialize() {
+		Assert.fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link kieker.monitoring.writer.filesystem.TextLogStreamHandler#TextLogStreamHandler(java.lang.Boolean, java.lang.Integer, java.nio.charset.Charset, kieker.monitoring.writer.compression.ICompressionFilter, kieker.common.registry.writer.WriterRegistry)}.
+	 */
+	@Test
+	public void testTextLogStreamHandler() {
+		Assert.fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ kieker-monitoring/test/kieker/monitoring/writer/filesystem/TextMapFileHandlerTest.java
@@ -0,0 +1,63 @@
+/***************************************************************************
+ * Copyright 2018 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+package kieker.monitoring.writer.filesystem;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Danish Manzoor
+ *
+ */
+public class TextMapFileHandlerTest {
+
+	/**
+	 * Test method for
+	 * {@link kieker.monitoring.writer.filesystem.TextMapFileHandler#TextMapFileHandler(kieker.common.configuration.Configuration)}.
+	 */
+	@Test
+	public void testTextMapFileHandler() {
+		Assert.fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link kieker.monitoring.writer.filesystem.TextMapFileHandler#create(java.nio.file.Path, java.nio.charset.Charset)}.
+	 */
+	@Test
+	public void testCreate() {
+		Assert.fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link kieker.monitoring.writer.filesystem.TextMapFileHandler#close()}.
+	 */
+	@Test
+	public void testClose() {
+		Assert.fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link kieker.monitoring.writer.filesystem.TextMapFileHandler#add(int, java.lang.String)}.
+	 */
+	@Test
+	public void testAdd() {
+		Assert.fail("Not yet implemented"); // TODO
+	}
+
+}
