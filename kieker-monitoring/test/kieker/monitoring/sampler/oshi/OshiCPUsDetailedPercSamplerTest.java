0a507102a619a5f21e4a0c08258b3a3d41af0005
Thomas F. Düllmann <duellmtk@informatik.uni-stuttgart.de>
2019-03-29 13:37:20 +0100

    KIEKER-1768: Fixed header issues and added since tags.

--- kieker-monitoring/test/kieker/monitoring/sampler/oshi/OshiCPUsDetailedPercSamplerTest.java
+++ kieker-monitoring/test/kieker/monitoring/sampler/oshi/OshiCPUsDetailedPercSamplerTest.java
@@ -1,3 +1,19 @@
+/***************************************************************************
+ * Copyright 2019 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
 package kieker.monitoring.sampler.oshi;
 
 import org.junit.Before;
@@ -6,6 +22,11 @@ import org.junit.Test;
 import kieker.monitoring.core.controller.IMonitoringController;
 import kieker.monitoring.core.controller.MonitoringController;
 import kieker.monitoring.sampler.oshi.samplers.CPUsDetailedPercSampler;
+
+/**
+ * @since 1.14
+ *
+ */
 
 public class OshiCPUsDetailedPercSamplerTest {
 
