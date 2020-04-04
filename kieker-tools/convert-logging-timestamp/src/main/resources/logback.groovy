9b3deabaf1fb8ed1ca55a9577900a7a671626d1b
Reiner Jung <reiner.jung@email.uni-kiel.de>
2020-04-04 19:29:00 +0200

    KIEKER-1815 Switched Logging facility setup to avoid security issues with log4j 1.2.* and enable logging in a separate file.

---
+++ kieker-tools/convert-logging-timestamp/src/main/resources/logback.groovy
@@ -0,0 +1,36 @@
+import ch.qos.logback.classic.filter.ThresholdFilter
+import ch.qos.logback.core.filter.EvaluatorFilter
+import ch.qos.logback.classic.boolex.GEventEvaluator
+
+import static ch.qos.logback.core.spi.FilterReply.DENY
+import static ch.qos.logback.core.spi.FilterReply.NEUTRAL
+
+appender("FILE", FileAppender) {
+  filter(ThresholdFilter) {
+    level=INFO
+  }
+  file = "testFile.log"
+  append = true
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+  }
+}
+
+appender("STDOUT", ConsoleAppender) {
+  filter(EvaluatorFilter) {
+    evaluator(GEventEvaluator) {
+      expression = 'e.level.toInt() == INFO.toInt()'
+    }
+    onMismatch = DENY
+    onMatch = NEUTRAL
+  }
+  encoder(PatternLayoutEncoder) {
+    pattern = "<> %d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+  }
+  target="System.out"
+}
+
+root(DEBUG, ["STDOUT", "FILE"])
+
+
+
