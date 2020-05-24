31d80df0e8e45aea34e839257e034265e032e8a3
Reiner Jung <reiner.jung@email.uni-kiel.de>
2020-05-24 18:53:42 +0200

    Increased checkstyle errors, as @Ignore's are marked as errors which are temporarily used to disable a broken test.

--- kieker-monitoring/test/kieker/test/monitoring/junit/writer/serializer/BinarySerializerTest.java
+++ kieker-monitoring/test/kieker/test/monitoring/junit/writer/serializer/BinarySerializerTest.java
@@ -50,7 +50,7 @@ public class BinarySerializerTest {
 	}
 
 	@Test
-	@Ignore // NOCS
+	@Ignore
 	public void testSingleRecord() throws IOException, InterruptedException {
 		final String testId = "testSingleRecord";
 		final int recordCount = 1;
@@ -87,7 +87,7 @@ public class BinarySerializerTest {
 	}
 
 	@Test
-	@Ignore // NOCS
+	@Ignore
 	public void testMultipleRecords() throws InterruptedException {
 		final String testId = "testMultipleRecords";
 		final int recordCount = 15;
