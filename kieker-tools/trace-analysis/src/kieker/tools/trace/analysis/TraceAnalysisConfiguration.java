87ec5e03418f2a78d96d950f3fc7786c1f431cf6
Reiner Jung <reiner.jung@email.uni-kiel.de>
2020-12-26 15:26:41 +0100

    KIEKER-1324 Minor cleanup in tooling.

--- kieker-tools/trace-analysis/src/kieker/tools/trace/analysis/TraceAnalysisConfiguration.java
+++ kieker-tools/trace-analysis/src/kieker/tools/trace/analysis/TraceAnalysisConfiguration.java
@@ -16,6 +16,7 @@
 package kieker.tools.trace.analysis;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -38,193 +39,124 @@ import kieker.tools.common.DateConverter;
  */
 public class TraceAnalysisConfiguration {
 
-	private static final String CMD_HELP = "--help";
-
-	private static final String CMD_INPUTDIRS = "--inputdirs";
-
-	private static final String CMD_DIR = "--outputdir";
-
-	private static final String CMD_OUTPUT_FILENAME_PREFIX = "--output-filename-prefix";
-
-	private static final String CMD_PLOT_DEPLOYMENT_SEQUENCE_DIAGRAMS = "--plot-Deployment-Sequence-Diagrams";
-
-	private static final String CMD_PLOT_ASSEMBLY_SEQUENCE_DIAGRAMS = "--plot-Assembly-Sequence-Diagrams";
-
-	private static final String CMD_PLOT_DEPLOYMENT_COMPONENT_DEPENDENCY_GRAPH = "--plot-Deployment-Component-Dependency-Graph";
-
-	private static final String CMD_PLOT_ASSEMBLY_COMPONENT_DEPENDENCY_GRAPH = "--plot-Assembly-Component-Dependency-Graph";
-
-	private static final String CMD_PLOT_CONTAINER_DEPENDENCY_GRAPH = "--plot-Container-Dependency-Graph";
-
-	private static final String CMD_PLOT_DEPLOYMENT_OPERATION_DEPENDENCY_GRAPH = "--plot-Deployment-Operation-Dependency-Graph";
-
-	private static final String CMD_PLOT_ASSEMBLY_OPERATION_DEPENDENCY_GRAPH = "--plot-Assembly-Operation-Dependency-Graph";
-
-	private static final String CMD_PLOT_AGGREGATED_DEPLOYMENT_CALL_TREE = "--plot-Aggregated-Deployment-Call-Tree";
-
-	private static final String CMD_PLOT_AGGREGATED_ASSEMBLY_CALL_TREE = "--plot-Aggregated-Assembly-Call-Tree";
-
-	private static final String CMD_PLOT_CALL_TREES = "--plot-Call-Trees";
-
-	private static final String CMD_PRINT_MESSAGE_TRACES = "--print-Message-Traces";
-
-	private static final String CMD_PRINT_EXECUTION_TRACES = "--print-Execution-Traces";
-
-	private static final String CMD_PRINT_INVALID_EXECUTION_TRACES = "--print-invalid-Execution-Traces";
-
-	private static final String CMD_PRINT_SYSTEM_MODEL = "--print-System-Model";
-
-	private static final String CMD_PRINT_DEPLOYMENT_EQUIVALENCE_CLASSES = "--print-Deployment-Equivalence-Classes";
-
-	private static final String CMD_PRINT_ASSEMBLY_EQUIVALENCE_CLASSES = "--print-Assembly-Equivalence-Classes";
-
-	private static final String CMD_SELECT_TRACES = "--select-traces";
-
-	private static final String CMD_FILTER_TRACES = "--filter-traces";
-
-	private static final String CMD_IGNORE_INVALID_TRACES = "--ignore-invalid-traces";
-
-	private static final String CMD_REPAIR_EVENT_BASED_TRACES = "--repair-event-based-traces";
-
-	private static final String CMD_MAX_TRACE_DURATION = "--max-trace-duration";
-
-	private static final String CMD_IGNORE_EXECUTIONS_BEFORE_DATE = "--ignore-executions-before-date";
-
-	private static final String CMD_IGNORE_EXECUTIONS_AFTER_DATE = "--ignore-executions-after-date";
-
-	private static final String CMD_SHORT_LABELS = "--short-labels";
-
-	private static final String CMD_INCLUDE_SELF_LOOPS = "--include-self-loops";
-
-	private static final String CMD_IGNORE_ASSUMED_CALLS = "--ignore-assumed-calls";
-
-	private static final String CMD_TRACE_COLORING = "--traceColoring";
-
-	private static final String CMD_ADD_DESCRIPTIONS = "--addDescriptions";
-
-	private static final String CMD_VERBOSE = "--verbose";
-
-	private static final String CMD_DEBUG = "--debug";
-
-	@Parameter(names = { "-v", CMD_VERBOSE }, description = "verbosely prints additional information")
+	@Parameter(names = { "-v", "--verbose" }, description = "verbosely prints additional information")
 	private boolean verbose;
 
-	@Parameter(names = { "-d", CMD_DEBUG }, description = "prints additional debug information")
+	@Parameter(names = { "-d", "--debug" }, description = "prints additional debug information")
 	private boolean debug;
 
-	@Parameter(names = { "-h", CMD_HELP }, help = true, description = "prints the usage information for the tool, including available options")
+	@Parameter(names = { "-h", "--help" }, help = true, description = "prints the usage information for the tool, including available options")
 	private boolean help;
 
-	@Parameter(names = { "-i", CMD_INPUTDIRS }, variableArity = true, description = "Log directories to read data from")
+	@Parameter(names = { "-i", "--inputdirs" }, variableArity = true, description = "Log directories to read data from")
 	private List<File> inputDirs;
 
-	@Parameter(names = { "-o", CMD_DIR }, required = true, description = "Directory for the generated file(s)")
+	@Parameter(names = { "-o", "--outputdir" }, required = true, description = "Directory for the generated file(s)")
 	private File outputDir;
 
-	@Parameter(names = { "-p", CMD_OUTPUT_FILENAME_PREFIX }, description = "Prefix for output filenames")
+	@Parameter(names = { "-p", "--output-filename-prefix" }, description = "Prefix for output filenames")
 	private String prefix;
 
-	@Parameter(names = { CMD_PLOT_DEPLOYMENT_SEQUENCE_DIAGRAMS }, description = "Generate and store deployment-level sequence diagrams (.pic)")
+	@Parameter(names = { "--plot-Deployment-Sequence-Diagrams" }, description = "Generate and store deployment-level sequence diagrams (.pic)")
 	private boolean plotDeploymentSequenceDiagrams;
 
-	@Parameter(names = { CMD_PLOT_ASSEMBLY_SEQUENCE_DIAGRAMS }, description = "Generate and store assembly-level sequence diagrams (.pic)")
+	@Parameter(names = { "--plot-Assembly-Sequence-Diagrams" }, description = "Generate and store assembly-level sequence diagrams (.pic)")
 	private boolean plotAssemblySequenceDiagrams;
 
-	@Parameter(names = { CMD_PLOT_DEPLOYMENT_COMPONENT_DEPENDENCY_GRAPH }, description = "Generate and store a deployment-level component dependency graph (.dot)")
+	@Parameter(names = { "--plot-Deployment-Component-Dependency-Graph" }, description = "Generate and store a deployment-level component dependency graph (.dot)")
 	private List<String> plotDeploymentComponentDependencyGraph;
 
-	@Parameter(names = { CMD_PLOT_ASSEMBLY_COMPONENT_DEPENDENCY_GRAPH }, description = "Generate and store an assembly-level component dependency graph (.dot)")
+	@Parameter(names = { "--plot-Assembly-Component-Dependency-Graph" }, description = "Generate and store an assembly-level component dependency graph (.dot)")
 	private List<String> plotAssemblyComponentDependencyGraph;
 
-	@Parameter(names = { CMD_PLOT_CONTAINER_DEPENDENCY_GRAPH }, description = "Generate and store a container dependency graph (.dot file)")
+	@Parameter(names = { "--plot-Container-Dependency-Graph" }, description = "Generate and store a container dependency graph (.dot file)")
 	private boolean plotContainerDependencyGraph;
 
-	@Parameter(names = {
-		CMD_PLOT_DEPLOYMENT_OPERATION_DEPENDENCY_GRAPH }, variableArity = true,
+	@Parameter(names = { "--plot-Deployment-Operation-Dependency-Graph" }, variableArity = true,
 			description = "Generate and store a deployment-level operation dependency graph (.dot)")
 	private List<String> plotDeploymentOperationDependencyGraph;
 
-	@Parameter(names = {
-		CMD_PLOT_ASSEMBLY_OPERATION_DEPENDENCY_GRAPH }, variableArity = true, description = "Generate and store an assembly-level operation dependency graph (.dot)")
+	@Parameter(names = { "--plot-Assembly-Operation-Dependency-Graph" }, variableArity = true,
+			description = "Generate and store an assembly-level operation dependency graph (.dot)")
 	private List<String> plotAssemblyOperationDependencyGraph;
 
-	@Parameter(names = { CMD_PLOT_AGGREGATED_DEPLOYMENT_CALL_TREE }, description = "Generate and store an aggregated deployment-level call tree (.dot)")
+	@Parameter(names = { "--plot-Aggregated-Deployment-Call-Tree" },
+			description = "Generate and store an aggregated deployment-level call tree (.dot)")
 	private boolean plotAggregatedDeploymentCallTree;
 
-	@Parameter(names = { CMD_PLOT_AGGREGATED_ASSEMBLY_CALL_TREE }, description = "Generate and store an aggregated assembly-level call tree (.dot)")
+	@Parameter(names = { "--plot-Aggregated-Assembly-Call-Tree" },
+			description = "Generate and store an aggregated assembly-level call tree (.dot)")
 	private boolean plotAggregatedAssemblyCallTree;
 
-	@Parameter(names = { CMD_PLOT_CALL_TREES }, description = "Generate and store call trees for the selected traces (.dot)")
+	@Parameter(names = { "--plot-Call-Trees" }, description = "Generate and store call trees for the selected traces (.dot)")
 	private boolean plotCallTrees;
 
-	@Parameter(names = { CMD_PRINT_MESSAGE_TRACES }, description = "Save message trace representations of valid traces (.txt)")
+	@Parameter(names = { "--print-Message-Traces" }, description = "Save message trace representations of valid traces (.txt)")
 	private boolean printMessageTraces;
 
-	@Parameter(names = { CMD_PRINT_EXECUTION_TRACES }, description = "Save execution trace representations of valid traces (.txt)")
+	@Parameter(names = { "--print-Execution-Traces" }, description = "Save execution trace representations of valid traces (.txt)")
 	private boolean printExecutionTraces;
 
-	@Parameter(names = { CMD_PRINT_INVALID_EXECUTION_TRACES }, description = "Save a execution trace representations of invalid trace artifacts (.txt)")
+	@Parameter(names = { "--print-invalid-Execution-Traces" }, description = "Save a execution trace representations of invalid trace artifacts (.txt)")
 	private boolean printInvalidExecutionTraces;
 
-	@Parameter(names = { CMD_PRINT_SYSTEM_MODEL }, description = "Save a representation of the internal system model (.html)")
+	@Parameter(names = { "--print-System-Model" }, description = "Save a representation of the internal system model (.html)")
 	private boolean printSystemModel;
 
-	@Parameter(names = { CMD_PRINT_DEPLOYMENT_EQUIVALENCE_CLASSES }, description = "Output an overview about the deployment-level trace equivalence classes")
+	@Parameter(names = { "--print-Deployment-Equivalence-Classes" }, description = "Output an overview about the deployment-level trace equivalence classes")
 	private boolean printDeploymentEquivalenceClasses;
 
-	@Parameter(names = { CMD_PRINT_ASSEMBLY_EQUIVALENCE_CLASSES }, description = "Output an overview about the assembly-level trace equivalence classes")
+	@Parameter(names = { "--print-Assembly-Equivalence-Classes" }, description = "Output an overview about the assembly-level trace equivalence classes")
 	private boolean printAssemblyEquivalenceClasses;
 
-	@Parameter(names = {
-		CMD_SELECT_TRACES }, variableArity = true, description = "Consider only the traces identified by the list of trace IDs. Defaults to all traces.")
+	@Parameter(names = { "--select-traces" }, variableArity = true,
+			description = "Consider only the traces identified by the list of trace IDs. Defaults to all traces.")
 	private List<Long> selectTraces;
 
-	@Parameter(names = {
-		CMD_FILTER_TRACES }, variableArity = true, description = "Consider only the traces not identified by the list of trace IDs. Defaults to no traces.")
+	@Parameter(names = { "--filter-traces" }, variableArity = true,
+			description = "Consider only the traces not identified by the list of trace IDs. Defaults to no traces.")
 	private List<Long> filterTraces;
 
-	@Parameter(names = { CMD_IGNORE_INVALID_TRACES }, description = "If selected, the execution aborts on the occurence of an invalid trace.")
+	@Parameter(names = { "--ignore-invalid-traces" }, description = "If selected, the execution aborts on the occurence of an invalid trace.")
 	private boolean ignoreInvalidTraces;
 
-	@Parameter(names = {
-		CMD_REPAIR_EVENT_BASED_TRACES }, description = "If selected, BeforeEvents with missing AfterEvents e.g. because of software crash will be repaired.")
+	@Parameter(names = { "--repair-event-based-traces" },
+			description = "If selected, BeforeEvents with missing AfterEvents e.g. because of software crash will be repaired.")
 	private boolean repairEventBasedTraces;
 
 	// "duration in ms"
-	@Parameter(names = {
-		CMD_MAX_TRACE_DURATION }, description = "Threshold (in ms) after which incomplete traces become invalid. Defaults to 600,000 (i.e, 10 minutes).")
+	@Parameter(names = { "--max-trace-duration" },
+			description = "Threshold (in ms) after which incomplete traces become invalid. Defaults to 600,000 (i.e, 10 minutes).")
 	private Long maxTraceDuration;
 
 	// DATE_FORMAT_PATTERN_CMD_USAGE_HELP
-	@Parameter(names = {
-		CMD_IGNORE_EXECUTIONS_BEFORE_DATE }, description = "Executions starting before this date (UTC timezone) or monitoring timestamp are ignored.",
+	@Parameter(names = { "--ignore-executions-before-date" },
+			description = "Executions starting before this date (UTC timezone) or monitoring timestamp are ignored.",
 			converter = DateConverter.class)
 	private Long ignoreExecutionsBeforeDate;
 
-	@Parameter(names = {
-		CMD_IGNORE_EXECUTIONS_AFTER_DATE }, description = "Executions ending after this date (UTC timezone) or monitoring timestamp  are ignored.",
+	@Parameter(names = { "--ignore-executions-after-date" },
+			description = "Executions ending after this date (UTC timezone) or monitoring timestamp  are ignored.",
 			converter = DateConverter.class)
 	private Long ignoreExecutionsAfterDate;
 
-	@Parameter(names = { CMD_SHORT_LABELS }, description = "If selected, abbreviated labels (e.g., package names) are used in the visualizations.")
+	@Parameter(names = { "--short-labels" }, description = "If selected, abbreviated labels (e.g., package names) are used in the visualizations.")
 	private boolean shortLabels;
 
-	@Parameter(names = { CMD_INCLUDE_SELF_LOOPS }, description = "If selected, self-loops are included in the visualizations.")
+	@Parameter(names = { "--include-self-loops" }, description = "If selected, self-loops are included in the visualizations.")
 	private boolean includeSelfLoops;
 
-	@Parameter(names = { CMD_IGNORE_ASSUMED_CALLS }, description = "If selected, assumed calls are visualized just as regular calls.")
+	@Parameter(names = { "--ignore-assumed-calls" }, description = "If selected, assumed calls are visualized just as regular calls.")
 	private boolean ignoreAssumedCalls;
 
 	// COLORING_FILE_OPTION_NAME
-	@Parameter(names = {
-		CMD_TRACE_COLORING },
+	@Parameter(names = { "--traceColoring" },
 			description = "Color traces according to the given color map given as a properties file (key: trace ID, value: color in hex format,"
 					+ " e.g., 0xff0000 for red; use trace ID 'default' to specify the default color)")
 	private File traceColoringFile;
 
 	// DESCRIPTIONS_FILE_OPTION_NAME
-	@Parameter(names = {
-		CMD_ADD_DESCRIPTIONS },
+	@Parameter(names = { "--addDescriptions" },
 			description = "Adds descriptions to elements according to the given file as a properties file (key: component ID, e.g., @1; value: description)")
 	private File addDescriptions;
 
@@ -432,33 +364,23 @@ public class TraceAnalysisConfiguration {
 				val += ", " + directory.getAbsolutePath();
 			}
 		}
-		logger.debug("{}: {}", CMD_INPUTDIRS, val);
 
-		logger.debug("{}: {}", CMD_DIR, this.outputDir.getAbsolutePath());
-
-		logger.debug("{}: {}", CMD_OUTPUT_FILENAME_PREFIX, this.prefix);
-
-		logger.debug("{}: {}", CMD_VERBOSE, String.valueOf(this.verbose));
-		logger.debug("{}: {}", CMD_DEBUG, String.valueOf(this.debug));
-
-		logger.debug("{}: {}", CMD_PLOT_DEPLOYMENT_SEQUENCE_DIAGRAMS, String.valueOf(this.plotDeploymentSequenceDiagrams));
-		logger.debug("{}: {}", CMD_PLOT_ASSEMBLY_SEQUENCE_DIAGRAMS, String.valueOf(this.plotAssemblySequenceDiagrams));
-
-		logger.debug("{}: {}", CMD_PLOT_DEPLOYMENT_COMPONENT_DEPENDENCY_GRAPH, String.valueOf(this.plotDeploymentComponentDependencyGraph));
-		logger.debug("{}: {}", CMD_PLOT_ASSEMBLY_COMPONENT_DEPENDENCY_GRAPH, String.valueOf(this.plotAssemblyComponentDependencyGraph));
-		logger.debug("{}: {}", CMD_PLOT_CONTAINER_DEPENDENCY_GRAPH, String.valueOf(this.plotContainerDependencyGraph));
-		logger.debug("{}: {}", CMD_PLOT_DEPLOYMENT_OPERATION_DEPENDENCY_GRAPH, String.valueOf(this.plotDeploymentOperationDependencyGraph));
-		logger.debug("{}: {}", CMD_PLOT_ASSEMBLY_OPERATION_DEPENDENCY_GRAPH, String.valueOf(this.plotAssemblyOperationDependencyGraph));
-		logger.debug("{}: {}", CMD_PLOT_AGGREGATED_DEPLOYMENT_CALL_TREE, String.valueOf(this.plotAggregatedDeploymentCallTree));
-		logger.debug("{}: {}", CMD_PLOT_AGGREGATED_ASSEMBLY_CALL_TREE, String.valueOf(this.plotAggregatedAssemblyCallTree));
-		logger.debug("{}: {}", CMD_PLOT_CALL_TREES, String.valueOf(this.plotCallTrees));
-		logger.debug("{}: {}", CMD_PRINT_MESSAGE_TRACES, String.valueOf(this.printMessageTraces));
-		logger.debug("{}: {}", CMD_PRINT_EXECUTION_TRACES, String.valueOf(this.printExecutionTraces));
-		logger.debug("{}: {}", CMD_PRINT_INVALID_EXECUTION_TRACES, String.valueOf(this.printInvalidExecutionTraces));
-		logger.debug("{}: {}", CMD_PRINT_SYSTEM_MODEL, String.valueOf(this.printSystemModel));
-
-		logger.debug("{}: {}", CMD_PRINT_DEPLOYMENT_EQUIVALENCE_CLASSES, String.valueOf(this.printDeploymentEquivalenceClasses));
-		logger.debug("{}: {}", CMD_PRINT_ASSEMBLY_EQUIVALENCE_CLASSES, String.valueOf(this.printAssemblyEquivalenceClasses));
+		for (final Field field : this.getClass().getDeclaredFields()) {
+			final Parameter parameter = field.getAnnotation(Parameter.class);
+			if (parameter != null) {
+				final String[] names = parameter.names();
+				Object data = null;
+				try {
+					data = field.get(this);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
+				if (data == null) {
+					data = "<none>";
+				}
+				logger.debug("{}: {}", names[names.length - 1], data);
+			}
+		}
 
 		if (this.selectedTraces.isEmpty()) {
 			val = "<null>";
@@ -473,39 +395,22 @@ public class TraceAnalysisConfiguration {
 			val = "<select all>";
 		}
 		if (this.selectTraces != null) {
-			logger.debug("{}: {}", CMD_SELECT_TRACES, val);
+			logger.debug("{}: {}", "--selected-traces", val);
 		}
 		if (this.filterTraces != null) {
-			logger.debug("{}: {}", CMD_FILTER_TRACES, val);
+			logger.debug("{}: {}", "--filter-traces", val);
 		}
-
-		logger.debug("{}: {}", CMD_IGNORE_INVALID_TRACES, String.valueOf(this.ignoreInvalidTraces));
-		logger.debug("{}: {}", CMD_REPAIR_EVENT_BASED_TRACES, String.valueOf(this.repairEventBasedTraces));
-		logger.debug("{}: {} ms", CMD_MAX_TRACE_DURATION, String.valueOf(this.getMaxTraceDuration()));
-
-		logger.debug("{}: {} {}", CMD_IGNORE_EXECUTIONS_BEFORE_DATE, String.valueOf(this.getIgnoreExecutionsBeforeDate()),
-				dateFormat.format(this.getIgnoreExecutionsBeforeDate()));
-
-		logger.debug("{}: {}", CMD_IGNORE_EXECUTIONS_AFTER_DATE, String.valueOf(this.getIgnoreExecutionsAfterDate()),
-				dateFormat.format(this.getIgnoreExecutionsAfterDate()));
-
-		logger.debug("{}: {}", CMD_SHORT_LABELS, String.valueOf(this.shortLabels));
-		logger.debug("{}: {}", CMD_INCLUDE_SELF_LOOPS, String.valueOf(this.includeSelfLoops));
-		logger.debug("{}: {}", CMD_IGNORE_ASSUMED_CALLS, String.valueOf(this.ignoreAssumedCalls));
 
 		if (this.traceColoringFile != null) {
-			logger.debug("{}: {}", CMD_TRACE_COLORING, this.traceColoringFile.getAbsolutePath());
+			logger.debug("{}: {}", "--cmd-trace-coloring", this.traceColoringFile.getAbsolutePath());
 		} else {
-			logger.debug("{}: <none>", CMD_TRACE_COLORING);
+			logger.debug("{}: <none>", "--cmd-trace-coloring");
 		}
 		if (this.addDescriptions != null) {
-			logger.debug("{}: {}", CMD_ADD_DESCRIPTIONS, String.valueOf(this.addDescriptions));
+			logger.debug("{}: {}", "--add-description", String.valueOf(this.addDescriptions));
 		} else {
-			logger.debug("{}: <none>", CMD_ADD_DESCRIPTIONS);
+			logger.debug("{}: <none>", "--add-description");
 		}
-
-		logger.debug("{}: {}", CMD_HELP, String.valueOf(this.help));
-
 	}
 
 }
