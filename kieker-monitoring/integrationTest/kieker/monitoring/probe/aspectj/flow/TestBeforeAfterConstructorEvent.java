a9855983685448ef312f7cf42d4803dc2b4b64a9
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-03-11 16:51:16 +0100

    Add hint about currently "wrong" line index in test

--- kieker-monitoring/integrationTest/kieker/monitoring/probe/aspectj/flow/TestBeforeAfterConstructorEvent.java
+++ kieker-monitoring/integrationTest/kieker/monitoring/probe/aspectj/flow/TestBeforeAfterConstructorEvent.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import kieker.monitoring.probe.aspectj.operationExecution.Util;
 
 public class TestBeforeAfterConstructorEvent {
+	public static final int BEFOREAFTER_COLUMN_SIGNATURE = 5;
 	private static final String OPERATION_BEFOREAFTER_PROJECT = "example-beforeafterconstructorevent";
 
 	@Test
@@ -17,24 +18,30 @@ public class TestBeforeAfterConstructorEvent {
 		File logFolder = Util.runTestcase(OPERATION_BEFOREAFTER_PROJECT, "TestSimpleOperationExecution");
 
 		List<String> lines = Util.getLatestLogRecord(logFolder);
-		String firstSignature = lines.get(2).split(";")[5];
+		String firstSignature = lines.get(2).split(";")[BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public net.example.Instrumentable.<init>()", firstSignature);
-		String secondSignature = lines.get(3).split(";")[5];
+		String secondSignature = lines.get(3).split(";")[BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public net.example.Instrumentable.<init>()", secondSignature);
-		String thirdSignature = lines.get(5).split(";")[5];
+
+		/*
+		 * TODO Currently, the ApplicationTraceMetadata are added twice
+		 * (https://kieker-monitoring.atlassian.net/browse/KIEKER-1865) Therefore, this
+		 * tests for line 5; after this bug is fixed, it should test for line 4
+		 */
+		String thirdSignature = lines.get(5).split(";")[BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.callee()", thirdSignature);
 	}
-	
+
 	@Test
 	public void testThrowingExecution() throws IOException, InterruptedException {
 		File logFolder = Util.runTestcase(OPERATION_BEFOREAFTER_PROJECT, "TestOperationExecutionException");
 
 		List<String> lines = Util.getLatestLogRecord(logFolder);
-		String firstSignature = lines.get(2).split(";")[5];
+		String firstSignature = lines.get(2).split(";")[BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public net.example.Instrumentable.<init>()", firstSignature);
-		String secondSignature = lines.get(3).split(";")[5];
+		String secondSignature = lines.get(3).split(";")[BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public net.example.Instrumentable.<init>()", secondSignature);
-		String thirdSignature = lines.get(5).split(";")[5];
+		String thirdSignature = lines.get(5).split(";")[BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.throwingCallee()", thirdSignature);
 	}
 }
--- kieker-monitoring/integrationTest/kieker/monitoring/probe/aspectj/flow/TestBeforeAfterOperationEvent.java
+++ kieker-monitoring/integrationTest/kieker/monitoring/probe/aspectj/flow/TestBeforeAfterOperationEvent.java
@@ -17,13 +17,13 @@ public class TestBeforeAfterOperationEvent {
 		File logFolder = Util.runTestcase(OPERATION_BEFOREAFTER_PROJECT, "TestSimpleOperationExecution");
 
 		List<String> lines = Util.getLatestLogRecord(logFolder);
-		String firstSignature = lines.get(2).split(";")[5];
+		String firstSignature = lines.get(2).split(";")[TestBeforeAfterConstructorEvent.BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.callee()", firstSignature);
-		String secondSignature = lines.get(3).split(";")[5];
+		String secondSignature = lines.get(3).split(";")[TestBeforeAfterConstructorEvent.BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.callee2(java.lang.String)", secondSignature);
-		String thirdSignature = lines.get(4).split(";")[5];
+		String thirdSignature = lines.get(4).split(";")[TestBeforeAfterConstructorEvent.BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.callee2(java.lang.String)", thirdSignature);
-		String forthSignature = lines.get(5).split(";")[5];
+		String forthSignature = lines.get(5).split(";")[TestBeforeAfterConstructorEvent.BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.callee()", forthSignature);
 	}
 	
@@ -32,13 +32,13 @@ public class TestBeforeAfterOperationEvent {
 		File logFolder = Util.runTestcase(OPERATION_BEFOREAFTER_PROJECT, "TestOperationExecutionException");
 
 		List<String> lines = Util.getLatestLogRecord(logFolder);
-		String firstSignature = lines.get(2).split(";")[5];
+		String firstSignature = lines.get(2).split(";")[TestBeforeAfterConstructorEvent.BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.throwingCallee()", firstSignature);
-		String secondSignature = lines.get(3).split(";")[5];
+		String secondSignature = lines.get(3).split(";")[TestBeforeAfterConstructorEvent.BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.callee2(java.lang.String)", secondSignature);
-		String thirdSignature = lines.get(4).split(";")[5];
+		String thirdSignature = lines.get(4).split(";")[TestBeforeAfterConstructorEvent.BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.callee2(java.lang.String)", thirdSignature);
-		String forthSignature = lines.get(5).split(";")[5];
+		String forthSignature = lines.get(5).split(";")[TestBeforeAfterConstructorEvent.BEFOREAFTER_COLUMN_SIGNATURE];
 		Assert.assertEquals("public void net.example.Instrumentable.throwingCallee()", forthSignature);
 	}
 }
