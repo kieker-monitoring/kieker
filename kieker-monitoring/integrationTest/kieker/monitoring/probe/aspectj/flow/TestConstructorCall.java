4f45b602ccfe3d03b3de591cb40a5ba325e9c3bb
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-07-10 10:55:32 +0200

    Add test for static call

--- kieker-monitoring/integrationTest/kieker/monitoring/probe/aspectj/flow/TestConstructorCall.java
+++ kieker-monitoring/integrationTest/kieker/monitoring/probe/aspectj/flow/TestConstructorCall.java
@@ -9,9 +9,6 @@ import org.junit.Test;
 
 import kieker.monitoring.probe.aspectj.operationExecution.Util;
 
-/**
- * TODO Does currently not work (no monitoring logs are produced), added for completeness
- */
 public class TestConstructorCall {
 	static final int CALL_SIGNATURE_INDEX = 7;
 
@@ -22,6 +19,20 @@ public class TestConstructorCall {
 		System.out.println(temporaryFile.getAbsolutePath());
 		
 		File logFolder = Util.runTestcase(temporaryFile, "TestSimpleOperationExecution");
+
+		List<String> lines = Util.getLatestLogRecord(logFolder);
+		System.out.println(lines);
+		String firstSignature = lines.get(2).split(";")[CALL_SIGNATURE_INDEX];
+		Assert.assertEquals("public example.kieker.Instrumentable.<init>()", firstSignature);
+	}
+	
+	@Test
+	public void testStaticExecution() throws IOException, InterruptedException {
+		File temporaryFile = Util.createTemporaryProject(new File(Util.EXAMPLE_PROJECT_FOLDER, "aop_constructorCall_static.xml"));
+		
+		System.out.println(temporaryFile.getAbsolutePath());
+		
+		File logFolder = Util.runTestcase(temporaryFile, "TestStaticOperationExecution");
 
 		List<String> lines = Util.getLatestLogRecord(logFolder);
 		System.out.println(lines);
