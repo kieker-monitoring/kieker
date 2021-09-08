540be985791dff2a9faae384a635a7015cbace3f
Andre van Hoorn <van.hoorn@informatik.uni-stuttgart.de>
2021-09-08 23:57:22 +0200

    KIEKER-1877 Preparing 1.15-RC

--- kieker-analysis/src/kieker/analysis/plugin/reader/RegularRecordHandler.java
+++ kieker-analysis/src/kieker/analysis/plugin/reader/RegularRecordHandler.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Copyright 2020 Kieker Project (http://kieker-monitoring.net)
+ * Copyright 2021 Kieker Project (http://kieker-monitoring.net)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
