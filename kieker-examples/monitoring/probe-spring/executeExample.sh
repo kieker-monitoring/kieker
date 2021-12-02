f6a315d077942d7f2c34c1ceb972ee498bf08b2e
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-12-02 09:31:20 +0100

    Add executeExample.sh for probe-aspectj and probe-spring

---
+++ kieker-examples/monitoring/probe-spring/executeExample.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This scripts automates the execution of the Spring example
+
+rm monitoring-logs/* -rf
+
+./gradlew runMonitoring
+
+logCount=$(cat monitoring-logs/kieker-*/kieker-*.dat | wc -l)
+
+if (( $logCount == 5 )) ; then
+	echo "Run was sucessful"
+else
+	echo "Run had $logCount log entries; error occured"
+	exit 1
+fi
