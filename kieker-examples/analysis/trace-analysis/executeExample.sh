a4ced4f0e76de46aa62c8512e40acb91c818d04e
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-02-16 23:14:07 +0100

    Also make trace-analysis example executable

---
+++ kieker-examples/analysis/trace-analysis/executeExample.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This scripts automates the execution of the trace-analysis example
+
+rm kieker.log
+
+./gradlew runAnalysis
+
+processedCount=$(cat kieker.log | grep "Records processed in total" | awk '{print $NF}' | tr -d ".")
+
+if (( $processedCount == 6541 )) ; then
+	echo "Run was sucessful"
+else
+	echo "Run had $logCount log entries; error occured"
+	exit 1
+fi
