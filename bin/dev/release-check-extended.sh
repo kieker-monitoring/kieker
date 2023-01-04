0c79b5fb653c8cc3e7e417987e5802602ee2ae86
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-01-05 00:32:45 +0100

    KIEKER-1908 further fixing of the example execution.

--- bin/dev/release-check-extended.sh
+++ bin/dev/release-check-extended.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export BASE_DIR=$(cd "$(dirname "$0")"; pwd)
+
 # include common variables and functions
 source "$(dirname $0)/release-check-common.sh"
 
@@ -129,8 +131,7 @@ function check_bin_archive {
 	    cd ${ARCHDIR}
 	done
 
-	
-	EXAMPLE_RUN_SCRIPT_SH="runAllexamples.sh"
+	EXAMPLE_RUN_SCRIPT_SH="${BASE_DIR}/run-all-examples.sh"
 	information "Running all examples"
 	if ! (cd examples && $EXAMPLE_RUN_SCRIPT_SH); then
 		error "Examples where not successful"
