313512e5f6674b13f1c48f0b9ef750f71b2df71b
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-01-04 22:31:33 +0100

    KIEKER-1908 readded runAllexamples.sh

---
+++ kieker-examples/runAllexamples.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+set -e
+
+start=$(pwd)
+for example in $(find . -name "executeExample.sh"); do
+	echo "Executing $example"
+	cd $(dirname "$example")
+	./executeExample.sh
+	cd $start
+done
+
+# end
