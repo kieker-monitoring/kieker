7fc57bc1c90f56f6909bc09e87eb1253e498a056
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-01-10 10:17:34 +0100

    Automatically update read the docs version

--- kieker-documentation/conf.py
+++ kieker-documentation/conf.py
@@ -26,7 +26,7 @@ author = 'Various'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.16'
+release = '2.0.0-SNAPSHOT'
 
 
 # -- General configuration ---------------------------------------------------
