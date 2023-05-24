29b9cf59e8291e6413ea059a095ee5a039ac4b23
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-05-24 08:08:26 +0200

    KIEKER-1959 switch to new build image.

--- local-docker-build.sh
+++ local-docker-build.sh
@@ -17,7 +17,7 @@
 groups | grep docker >/dev/null
 
 if [ $? -eq 0 ]; then
-  docker run -u `id -u` --rm -v `pwd`:/opt/kieker kieker/kieker-build:openjdk8 /bin/bash -c "\
+  docker run -u `id -u` --rm -v `pwd`:/opt/kieker kieker/kieker-build:openjdk8 /bin/sh -c "\
     cd /opt/kieker;\
     export GRADLE_USER_HOME=/opt/kieker;\
     ./gradlew clean && \
