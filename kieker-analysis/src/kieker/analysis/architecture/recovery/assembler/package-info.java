5096f8c2e5d6b76836b4065156bda85c29560cf6
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-05-25 18:32:59 +0200

    Added java doc and added package-info

--- kieker-analysis/src/kieker/analysis/architecture/recovery/assembler/package-info.java
+++ kieker-analysis/src/kieker/analysis/architecture/recovery/assembler/package-info.java
@@ -1,11 +1,11 @@
 /***************************************************************************
- * Copyright (C) 2023 OceanDSL (https://oceandsl.uni-kiel.de)
+ * Copyright 2022 Kieker Project (http://kieker-monitoring.net)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,24 +14,3 @@
  * limitations under the License.
  ***************************************************************************/
 package kieker.analysis.architecture.recovery.assembler;
-
-import kieker.analysis.architecture.recovery.signature.IStorageSignatureExtractor;
-import kieker.model.analysismodel.type.StorageType;
-
-/**
- * Dummy signature extractor for storage signatures.
- *
- * @author Reiner Jung
- * @since 1.3.0
- */
-public class SimpleStorageSignatureExtractor implements IStorageSignatureExtractor {
-
-	/**
-	 * Dummy function used in tests to support the {@link IStorageSignatureExtractor} interface.
-	 */
-	@Override
-	public void extract(final StorageType storageType) {
-		// nothing to be done here, as this is used testing {@link StorageTypeModelAssembler}
-	}
-
-}
