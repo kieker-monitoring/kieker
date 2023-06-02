3a2c8e6022a39b4927e9ac60b670fa44927e3a82
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-06-02 16:44:04 +0200

    Add first real probe part to DiSL instrumentation

--- kieker-monitoring/src/kieker/monitoring/probe/disl/flow/operationExecution/OperationExecutionDiSL.java
+++ kieker-monitoring/src/kieker/monitoring/probe/disl/flow/operationExecution/OperationExecutionDiSL.java
@@ -1,5 +1,7 @@
 package kieker.monitoring.probe.disl.flow.operationExecution;
 
+import kieker.monitoring.core.controller.MonitoringController;
+
 import ch.usi.dag.disl.annotation.After;
 import ch.usi.dag.disl.annotation.Before;
 import ch.usi.dag.disl.marker.BodyMarker;
@@ -8,7 +10,9 @@ public class OperationExecutionDiSL {
 	
 	@Before(marker = BodyMarker.class, scope = "Main.*")
 	public static void beforemain() {
-
+		if (!MonitoringController.getInstance().isMonitoringEnabled()) {
+			return;
+		}
 		System.out.println("Instrumentation: Before (Kieker2)");
 	}
 
