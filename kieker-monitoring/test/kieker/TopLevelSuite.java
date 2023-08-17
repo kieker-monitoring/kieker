f890332e9a47f92b8a10738908dbd666e187a977
9mueller <justus.mueller@uni-hamburg.de>
2023-08-17 22:27:46 +0200

    Test

--- kieker-monitoring/test/kieker/TopLevelSuite.java
+++ kieker-monitoring/test/kieker/TopLevelSuite.java
@@ -1,2 +1,37 @@
-package kieker;public class TopLevelSuite {
+package kieker;
+
+import kieker.test.monitoring.junit.core.configuration.TestConfigurationFactoryMethods;
+import kieker.test.monitoring.junit.core.controller.*;
+import kieker.test.monitoring.junit.core.sampler.TestPeriodicSampling;
+import kieker.test.monitoring.junit.core.signaturePattern.TestPatternParser;
+import kieker.test.monitoring.junit.probe.adaptiveMonitoring.cxf.TestCXFClientServerInterceptors;
+import kieker.test.monitoring.junit.probe.adaptiveMonitoring.mxbean.*;
+import kieker.test.monitoring.junit.probe.adaptiveMonitoring.spring.TestSpringMethodInterceptor;
+import kieker.test.monitoring.junit.probe.cxf.executions.TestCXFClientServerInterceptorsNoSessionRegisteredBefore;
+import kieker.test.monitoring.junit.probe.cxf.executions.TestCXFClientServerInterceptorsSessionRegisteredBefore;
+import kieker.test.monitoring.junit.probe.spring.executions.TestSpringMethodInterceptorInterceptorIsEntryPoint;
+import kieker.test.monitoring.junit.probe.spring.executions.TestSpringMethodInterceptorInterceptorIsNotEntryPoint;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@Suite.SuiteClasses({
+        TestPeriodicSampling.class,
+        TestJMXInterface.class,
+        TestProbeController.class,
+        TestControllerConstruction.class,
+        TestAutoSetLoggingTimestamp.class,
+        TestMonitoringControllerStateTransitions.class,
+        TestMonitoringControllerRecordsPassedInMonitoringStates.class,
+        TestConfigurationFactoryMethods.class,
+        TestPatternParser.class,
+        TestCXFClientServerInterceptorsNoSessionRegisteredBefore.class,
+        TestCXFClientServerInterceptorsSessionRegisteredBefore.class,
+        TestSpringMethodInterceptorInterceptorIsEntryPoint.class,
+        TestCXFClientServerInterceptors.class,
+        TestGCSampler.class,
+        TestSpringMethodInterceptor.class,
+        TestSpringMethodInterceptorInterceptorIsNotEntryPoint.class
+})
+@RunWith(Suite.class)
+public class TopLevelSuite {
 }
