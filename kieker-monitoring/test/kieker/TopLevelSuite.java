0ca9df30d9d7a67b7f334b6a78ef6615ebe1c9b8
9mueller <justus.mueller@uni-hamburg.de>
2023-08-18 11:00:24 +0200

    Added more tests

--- kieker-monitoring/test/kieker/TopLevelSuite.java
+++ kieker-monitoring/test/kieker/TopLevelSuite.java
@@ -1,5 +1,19 @@
 package kieker;
 
+import kieker.monitoring.core.controller.ProbeControllerTest;
+import kieker.monitoring.core.controller.TCPControllerTest;
+import kieker.monitoring.core.controller.WriterControllerTest;
+import kieker.monitoring.core.controller.tcp.TcpRecordReaderTest;
+import kieker.monitoring.timer.SystemMilliTimerTest;
+import kieker.monitoring.timer.SystemNanoTimerTest;
+import kieker.monitoring.writer.MonitoringWriterThread;
+import kieker.monitoring.writer.MonitoringWriterThreadTest;
+import kieker.monitoring.writer.compression.*;
+import kieker.monitoring.writer.filesystem.*;
+import kieker.monitoring.writer.influxdb.InfluxDBWriterFailingTest;
+import kieker.monitoring.writer.namedRecordPipe.TestPipeWriter;
+import kieker.monitoring.writer.tcp.SingleSocketTcpWriterTest;
+import kieker.monitoring.writer.tcp.SocketChannelTest;
 import kieker.test.monitoring.junit.core.configuration.TestConfigurationFactoryMethods;
 import kieker.test.monitoring.junit.core.controller.*;
 import kieker.test.monitoring.junit.core.sampler.TestPeriodicSampling;
@@ -16,6 +30,30 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+
+        TcpRecordReaderTest.class,
+        ProbeControllerTest.class,
+        TCPControllerTest.class,
+        WriterControllerTest.class,
+        SystemMilliTimerTest.class,
+        SystemNanoTimerTest.class,
+        DeflateCompressionFilterTest.class,
+        GZipCompressionFilterTest.class,
+        NoneCompressionFilterTest.class,
+        XZCompressionFilterTest.class,
+        ZipCompressionFilterTest.class,
+        AsciiFileWriterTest.class,
+        BinaryFileWriterTest.class,
+        BinaryLogStreamHandlerTest.class,
+        GenericBinaryFileWriterTest.class,
+        GenericTextFileWriterTest.class,
+        TextLogStreamHandlerTest.class,
+        InfluxDBWriterFailingTest.class,
+        TestPipeWriter.class,
+        SingleSocketTcpWriterTest.class,
+        SocketChannelTest.class,
+        MonitoringWriterThreadTest.class,
+        //
         TestPeriodicSampling.class,
         TestJMXInterface.class,
         TestProbeController.class,
