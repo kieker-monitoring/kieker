306b8a1203623f585e3af95d0c2956ee93586320
DaGeRe <davidgeorg_reichelt@dagere.de>
2024-01-25 14:37:03 +0100

    Fix start of Zipkin and destroying it afterwards

--- kieker-tools/log-replayer/test/kieker/tools/log/replayer/stages/ZipkinServerTest.java
+++ kieker-tools/log-replayer/test/kieker/tools/log/replayer/stages/ZipkinServerTest.java
@@ -1,7 +1,10 @@
 package kieker.tools.log.replayer.stages;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,33 +12,55 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class ZipkinServerTest {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DataSendStage.class);
 
+	private static final String jarPath = "src/test/resources/zipkin-server-3.0.4-exec.jar";
+	
+	private Process process;
+	
     @BeforeEach
     public void startZipkinServer() throws IOException {
-        String jarPath = "kieker-tools/test-resources/kieker.monitoring.probe.aspectj.flow.operationExecution/zipkin-server-3.0.4-exec.jar";
+    	LOGGER.info("Starting zipking");
+        
         String command = String.format("java -jar %s", jarPath);
 
         try {
-            Process process = Runtime.getRuntime().exec(command);
+        	LOGGER.info("Command: " + command);
+            process = Runtime.getRuntime().exec(command);
 
-            // capture and print the process output
-             InputStream inputStream = process.getInputStream();
-             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-             String line;
-             while ((line = bufferedReader.readLine()) != null) {
-                 System.out.println(line);
-            }
-
-            // Wait for a moment to allow Zipkin to start 
-            Thread.sleep(5000);
-        } catch (InterruptedException | IOException e) {
+            waitForZipkinStartup();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+	private void waitForZipkinStartup() throws IOException {
+		// capture and print the process output
+		 InputStream inputStream = process.getInputStream();
+		 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		 String line;
+		 while ((line = bufferedReader.readLine()) != null) {
+			 LOGGER.info(line);
+			 if (line.contains("Serving HTTP at")) {
+				 LOGGER.info("Startup finished");
+				 return;
+			 }
+		}
+	}
+
     @Test
     public void Test() {
-        //test method 
+    	try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    @AfterEach
+    public void stopZipkinServer() {
+    	process.destroyForcibly();
     }
 }
