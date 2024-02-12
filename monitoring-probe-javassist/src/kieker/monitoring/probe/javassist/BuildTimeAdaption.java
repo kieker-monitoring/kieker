6897dc80aebe27b2c4e90a2bff229725c5605b91
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-02-12 16:22:07 +0100

    Finalize jar creation

--- monitoring-probe-javassist/src/kieker/monitoring/probe/javassist/BuildTimeAdaption.java
+++ monitoring-probe-javassist/src/kieker/monitoring/probe/javassist/BuildTimeAdaption.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
@@ -24,16 +25,39 @@ public class BuildTimeAdaption {
 		
 		System.out.println("Writing to: " + tempDir.getAbsolutePath());
 		
-		ProcessBuilder builder = new ProcessBuilder(new String[] {"jar", "xf", instrumentJar.getAbsolutePath() });
-		builder.directory(tempDir);
-		
-		builder.start();
+		extractJar(instrumentJar, tempDir);
 		
 		ClassPool pool = ClassPool.getDefault();
 		pool.insertClassPath(instrumentJar.getAbsolutePath());
 		
 		String className = "moobench.application.MonitoredClassSimple";
 		instrumentClass(tempDir, pool, className);
+		
+		createJar(instrumentJar, tempDir);
+	}
+
+	private static void extractJar(File instrumentJar, File tempDir) throws IOException {
+		ProcessBuilder builder = new ProcessBuilder(new String[] {"jar", "xf", instrumentJar.getAbsolutePath() });
+		builder.directory(tempDir);
+		builder.inheritIO();
+		builder.start();
+	}
+	
+	private static void createJar(File instrumentJar, File tempDir) throws IOException {
+		ProcessBuilder builder = new ProcessBuilder(new String[] {"jar", "cf",
+				instrumentJar.getAbsolutePath(),
+				"." });
+		builder.directory(tempDir);
+		
+		builder.inheritIO();
+		
+		Process start = builder.start();
+		
+		try {
+			start.waitFor();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private static void instrumentClass(File tempDir, ClassPool pool, String className)
