c814d6fd7325bd2e793da875517ccf430889babd
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-02-13 16:49:34 +0100

    Fix checkstyle errors; some cannot be fixed easily since of DiSLs internal structure, so I increased the thresholds

--- monitoring/src/kieker/monitoring/probe/disl/flow/operationExecution/KiekerStaticContext.java
+++ monitoring/src/kieker/monitoring/probe/disl/flow/operationExecution/KiekerStaticContext.java
@@ -1,8 +1,7 @@
 package kieker.monitoring.probe.disl.flow.operationExecution;
 
-import static java.util.stream.Collectors.joining;
-
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
@@ -17,53 +16,52 @@ import ch.usi.dag.disl.util.JavaNames;
  */
 public class KiekerStaticContext extends AbstractStaticContext {
 
-    /**
-     * Returns the fully qualified name of the method and its signature,
-     * including the name of the containing class in the canonical (not
-     * internal) format, i.e., with packages delimited by the '.' character.
-     */
-    public String operationSignature() {
-        final ClassNode classNode = staticContextData.getClassNode ();
-        final MethodNode methodNode = staticContextData.getMethodNode ();
-        final Type methodType = Type.getMethodType (methodNode.desc);
+	/**
+	 * Returns the fully qualified name of the method and its signature,
+	 * including the name of the containing class in the canonical (not
+	 * internal) format, i.e., with packages delimited by the '.' character.
+	 */
+	public String operationSignature() {
+		final ClassNode classNode = staticContextData.getClassNode();
+		final MethodNode methodNode = staticContextData.getMethodNode();
+		final Type methodType = Type.getMethodType(methodNode.desc);
 
-        final String result = String.format ("%s %s%s(%s)",
-            __formatMethodReturnType(methodType),
-            __formatClassName(classNode.name),
-            __formatMethodName(methodNode.name),
-            __formatMethodParameters(methodType)
-        );
+		final String result = String.format("%s %s%s(%s)",
+				formatMethodReturnType(methodType),
+				__formatClassName(classNode.name),
+				__formatMethodName(methodNode.name),
+				formatMethodParameters(methodType));
 
-        return result;
-    }
+		return result;
+	}
 
-    private String __formatClassName(final String className) {
-        return JavaNames.internalToType (className);
-    }
+	private String __formatClassName(final String className) {
+		return JavaNames.internalToType(className);
+	}
 
-    private String __formatMethodName(final String methodName) {
-        if (JavaNames.isConstructorName (methodName)) {
-            return "";
-        } else {
-            return String.format (".%s", methodName);
-        }
-    }
+	private String __formatMethodName(final String methodName) {
+		if (JavaNames.isConstructorName(methodName)) {
+			return "";
+		} else {
+			return String.format(".%s", methodName);
+		}
+	}
 
-    private String __formatMethodReturnType(final Type methodType) {
-        return __formatType(methodType.getReturnType ());
-    }
+	private String formatMethodReturnType(final Type methodType) {
+		return __formatType(methodType.getReturnType());
+	}
 
-    private String __formatMethodParameters(final Type methodType) {
-        return Arrays.stream (methodType.getArgumentTypes ())
-            .map (t -> __formatType(t)).collect (joining (","));
-    }
+	private String formatMethodParameters(final Type methodType) {
+		return Arrays.stream(methodType.getArgumentTypes())
+				.map(t -> __formatType(t)).collect(Collectors.joining(","));
+	}
 
-    private String __formatType(final Type type) {
-        if (type.getSort () != Type.METHOD) {
-            return type.getClassName ().replace ("java.lang.", "");
-        } else {
-            throw new AssertionError ("unexpected sort of type: "+ type.getSort ());
-        }
-    }
+	private String __formatType(final Type type) {
+		if (type.getSort() != Type.METHOD) {
+			return type.getClassName().replace("java.lang.", "");
+		} else {
+			throw new AssertionError("unexpected sort of type: " + type.getSort());
+		}
+	}
 
 }
