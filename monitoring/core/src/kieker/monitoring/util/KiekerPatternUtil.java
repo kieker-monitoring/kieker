e807085640380ecf6a0fef170ebd79d14f590a81
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-05-22 22:33:09 +0200

    Fix PMD issues, remove duplicated class

--- monitoring/core/src/kieker/monitoring/util/KiekerPatternUtil.java
+++ monitoring/core/src/kieker/monitoring/util/KiekerPatternUtil.java
@@ -1,3 +1,19 @@
+/***************************************************************************
+ * Copyright 2022 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
 package kieker.monitoring.util;
 
 import java.util.LinkedList;
@@ -6,37 +22,40 @@ import java.util.List;
 import kieker.monitoring.core.signaturePattern.InvalidPatternException;
 import kieker.monitoring.core.signaturePattern.PatternParser;
 
+/**
+ * Utility class for checking whether something should be monitored or not (which is specified by a semicolon separated list of kieker patterns).
+ */
 public class KiekerPatternUtil {
-	public static List<KiekerPattern> getPatterns(String instrumentables) {
-		List<KiekerPattern> patternObjects = new LinkedList<KiekerPattern>();
-		String[] patterns = instrumentables.split(";");
-		for (String pattern : patterns) {
-			String clazzMethodAndPrefix = pattern.substring(0, pattern.indexOf('('));
-			String clazzAndPrefix = clazzMethodAndPrefix.substring(0, pattern.lastIndexOf('.'));
-			String onlyClazz = clazzAndPrefix.contains(" ")
-					? clazzAndPrefix.substring(clazzAndPrefix.lastIndexOf(" ") + 1)
+	public static List<KiekerPattern> getPatterns(final String instrumentables) {
+		final List<KiekerPattern> patternObjects = new LinkedList<>();
+		final String[] patterns = instrumentables.split(";");
+		for (final String pattern : patterns) {
+			final String clazzMethodAndPrefix = pattern.substring(0, pattern.indexOf('('));
+			final String clazzAndPrefix = clazzMethodAndPrefix.substring(0, pattern.lastIndexOf('.'));
+			final String onlyClazz = clazzAndPrefix.contains(" ")
+					? clazzAndPrefix.substring(clazzAndPrefix.lastIndexOf(' ') + 1)
 					: clazzAndPrefix;
 			try {
-				KiekerPattern patternObject = new KiekerPattern(onlyClazz, PatternParser.parseToPattern(pattern));
+				final KiekerPattern patternObject = new KiekerPattern(onlyClazz, PatternParser.parseToPattern(pattern));
 				patternObjects.add(patternObject);
-			} catch (InvalidPatternException e) {
+			} catch (final InvalidPatternException e) {
 				e.printStackTrace();
 			}
 		}
 		return patternObjects;
 	}
-	
-	public static boolean classIsContained(List<KiekerPattern> patternObjects, String clazz) {
-		for (KiekerPattern pattern : patternObjects) {
+
+	public static boolean classIsContained(final List<KiekerPattern> patternObjects, final String clazz) {
+		for (final KiekerPattern pattern : patternObjects) {
 			if (clazz.equals(pattern.getOnlyClass())) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public static boolean methodIsContained(List<KiekerPattern> patternObjects, String signature) {
-		for (KiekerPattern pattern : patternObjects) {
+
+	public static boolean methodIsContained(final List<KiekerPattern> patternObjects, final String signature) {
+		for (final KiekerPattern pattern : patternObjects) {
 			if (pattern.getFullPattern().matcher(signature).matches()) {
 				return true;
 			}
