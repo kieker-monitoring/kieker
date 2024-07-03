a3f9a992782fa3a50c3b112ffef0ea23ca44e1c7
Reiner Jung <reiner.jung@email.uni-kiel.de>
2024-07-03 12:43:39 +0200

    2.0.0-RC minor fixes

---
+++ monitoring/javassist/src/kieker/monitoring/probe/javassist/KiekerClassTransformer.java
@@ -0,0 +1,74 @@
+/***************************************************************************
+ * Copyright 2024 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+package kieker.monitoring.probe.javassist;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+import java.util.List;
+
+import kieker.monitoring.util.KiekerPattern;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.NotFoundException;
+
+/**
+ * Kieker class transformer.
+ *
+ * @author David G. Reichelt
+ */
+public class KiekerClassTransformer implements ClassFileTransformer {
+
+	private final List<KiekerPattern> patternObjects;
+
+	public KiekerClassTransformer(final List<KiekerPattern> patternObjects) {
+		this.patternObjects = patternObjects;
+	}
+
+	@Override
+	public byte[] transform(final ClassLoader loader, final String className, final Class<?> classBeingRedefined,
+			final ProtectionDomain protectionDomain, final byte[] classfileBuffer) throws IllegalClassFormatException {
+		final String realClassName = className.replaceAll(File.separator, ".");
+		for (final KiekerPattern pattern : this.patternObjects) {
+			if (realClassName.equals(pattern.getOnlyClass())) {
+				System.out.println("Instrumenting: " + realClassName);
+				final ClassPool cp = ClassPool.getDefault();
+				try {
+					final CtClass cc = cp.get(realClassName);
+					if (!cc.isInterface()) {
+						// TODO: Interfaces would also require instrumentation of default method; this is left out for now because of the experimental structure
+
+						final MethodInstrumenter instrumenter = new MethodInstrumenter(cp);
+						instrumenter.instrumentAllMethods(cc);
+
+						final byte[] byteCode = cc.toBytecode();
+						cc.detach();
+
+						return byteCode;
+					}
+				} catch (NotFoundException | CannotCompileException | IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null; // NOPMD API requires it
+	}
+
+}
---
+++ monitoring/javassist/src/kieker/monitoring/probe/javassist/PremainClass.java
@@ -0,0 +1,50 @@
+/***************************************************************************
+ * Copyright 2024 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+package kieker.monitoring.probe.javassist;
+
+import java.lang.instrument.Instrumentation;
+import java.util.List;
+
+import kieker.monitoring.util.KiekerPattern;
+import kieker.monitoring.util.KiekerPatternUtil;
+
+/**
+ * Javassist monitoring.
+ *
+ * @author David G. Reichelt
+ */
+public final class PremainClass { // NOPMD not sure whether this class could be renamed
+
+	private static Instrumentation currentInstrumentation;
+
+	private PremainClass() {
+		// Nothing to do here
+	}
+
+	public static void premain(final String agentArgs, final Instrumentation inst) {
+		currentInstrumentation = inst;
+
+		final String instrumentables = System.getenv("KIEKER_SIGNATURES");
+		if (instrumentables != null) {
+			final List<KiekerPattern> patternObjects = KiekerPatternUtil.getPatterns(instrumentables);
+			final KiekerClassTransformer kiekerTransformer = new KiekerClassTransformer(patternObjects);
+			currentInstrumentation.addTransformer(kiekerTransformer);
+		} else {
+			System.err.println("Environment variable KIEKER_SIGNATURES not defined - not instrumenting anything!"); // NOPMD
+		}
+	}
+
+}
